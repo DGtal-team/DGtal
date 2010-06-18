a23a942cc71aed2038fdcc8972e299c532a280a7
dcoeurjo <dcoeurjo@5422497e-619f-4381-bc00-4cdae604c9f6>
2010-06-18 12:41:05 +0000

    Documentation...
    git-svn-id: https://svn.liris.cnrs.fr/dgtal/trunk@171 5422497e-619f-4381-bc00-4cdae604c9f6

--- src/DGtal/kernel/images/Image.h
+++ src/DGtal/kernel/images/Image.h
@@ -35,26 +35,25 @@
 namespace DGtal
 {
 
-/////////////////////////////////////////////////////////////////////////////
-// class Image
-/**
- * Description of class 'Image' <p>
- * Aim:
- *
- * \todo Iterateurs dans le cas générique (const et non const) ( (*It) = Point-> Indice -> Valeur)
- * \todo spécialisé si TContainer=vector pour avoir des iterateurs customs (*It)= Indice -> Valeur
- * \todo Documentation
- */
+  /////////////////////////////////////////////////////////////////////////////
+  // class Image
+  /**
+   * Description of class 'Image' <p>
+   * Aim:
+   *
+   * \todo spaniterator dans le container_map
+   * \todo Documentation
+   */
 
-template <class THyperRectDomain, typename TValue, class TContainer >
-class Image
-{
+  template <class THyperRectDomain, typename TValue, class TContainer >
+  class Image
+  {
 
 
 
 
     // ----------------------- Standard services ------------------------------
-public:
+  public:
 
 
     typedef typename THyperRectDomain::TPoint TPoint;
@@ -80,21 +79,24 @@ public:
     ~Image();
 
     // ----------------------- Interface --------------------------------------
-public:
+  public:
 
     /**
-    *  Generic function to get the value of an image at a point \param aPoint
-    *
-    */
+     *  Generic function to get the value of an image at a point.
+     *
+     * \param aPoint the point
+     * \return the value at the point \param aPoint
+     */
     TValue operator()(const TPoint &aPoint);
 
 
     /**
-     *  Generic function to get the value of an image at a point \param aPoint
-     *
+     *  Generic function to get the value of an image at position given
+     * by a build-in iterator.
+     * \param aIt the iteror
+     * \return the value referenced by the iterator
      */
     TValue operator()(const Iterator &aIt);
-
 
 
     /**
@@ -103,18 +105,35 @@ public:
      */
     TValue operator()(const SpanIterator &aIt);
 
-    void setValue(const TPoint &aPoint, const TValue aVal)
+
+    /** 
+     * Set a value in a Image position.
+     * 
+     * @param aPoint the position as a Point
+     * @param aVal the value to store
+     */    void setValue(const TPoint &aPoint, const TValue aVal)
     {
-        myImageMap.setValue(aPoint,aVal);
+      myImageMap.setValue(aPoint,aVal);
     }
 
-    ///\todo FIXME
+     /** 
+     * Set a value in a Image position given by a built-in iterator
+     * 
+     * @param aIt the built-in iterator 
+     * @param aVal the value to store
+     */
     void setValue(Iterator &aIt, const TValue aVal)
     {
       myImageMap.setValue(aIt,aVal);
       
     }
 
+    /** 
+     * Set a value in a Image position given by a span iterator
+     * 
+     * @param aIt the span iterator 
+     * @param aVal the value to store
+     */
     void setValue(SpanIterator &aIt, const TValue aVal)
     {
       myImageMap.setValue(aIt,aVal);
@@ -123,45 +142,89 @@ public:
 
     // ----------------------- Built-in terators  from the container--------------------------------
 
+    /** 
+     * ImageContainer built-in begin() iterator
+     * @return the built-in begin() iterator
+     */
     Iterator begin() {
-        return myImageMap.begin();
+      return myImageMap.begin();
     }
 
+    /** 
+     * ImageContainer built-in end()  iterator
+     * @return the built-in end()  iterator
+     */
     Iterator end() {
-        return myImageMap.end();
+      return myImageMap.end();
     }
 
+    /** 
+     * ImageContainer span iterator  starting at aPoint along  dimension.
+     *
+     * \param aPoint starting point
+     * \param aDimension direction of the span iterator
+     * @return the begin span iterator
+     */
     SpanIterator span_begin(const TPoint &aPoint, const std::size_t aDimension) {
-        return myImageMap.span_begin(aPoint,aDimension);
+      return myImageMap.span_begin(aPoint,aDimension);
 
     }
 
+    /** 
+     * ImageContainer span "end" iterator  starting at aPoint along  dimension.
+     *
+     * \param aPoint starting point
+     * \param aDimension direction of the span iterator
+     * @return the  end  span iterator
+     */
     SpanIterator span_end(const TPoint &aPoint, const std::size_t aDimension) {
-        return myImageMap.span_end(aPoint,aDimension);
+      return myImageMap.span_end(aPoint,aDimension);
     }
-
+    /** 
+     * ImageContainer built-in rend()  iterator
+     * @return the built-in rend()  iterator
+     */
     Iterator rend() {
-        return myImageMap.rend();
+      return myImageMap.rend();
     }
 
+    /** 
+     * ImageContainer built-in rbegin() iterator
+     * @return the built-in rbegin() iterator
+     */
     Iterator rbegin() {
-        return myImageMap.rend();
+      return myImageMap.rend();
     }
 
+    /** 
+     * ImageContainer built-in begin() const iterator
+     * @return the built-in begin() const iterator
+     */
     ConstIterator begin() const {
-        return myImageMap.begin();
+      return myImageMap.begin();
     }
 
+    /** 
+     * ImageContainer built-in end() const iterator
+     * @return the built-in end() const iterator
+     */
     ConstIterator end() const {
-        return myImageMap.end();
+      return myImageMap.end();
     }
-
+    /** 
+     * ImageContainer built-in rend() const iterator
+     * @return the built-in rend() const iterator
+     */
     ConstIterator rend() const {
-        return myImageMap.rend();
+      return myImageMap.rend();
     }
 
+    /** 
+     * ImageContainer built-in rbegin() const iterator
+     * @return the built-in rbegin() const iterator
+     */
     ConstIterator rbegin() const {
-        return myImageMap.rend();
+      return myImageMap.rend();
     }
 
     //-----------------------------------------------------------------------
@@ -177,46 +240,46 @@ public:
      */
     bool isValid() const;
 
-protected:
+  protected:
 
     THyperRectDomain myDomain; ///Local copie of the HyperRectDomain (to have generic iterators) \todo should be removed in specialized classes
     TContainer myImageMap; ///Image Container
 
     TValue myValue;
-private:
+  private:
 
     /**
-    * Assignment.
-    * @param other the object to copy.
-    * @return a reference on 'this'.
-    * Forbidden by default.
-    */
+     * Assignment.
+     * @param other the object to copy.
+     * @return a reference on 'this'.
+     * Forbidden by default.
+     */
     Image & operator= ( const Image & other );
 
     // ------------------------- Internals ------------------------------------
-private:
+  private:
 
 
 
-}; // end of class Image
+  }; // end of class Image
 
 
-/**
- * Overloads 'operator<<' for displaying objects of class 'Image'.
- * @param out the output stream where the object is written.
- * @param object the object of class 'Image' to write.
- * @return the output stream after the writing.
- */
-template <class THyperRectDomain, typename T, class TCont>
-inline
-std::ostream&
-operator<< ( std::ostream & out, const Image<THyperRectDomain,T,TCont> & object );
+  /**
+   * Overloads 'operator<<' for displaying objects of class 'Image'.
+   * @param out the output stream where the object is written.
+   * @param object the object of class 'Image' to write.
+   * @return the output stream after the writing.
+   */
+  template <class THyperRectDomain, typename T, class TCont>
+  inline
+  std::ostream&
+  operator<< ( std::ostream & out, const Image<THyperRectDomain,T,TCont> & object );
 
 
 
 
-///////////////////////////////////////////////////////////////////////////////
-// Inline methods.
+  ///////////////////////////////////////////////////////////////////////////////
+  // Inline methods.
 #include "DGtal/kernel/images/Image.ih"
 
 
--- src/DGtal/kernel/images/ImageContainerConcept.h
+++ src/DGtal/kernel/images/ImageContainerConcept.h
@@ -1,9 +1,51 @@
+#pragma once
+
+/**
+ * @file ImageContainerConcept.h
+ * @author David Coeurjolly (\c david.coeurjolly@liris.cnrs.fr )
+ * Laboratoire d'InfoRmatique en Image et Systèmes d'information - LIRIS (CNRS, UMR 5205), CNRS, France
+ *
+ * @date 2010/06/09
+*
+ * This file is part of the DGtal library.
+ */
+
+#if defined(ImageContainerConceptRECURSES)
+#error Recursive header files inclusion detected in ImageContainerConcept.h
+#else // defined(ImageContainerConceptRECURSES)
+/** Prevents recursive inclusion of headers. */
+#define ImageContainerConceptRECURSES
+
+#if !defined ImageContainerConcept_h
+/** Prevents repeated inclusion of headers. */
+#define ImageContainerConcept_h
+
 #include <boost/concept_check.hpp>
 #include <boost/concept/assert.hpp>
 #include <boost/concept/requires.hpp>
 
 namespace DGtal
 {
+
+ /////////////////////////////////////////////////////////////////////////////
+  // struct ImageContainerConcept
+  /**
+   * Description of struct 'ImageContainerConcept' <p>
+   *
+   *
+   * @brief Implements the concept 'ImageContainer' using boost concept.
+   * 
+   * Usage: to check if the concept is consistent with the  template arguments TPoint (Point class), 
+   * TValue, and TContainer, we have to use the following snippet:
+   *
+   * \code
+   * ...
+   *  BOOST_CONCEPT_ASSERT((DGtal::ImageContainerConcept<TPoint,TValue,TContainer>));
+   * ...
+   * \endcode
+   * \see Image.h
+   */
+
 template <class TPoint, typename TValue, class TContainer>
 struct ImageContainerConcept  : boost::Assignable<TValue>, boost::EqualityComparable<TValue>
 {
@@ -26,9 +68,16 @@ struct ImageContainerConcept  : boost::Assignable<TValue>, boost::EqualityCompar
     Iterator it;
     TValue v;
     TPoint a,b;
-
+ 
     // Type deduction will fail unless the arguments have the same type.
     template <typename T>
     void same_type(T const&, T const&);
 };
-}
+} // namespace DGtal
+//                                                                           //
+///////////////////////////////////////////////////////////////////////////////
+
+#endif // !defined ImageContainerConcept_h
+
+#undef ImageContainerConceptRECURSES
+#endif // else defined(ImageContainerConceptRECURSES)
