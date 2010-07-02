30b3cdf45d936048f33aa115752fe2e5fb935533
troussil <troussil@5422497e-619f-4381-bc00-4cdae604c9f6>
2010-07-02 10:09:45 +0000

    ajout classe ArithDSS, tentative 2
    git-svn-id: https://svn.liris.cnrs.fr/dgtal/trunk@261 5422497e-619f-4381-bc00-4cdae604c9f6

---
+++ src/DGtal/geometry/2d/ArithDSS.h
@@ -0,0 +1,170 @@
+#pragma once
+
+/**
+ * @file ArithDSS.h
+ * @author Tristan Roussillon (\c tristan.roussillon@liris.cnrs.fr )
+ * Laboratoire d'InfoRmatique en Image et Systèmes d'information - LIRIS (CNRS, UMR 5205), CNRS, France
+ *
+ * @date 2010/07/01
+ *
+ * Header file for module ArithDSS.cpp
+ *
+ * This file is part of the DGtal library.
+ */
+
+#if defined(ArithDSS_RECURSES)
+#error Recursive header files inclusion detected in ArithDSS.h
+#else // defined(ArithDSS_RECURSES)
+/** Prevents recursive inclusion of headers. */
+#define ArithDSS_RECURSES
+
+#if !defined ArithDSS_h
+/** Prevents repeated inclusion of headers. */
+#define ArithDSS_h
+
+//////////////////////////////////////////////////////////////////////////////
+// Inclusions
+#include <iostream>
+#include "DGtal/base/Common.h"
+//////////////////////////////////////////////////////////////////////////////
+
+namespace DGtal
+{
+
+/////////////////////////////////////////////////////////////////////////////
+// class ArithDSS
+/**
+ * Description of class 'ArithDSS' <p>
+ * \brief Aim:
+ * Recognition of a digital straight segment (DSS)
+ * based on the arithmetical algorithm of 
+ * Debled and Reveilles (1995)
+ */
+template <typename Domain2D>
+class ArithDSS
+{
+    // ----------------------- Standard services ------------------------------
+public:
+
+		
+		typedef typename Domain2D::Coordinate Integer;
+		//2D point of a domain
+		typedef typename Domain2D::Point Point;
+		//2D vector of a domain
+		typedef typename Domain2D::Vector Vector;
+
+
+    /**
+     * Constructor.
+     * Forbidden by default (protected to avoid g++ warnings).
+     */
+    ArithDSS(){};
+
+    /**
+     * Destructor.
+     */
+    ~ArithDSS(){};
+
+    // ----------------------- Interface --------------------------------------
+public:
+
+    /**
+     * Writes/Displays the object on an output stream.
+     * @param out the output stream where the object is written.
+     */
+    void selfDisplay ( std::ostream & out ) const;
+
+    /**
+     * Checks the validity/consistency of the object.
+     * @return 'true' if the object is valid, 'false' otherwise.
+		 * \todo implementation
+     */
+    bool isValid() const;
+
+    /**
+		 * Tests whether the union between a point and a DSS
+	   * is a DSS. Computes the parameters of the new DSS
+     * if true.
+     * @return 'true' if the union is a DSS, 'false' otherwise.
+     */
+    bool add(const Point & aPoint);
+
+
+    // ------------------------- Protected Datas ------------------------------
+protected:
+
+	//parameters of the DSS
+	Integer myA, myB, myMu, myOmega;
+
+	//leaning points
+	Point myUf, myUl, myLf, myLl;
+
+	//first and last point
+	Point myF, myL;
+
+    // ------------------------- Private Datas --------------------------------
+	
+private:
+
+    // ------------------------- Hidden services ------------------------------
+protected:
+
+
+    /**
+		 * Computes the norm of the two components
+     * of a 2D vector.
+		 * @param x and y, two values. 
+     * @return the norm of a 2D vector.
+     */
+    virtual Integer norm(const Integer& x, const Integer& y) const = 0;
+
+private:
+
+    /**
+     * Copy constructor.
+     * @param other the object to clone.
+     * Forbidden by default.
+     */
+    ArithDSS ( const ArithDSS & other );
+
+    /**
+     * Assignment.
+     * @param other the object to copy.
+     * @return a reference on 'this'.
+     * Forbidden by default.
+     */
+    ArithDSS & operator= ( const ArithDSS & other );
+
+    // ------------------------- Internals ------------------------------------
+private:
+
+}; // end of class ArithDSS
+
+
+/**
+ * Overloads 'operator<<' for displaying objects of class 'ArithDSS'.
+ * @param out the output stream where the object is written.
+ * @param object the object of class 'ArithDSS' to write.
+ * @return the output stream after the writing.
+ */
+template<typename Domain2D>
+std::ostream&
+operator<< ( std::ostream & out, const ArithDSS<Domain2D> & object );
+
+
+} // namespace DGtal
+
+
+///////////////////////////////////////////////////////////////////////////////
+// Includes inline functions/methods if necessary.
+#if defined(INLINE)
+#include "DGtal/geometry/2d/ArithDSS.ih"
+#endif
+
+//                                                                           //
+///////////////////////////////////////////////////////////////////////////////
+
+#endif // !defined ArithDSS_h
+
+#undef ArithDSS_RECURSES
+#endif // else defined(ArithDSS_RECURSES)
---
+++ src/DGtal/geometry/2d/ArithDSS4.h
@@ -0,0 +1,132 @@
+#pragma once
+
+/**
+ * @file ArithDSS4.h
+ * @author Tristan Roussillon (\c tristan.roussillon@liris.cnrs.fr )
+ * Laboratoire d'InfoRmatique en Image et Systèmes d'information - LIRIS (CNRS, UMR 5205), CNRS, France
+ *
+ * @date 2010/07/02
+ *
+ * Header file for module ArithDSS4.cpp
+ *
+ * This file is part of the DGtal library.
+ */
+
+#if defined(ArithDSS4_RECURSES)
+#error Recursive header files inclusion detected in ArithDSS4.h
+#else // defined(ArithDSS4_RECURSES)
+/** Prevents recursive inclusion of headers. */
+#define ArithDSS4_RECURSES
+
+#if !defined ArithDSS4_h
+/** Prevents repeated inclusion of headers. */
+#define ArithDSS4_h
+
+//////////////////////////////////////////////////////////////////////////////
+// Inclusions
+#include <iostream>
+#include "DGtal/base/Common.h"
+#include "DGtal/geometry/2d/ArithDSS.h"
+#include <cmath>
+//////////////////////////////////////////////////////////////////////////////
+
+namespace DGtal
+{
+
+  /////////////////////////////////////////////////////////////////////////////
+  // template class ArithDSS4
+  /**
+   * Description of template class 'ArithDSS4' <p>
+   * \brief Aim:
+   */
+  template <typename Domain2D>
+  class ArithDSS4: public ArithDSS<Domain2D>
+  {
+    // ----------------------- Standard services ------------------------------
+  public:
+
+		typedef typename Domain2D::Coordinate Integer;
+		//2D point of a domain
+		typedef typename Domain2D::Point Point;
+		//2D vector of a domain
+		typedef typename Domain2D::Vector Vector;
+
+    /**
+     * Constructor.
+     */
+    ArithDSS4(const Point& aFirstPoint, const Point& aSecondPoint);
+
+
+    /**
+     * Destructor.
+     */
+    ~ArithDSS4();
+
+    // ----------------------- Interface --------------------------------------
+  public:
+
+
+    // ------------------------- Protected Datas ------------------------------
+  protected:
+    /**
+		 * Computes the norm of the two components
+     * of a 2D vector as the L1 norm
+		 * @param x and y, two values. 
+     * @return the L1 norm of a 2D vector.
+     */
+    Integer norm(const Integer  & x, const Integer & y) const;
+
+    // ------------------------- Private Datas --------------------------------
+  private:
+
+    // ------------------------- Hidden services ------------------------------
+  protected:
+
+
+  private:
+
+    /**
+     * Copy constructor.
+     * @param other the object to clone.
+     * Forbidden by default.
+     */
+    ArithDSS4 ( const ArithDSS4 & other );
+
+    /**
+     * Assignment.
+     * @param other the object to copy.
+     * @return a reference on 'this'.
+     * Forbidden by default.
+     */
+    ArithDSS4 & operator= ( const ArithDSS4 & other );
+
+    // ------------------------- Internals ------------------------------------
+  private:
+
+  }; // end of class ArithDSS4
+
+
+  /**
+   * Overloads 'operator<<' for displaying objects of class 'ArithDSS4'.
+   * @param out the output stream where the object is written.
+   * @param object the object of class 'ArithDSS4' to write.
+   * @return the output stream after the writing.
+   */
+  template <typename T>
+  std::ostream&
+  operator<< ( std::ostream & out, const ArithDSS4<T> & object );
+
+} // namespace DGtal
+
+
+///////////////////////////////////////////////////////////////////////////////
+// Includes inline functions.
+#include "DGtal/geometry/2d/ArithDSS4.ih"
+
+//                                                                           //
+///////////////////////////////////////////////////////////////////////////////
+
+#endif // !defined ArithDSS4_h
+
+#undef ArithDSS4_RECURSES
+#endif // else defined(ArithDSS4_RECURSES)
