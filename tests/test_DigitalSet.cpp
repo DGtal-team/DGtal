724485a797e58b13c9af3aa5779e60be8160bd5c
SebastienFourey <SebastienFourey@5422497e-619f-4381-bc00-4cdae604c9f6>
2010-07-02 09:50:38 +0000

    Templated tests for DigitalSet types
    git-svn-id: https://svn.liris.cnrs.fr/dgtal/trunk@254 5422497e-619f-4381-bc00-4cdae604c9f6

--- tests/test_DigitalSet.cpp
+++ tests/test_DigitalSet.cpp
@@ -1,7 +1,11 @@
 /**
- * @file test_HyperRectDomain.cpp
+ * @file test_DigitalSet.cpp
  * @author Jacques-Olivier Lachaud (\c jacques-olivier.lachaud@univ-savoie.fr )
  * Laboratory of Mathematics (CNRS, UMR 5807), University of Savoie, France
+
+ * @author Sebastien Fourey (\c Sebastien.Fourey@greyc.ensicaen.fr )
+ * Groupe de Recherche en Informatique, Image, Automatique et
+ * Instrumentation de Caen - GREYC (CNRS, UMR 6072), ENSICAEN, France
  *
  * @date 2010/07/01
  *
@@ -23,51 +27,34 @@
 #include "DGtal/kernel/SpaceND.h"
 #include "DGtal/kernel/domains/HyperRectDomain.h"
 #include "DGtal/kernel/sets/DigitalSetBySTLVector.h"
-// #include "DGtal/kernel/sets/DigitalSetBySTLSet.h"
-
+#include "DGtal/kernel/sets/DigitalSetBySTLSet.h"
 
 using namespace DGtal;
 using namespace std;
 
 
-/**
-* Test of DigitalSetBySTLVector.
-*
-**/
-bool testDigitalSetBySTLVector()
+template < typename DigitalSetType >
+bool testDigitalSet( const typename DigitalSetType::DomainType & domain )
 {
+  typedef typename DigitalSetType::DomainType DomainType;
+  typedef typename DomainType::Point Point;
   unsigned int nbok = 0;
   unsigned int nb = 0;
-  
-  typedef SpaceND<int,4> Space4Type;
-  typedef HyperRectDomain<Space4Type> DomainType;
-  typedef Space4Type::Point Point;
-  
-  Point a ( { 1, 2, 3 ,4} );
-  Point b ( { 5, 5, 3 ,5} );
-  Point p1( { 4, 3, 3 ,4} );
-  Point p2( { 2, 5, 3 ,5} );
-  Point p3( { 2, 5, 3 ,4} );
-  trace.beginBlock ( "HyperRectDomain init" );
-
-    typedef SpaceND<int,4> Space4Type;
-    typedef HyperRectDomain<Space4Type> DomainType;
-    typedef Space4Type::Point Point;
-  ///Domain characterized by points a and b
-  DomainType domain ( a,b );
-  trace.info() << domain << std::endl;
-  trace.info() << "Domain Extent= "<< domain.extent() << std::endl;
-  trace.endBlock();
  
-  trace.beginBlock ( "DigitalSetBySTLVector constructor." );
-  DigitalSetBySTLVector<DomainType> set1( domain );
+  trace.beginBlock ( "Constructor." );
+
+  DigitalSetType set1( domain );
   nbok += set1.size() == 0 ? 1 : 0; 
   nb++;
   trace.info() << "(" << nbok << "/" << nb << ") "
 	       << "Empty set: " << set1 << std::endl;
   trace.endBlock();
 
-  trace.beginBlock ( "DigitalSetBySTLVector insert." );
+  Point p1( { 4, 3, 3 ,4} );
+  Point p2( { 2, 5, 3 ,5} );
+  Point p3( { 2, 5, 3 ,4} );
+
+  trace.beginBlock ( "Insertion." );
   set1.insert( p1 );
   set1.insert( p2 );
   set1.insert( p3 );
@@ -81,60 +68,30 @@ bool testDigitalSetBySTLVector()
   return nbok == nb;
 }
 
-/**
-* Test of DigitalSetBySTLSet.
-*
-**/
-// bool testDigitalSetBySTLSet()
-// {
-//   unsigned int nbok = 0;
-//   unsigned int nb = 0;
-  
-//   typedef Space<int,4> Space4Type;
-//   typedef HyperRectDomain<Space4Type> DomainType;
-//   typedef Space4Type::Point Point;
-  
-//   Point a ( { 1, 2, 3 ,4} );
-//   Point b ( { 5, 5, 3 ,5} );
-//   Point p1( { 4, 3, 3 ,4} );
-//   Point p2( { 2, 5, 3 ,5} );
-//   Point p3( { 2, 5, 3 ,4} );
-//   trace.beginBlock ( "HyperRectDomain init" );
-
-//   ///Domain characterized by points a and b
-//   DomainType domain ( a,b );
-//   trace.info() << domain << std::endl;
-//   trace.info() << "Domain Extent= "<< domain.extent() << std::endl;
-//   trace.endBlock();
- 
-//   trace.beginBlock ( "DigitalSetBySTLVector constructor." );
-//   DigitalSetBySTLSet<DomainType> set1( domain );
-//   nbok += set1.size() == 0 ? 1 : 0; 
-//   nb++;
-//   trace.info() << "(" << nbok << "/" << nb << ") "
-// 	       << "Empty set: " << set1 << std::endl;
-//   trace.endBlock();
-
-//   trace.beginBlock ( "DigitalSetBySTLSet insert." );
-//   set1.insert( p1 );
-//   set1.insert( p2 );
-//   set1.insert( p3 );
-//   set1.insert( p2 );
-//   nbok += set1.size() == 3 ? 1 : 0; 
-//   nb++;
-//   trace.info() << "(" << nbok << "/" << nb << ") "
-// 	       << "Set (3 elements): " << set1 << std::endl;
-//   trace.endBlock();
-
-//   return nbok == nb;
-// }
-
 int main()
 {
-  if ( testDigitalSetBySTLVector()
-       /* && testDigitalSetBySTLVector() */ )
-    return 0;
-  else
-    return 1;
+  typedef SpaceND<int,4> Space4Type;
+  typedef HyperRectDomain<Space4Type> DomainType;
+  typedef Space4Type::Point Point;
+  
+  Point a ( { 1, 2, 3 ,4} );
+  Point b ( { 5, 5, 3 ,5} );
+  trace.beginBlock ( "HyperRectDomain init" );
+
+  ///Domain characterized by points a and b
+  DomainType domain ( a,b );
+  trace.info() << domain << std::endl;
+  trace.info() << "Domain Extent= "<< domain.extent() << std::endl;
+  trace.endBlock();
+  
+  trace.beginBlock( "DigitalSetBySTLVector" );
+  bool okVector = testDigitalSet< DigitalSetBySTLVector<DomainType> >( domain );
+  trace.endBlock();
+  
+  trace.beginBlock( "DigitalSetBySTLSet" );
+  bool okSet = testDigitalSet< DigitalSetBySTLSet<DomainType> >( domain );
+  trace.endBlock();
+
+  return !( okVector && okSet );
 }
 
