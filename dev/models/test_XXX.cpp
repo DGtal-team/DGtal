a889c558109907ecfab68bc01ef5ea682ddf2304
LaurentProvot <LaurentProvot@5422497e-619f-4381-bc00-4cdae604c9f6>
2010-07-03 12:05:03 +0000

    newtest.sh updated and test_XXX.cpp model added
    git-svn-id: https://svn.liris.cnrs.fr/dgtal/trunk@272 5422497e-619f-4381-bc00-4cdae604c9f6

---
+++ dev/models/test_XXX.cpp
@@ -0,0 +1,31 @@
+/**
+ * @file test_XXX.cpp
+ * @author AUTHOR (\c EMAIL )
+ * INSTITUTION
+ *
+ * @date 2000/??/??
+ *
+ * This file is part of the DGtal library.
+ */
+
+
+/**
+ * Description of test_XXX <p>
+ * Aim: 
+ */
+
+#include <iostream>
+
+#include "DGtal/base/Common.h"
+
+
+using namespace DGtal;
+using namespace std;
+
+
+int main()
+{
+
+
+  return 0;
+}
--- dev/scripts/newtest.sh
+++ dev/scripts/newtest.sh
@@ -9,27 +9,27 @@ fi
 SCRIPTS_DIR=${DGtal}/dev/scripts
 source ${SCRIPTS_DIR}/common.sh
 
-if test \( "$#" != "2" \);
+if test \( "$#" != "1" \);
 then 
     echo "usage: $0 <test_name>" ;
     echo "       - creates a C++ skeleton files (.cpp) designed to test"
-    echo "         for a class [test_name]. Tests are expected to be in"
-    echo "         directory of the form: tests/subdir/test[test_name]."
+    echo "         a class [test_name]. Tests are expected to be in"
+    echo "         directory of the form: ${TESTS_DIR}/test_[test_name]."
     exit 1
 fi
 
 
-if test -w "${TESTS_DIR}/test$1.cpp" ;
+if test -w "${TESTS_DIR}/test_$1.cpp" ;
 then
-    echo "File ${TESTS_DIR}/test$1.cpp exists and is writable. Please remove it before." ;
+    echo "File ${TESTS_DIR}/test_$1.cpp exists and is writable. Please remove it before." ;
     exit 2;
 fi
 
-echo "--- Creating file ${TESTS_DIR}/$2/test$1.cpp"
+echo "--- Creating file ${TESTS_DIR}/test_$1.cpp"
 
-if test "$#" = "3"; then namespace=$3; fi
-enspace="s@YYY@${namespace}@g"
-esubdir="s@ZZZ@$2@g"
+#if test "$#" = "3"; then namespace=$3; fi
+#enspace="s@YYY@${namespace}@g"
+#esubdir="s@ZZZ@$2@g"
 ename="s@XXX@$1@g"
 #etoday='s/2000\/??\/??/'`date '+20%y\/%m\/%d'`'/g'
 etoday='s@2000/??/??@'"${today}"'@g'
@@ -51,10 +51,17 @@ einstitution="s@INSTITUTION@${institution}@g"
 #     institution=${INSTITUTION}
 # fi
 
-if test ! -r "${MODELS_DIR}/TestXXX.cpp"; then
-    echo "Missing model TestXXX.cpp in ${MODELS_DIR}."
+if test ! -r "${MODELS_DIR}/test_XXX.cpp"; then
+    echo "Missing model test_XXX.cpp in ${MODELS_DIR}."
     exit 2
 fi
 
-cat "${MODELS_DIR}/TestXXX.cpp" | sed -e "${enspace}" -e "${esubdir}" -e "${ename}" -e "${etoday}" -e "${eauthor}" -e "${eemail}" -e "${einstitution}"  > "${TESTS_DIR}/$2/test$1.cpp"
+cat "${MODELS_DIR}/test_XXX.cpp" | sed -e "${ename}" -e "${etoday}" -e "${eauthor}" -e "${eemail}" -e "${einstitution}"  > "${TESTS_DIR}/test_$1.cpp"
+
+#TODO: Do we have to insert an entry for the test in CMakeLists.txt ?
+#      Here is a simple way to do it (but it does not check if the entry is
+#      already in the list of DGTAL_TESTS_SRC)
+#echo "--- Add test_$1 to CMakeLists.txt"
+#sed -i -e "s@SET(DGTAL_TESTS_SRC@SET(DGTAL_TESTS_SRC\n   test_$1@" "${TESTS_DIR}/CMakeLists.txt"
+
 echo "--> done."
