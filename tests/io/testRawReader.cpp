86cef8acfe9fb3716c2b1819937f5860fa684d5b
dcoeurjo <dcoeurjo@5422497e-619f-4381-bc00-4cdae604c9f6>
2010-08-09 09:15:42 +0000

    New ConfigTest and samples added. VolReader (ok) Raw8Reader (ok, todo: complete the test) Todo: check io errors more carefully
    git-svn-id: https://svn.liris.cnrs.fr/dgtal/trunk@395 5422497e-619f-4381-bc00-4cdae604c9f6

---
+++ tests/io/testRawReader.cpp
@@ -0,0 +1,98 @@
+/**
+ * @file testRawReader.cpp
+ * @ingroup Tests
+ * @author David Coeurjolly (\c david.coeurjolly@liris.cnrs.fr )
+ * Laboratoire d'InfoRmatique en Image et Systèmes d'information - LIRIS (CNRS, UMR 5205), CNRS, France
+ *
+ * @date 2010/07/29
+ *
+ * Functions for testing class RawReader.
+ *
+ * This file is part of the DGtal library.
+ */
+
+///////////////////////////////////////////////////////////////////////////////
+#include <iostream>
+#include "DGtal/base/Common.h"
+
+#include "DGtal/kernel/SpaceND.h"
+#include "DGtal/kernel/domains/HyperRectDomain.h"
+#include "DGtal/kernel/images/ImageSelector.h"
+#include "DGtal/io/readers/RawReader.h"
+#include "DGtal/io/colormaps/HueShadeColorMap.h"
+#include "DGtal/io/colormaps/GrayscaleColorMap.h"
+#include "DGtal/io/colormaps/GradientColorMap.h"
+#include "DGtal/io/colormaps/ColorBrightnessColorMap.h"
+
+#include "DGtal/io/writers/PNMWriter.h"
+
+#include "ConfigTest.h"
+
+
+///////////////////////////////////////////////////////////////////////////////
+
+using namespace std;
+using namespace DGtal;
+
+///////////////////////////////////////////////////////////////////////////////
+// Functions for testing class RawReader.
+///////////////////////////////////////////////////////////////////////////////
+/**
+ * Example of a test. To be completed.
+ *
+ */
+bool testRawReader()
+{
+  unsigned int nbok = 0;
+  unsigned int nb = 0;
+  
+  trace.beginBlock ( "Testing Raw reader ..." );
+  
+  typedef SpaceND<int,2> Space2Type;
+  typedef HyperRectDomain<Space2Type> TDomain;
+  typedef TDomain::Vector Vector;
+  
+  //Default image selector = STLVector
+  typedef ImageSelector<TDomain, unsigned char>::Type Image;
+  
+  std::string filename = testPath + "samples/raw2D-64x64.raw";
+  
+  Vector ext(16,16);
+  
+  Image image = RawReader<Image>::importRaw8( filename , ext);
+
+  ///FIXME: check io errors
+  trace.info() << image <<endl;
+
+  //export
+  typedef GrayscaleColorMap<unsigned char> Gray;  
+  PNMWriter<Image,Gray>::exportPGM("export-raw-reader.pgm",image,0,255);
+
+
+  nbok += true ? 1 : 0; 
+  nb++;
+  trace.info() << "(" << nbok << "/" << nb << ") "
+	       << "true == true" << std::endl;
+  trace.endBlock();
+  
+  return nbok == nb;
+}
+
+///////////////////////////////////////////////////////////////////////////////
+// Standard services - public :
+
+int main( int argc, char** argv )
+{
+  trace.beginBlock ( "Testing class RawReader" );
+  trace.info() << "Args:";
+  for ( int i = 0; i < argc; ++i )
+    trace.info() << " " << argv[ i ];
+  trace.info() << endl;
+
+  bool res = testRawReader(); // && ... other tests
+  trace.emphase() << ( res ? "Passed." : "Error." ) << endl;
+  trace.endBlock();
+  return res ? 0 : 1;
+}
+//                                                                           //
+///////////////////////////////////////////////////////////////////////////////
