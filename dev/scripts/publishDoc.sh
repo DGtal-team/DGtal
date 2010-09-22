07d2df238d4c6e6ea4162a71ca84765f26049b87
dcoeurjo <dcoeurjo@5422497e-619f-4381-bc00-4cdae604c9f6>
2010-09-22 20:02:30 +0000

    publishDoc.sh script added to rsync the documentation to http://liris.cnrs.fr/dgtal/doc
    git-svn-id: https://svn.liris.cnrs.fr/dgtal/trunk@440 5422497e-619f-4381-bc00-4cdae604c9f6

---
+++ dev/scripts/publishDoc.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+if ! test -d "${DGtal}"; then
+    echo "Environment variable DGtal is undefined."
+    exit 1
+fi
+
+rsync -azv --delete --delete-after ${DGtal}/doc/html/ dgtal@liris.cnrs.fr:/home/dgtal/public_html/doc
+
+echo "--> done."
