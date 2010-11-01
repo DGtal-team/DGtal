150f0aba1add07ac65249a8e28e5b9fef9773830
dcoeurjo <dcoeurjo@5422497e-619f-4381-bc00-4cdae604c9f6>
2010-11-01 14:35:33 +0000

    New exercises on images
    git-svn-id: https://svn.liris.cnrs.fr/dgtal/trunk@567 5422497e-619f-4381-bc00-4cdae604c9f6

---
+++ exercises/2_basic_images/a_imageinit.cpp
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <iostream>
+
+#include <DGtal/base/Common.h>
+
+
+int main(int argc, char **argv)
+{
+
+  /**
+   *
+   * Create an image (0,0)x(255,255) in which we can store small
+   * unsigned integers. 
+   *
+   */
+
+
+  /**
+   *
+   * In the previous image, set the value of pixels belonging to a
+   * disk of center (128,128) with radius 64 to 1 (and 0 otherwise).
+   *
+   */
+
+
+
+  /**
+   *
+   * With the trace mechanism, display the values of the vertical  1D
+   * slice with abscissa 110. 
+   * 
+   */
+
+  /**
+   *
+   * Create a SVG export of the image. 
+   *
+   */
+
+  return 0;
+}
