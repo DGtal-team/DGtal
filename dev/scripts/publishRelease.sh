0ef8e8eab718af43551c03bb6efa672b5416ad59
dcoeurjo <dcoeurjo@5422497e-619f-4381-bc00-4cdae604c9f6>
2010-11-26 13:00:09 +0000

    Release script
    git-svn-id: https://svn.liris.cnrs.fr/dgtal/trunk@626 5422497e-619f-4381-bc00-4cdae604c9f6

---
+++ dev/scripts/publishRelease.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+if ! test -d "${DGtal}"; then
+    echo "Environment variable DGtal is undefined."
+    exit 1
+fi
+
+rsync -azv --delete --delete-after ${DGtal}/build/DGtal*.tar.gz dgtal@liris.cnrs.fr:/home/dgtal/public_html/releases
+
+
+echo "--> done."
