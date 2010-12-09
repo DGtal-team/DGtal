7336169f4fb11522336a2db88a416d4b23e25289
dcoeurjo <dcoeurjo@5422497e-619f-4381-bc00-4cdae604c9f6>
2010-12-09 13:48:43 +0000

    VTKImageContainer -> experimental
    git-svn-id: https://svn.liris.cnrs.fr/dgtal/trunk@701 5422497e-619f-4381-bc00-4cdae604c9f6

--- src/DGtal/kernel/images/ImageContainerByVTKImageData.h
+++ src/DGtal/kernel/images/ImageContainerByVTKImageData.h
@@ -55,139 +55,141 @@ class vtkImageData;
 
 namespace DGtal
 {
-
-
-  /////////////////////////////////////////////////////////////////////////////
-  // template class ImageContainerByVTKImageData
-  /**
-   * Description of template class 'ImageContainerByVTKImageData' <p>
-   * \brief Aim:
-   */
-  template <typename TDomain, typename TValueType>
-  class ImageContainerByVTKImageData
+  namespace experimental
   {
-    // ----------------------- Standard services ------------------------------
-  public:
+
+    /////////////////////////////////////////////////////////////////////////////
+    // template class ImageContainerByVTKImageData
+    /**
+     * Description of template class 'ImageContainerByVTKImageData' <p>
+     * \brief Aim:
+     */
+    template <typename TDomain, typename TValueType>
+    class ImageContainerByVTKImageData
+    {
+      // ----------------------- Standard services ------------------------------
+    public:
  
-    BOOST_CONCEPT_ASSERT(( CValueType<TValueType> ));
-    BOOST_CONCEPT_ASSERT(( CDomain<TDomain> ));
+      BOOST_CONCEPT_ASSERT(( CValueType<TValueType> ));
+      BOOST_CONCEPT_ASSERT(( CDomain<TDomain> ));
 			
-    typedef TValueType ValueType;
-    typedef TDomain Domain;
+      typedef TValueType ValueType;
+      typedef TDomain Domain;
 
-    // static constants
-    static const typename Domain::Dimension staticDimension = Domain::staticDimension;
+      // static constants
+      static const typename Domain::Dimension staticDimension = Domain::staticDimension;
     
-    ///Only defined for 3D images
-    BOOST_STATIC_ASSERT(staticDimension == 3);
+      ///Only defined for 3D images
+      BOOST_STATIC_ASSERT(staticDimension == 3);
     
-    ///@todo IMPORTANT : Size must be int-compatible (see operator()(Point))
+      ///@todo IMPORTANT : Size must be int-compatible (see operator()(Point))
 
-    typedef typename Domain::Point Point;
-    typedef typename Domain::Vector Vector;
-    typedef typename Domain::Dimension Dimension;
-    typedef typename Domain::Integer Integer;
-    typedef typename Domain::Size Size;
-    //    typedef typename vector<ValueType>::iterator Iterator;
-    //    typedef typename vector<ValueType>::const_iterator ConstIterator;
+      typedef typename Domain::Point Point;
+      typedef typename Domain::Vector Vector;
+      typedef typename Domain::Dimension Dimension;
+      typedef typename Domain::Integer Integer;
+      typedef typename Domain::Size Size;
+      //    typedef typename vector<ValueType>::iterator Iterator;
+      //    typedef typename vector<ValueType>::const_iterator ConstIterator;
 
-    /**
-     * Constructor.
-     */
-    ImageContainerByVTKImageData(const Point &aPointA,
-				 const Point &aPointB );
+      /**
+       * Constructor.
+       */
+      ImageContainerByVTKImageData(const Point &aPointA,
+				   const Point &aPointB );
 
     
-    /**
-     * Destructor.
-     */
-    ~ImageContainerByVTKImageData();
+      /**
+       * Destructor.
+       */
+      ~ImageContainerByVTKImageData();
 
-    // ----------------------- Interface --------------------------------------
-  public:
+      // ----------------------- Interface --------------------------------------
+    public:
 
-    /**
-     * Get the value of an image at a given position.
-     *
-     * @param aPoint  position in the image.
-     * @return the value at aPoint.
-     */
-    ValueType operator()(const Point &aPoint) const;
+      /**
+       * Get the value of an image at a given position.
+       *
+       * @param aPoint  position in the image.
+       * @return the value at aPoint.
+       */
+      ValueType operator()(const Point &aPoint) const;
     
    
-    /**
-     * Set a value on an Image at aPoint.
-     *
-     * @param aPoint location of the point to associate with aValue.
-     * @param aValue the value.
-     */
-    void setValue(const Point &aPoint, const ValueType &aValue);
+      /**
+       * Set a value on an Image at aPoint.
+       *
+       * @param aPoint location of the point to associate with aValue.
+       * @param aValue the value.
+       */
+      void setValue(const Point &aPoint, const ValueType &aValue);
 
 
-    /**
-     * Writes/Displays the object on an output stream.
-     * @param out the output stream where the object is written.
-     */
-    void selfDisplay ( std::ostream & out ) const;
+      /**
+       * Writes/Displays the object on an output stream.
+       * @param out the output stream where the object is written.
+       */
+      void selfDisplay ( std::ostream & out ) const;
 
-    /**
-     * Checks the validity/consistency of the object.
-     * @return 'true' if the object is valid, 'false' otherwise.
-     */
-    bool isValid() const;
+      /**
+       * Checks the validity/consistency of the object.
+       * @return 'true' if the object is valid, 'false' otherwise.
+       */
+      bool isValid() const;
 
-    // ------------------------- Protected Datas ------------------------------
-  private:
-    // ------------------------- Private Datas --------------------------------
-  private:
+      // ------------------------- Protected Datas ------------------------------
+    private:
+      // ------------------------- Private Datas --------------------------------
+    private:
 
-    // ------------------------- Hidden services ------------------------------
-  protected:
+      // ------------------------- Hidden services ------------------------------
+    protected:
 
-    /**
-     * Constructor.
-     * Forbidden by default (protected to avoid g++ warnings).
-     */
-    ImageContainerByVTKImageData();
+      /**
+       * Constructor.
+       * Forbidden by default (protected to avoid g++ warnings).
+       */
+      ImageContainerByVTKImageData();
 
-  private:
+    private:
 
-    /**
-     * Copy constructor.
-     * @param other the object to clone.
-     * Forbidden by default.
-     */
-    ImageContainerByVTKImageData ( const ImageContainerByVTKImageData & other );
+      /**
+       * Copy constructor.
+       * @param other the object to clone.
+       * Forbidden by default.
+       */
+      ImageContainerByVTKImageData ( const ImageContainerByVTKImageData & other );
 
-    /**
-     * Assignment.
-     * @param other the object to copy.
-     * @return a reference on 'this'.
-     * Forbidden by default.
-     */
-    ImageContainerByVTKImageData & operator= ( const ImageContainerByVTKImageData & other );
+      /**
+       * Assignment.
+       * @param other the object to copy.
+       * @return a reference on 'this'.
+       * Forbidden by default.
+       */
+      ImageContainerByVTKImageData & operator= ( const ImageContainerByVTKImageData & other );
 
-    // ------------------------- Internals ------------------------------------
-  private:
+      // ------------------------- Internals ------------------------------------
+    private:
 
-    Point myLowerBound;
-    Point myUpperBound;
-    vtkImageData *myVTKImage;
+      Point myLowerBound;
+      Point myUpperBound;
+      vtkImageData *myVTKImage;
 
-  }; // end of class ImageContainerByVTKImageData
+    }; // end of class ImageContainerByVTKImageData
 
 
-  /**
-   * Overloads 'operator<<' for displaying objects of class 'ImageContainerByVTKImageData'.
-   * @param out the output stream where the object is written.
-   * @param object the object of class 'ImageContainerByVTKImageData' to write.
-   * @return the output stream after the writing.
-   */
-  template <typename TD, typename TV>
-  std::ostream&
-  operator<< ( std::ostream & out, const ImageContainerByVTKImageData<TD,TV> & object );
+  } // namespace exerimental
+}// namespace DGtal
 
-} // namespace DGtal
+/**
+ * Overloads 'operator<<' for displaying objects of class 'ImageContainerByVTKImageData'.
+ * @param out the output stream where the object is written.
+ * @param object the object of class 'ImageContainerByVTKImageData' to write.
+ * @return the output stream after the writing.
+ */
+template <typename TD, typename TV>
+std::ostream&
+operator<< ( std::ostream & out, const DGtal::experimental::ImageContainerByVTKImageData<TD,TV> & object );
 
 
 ///////////////////////////////////////////////////////////////////////////////
