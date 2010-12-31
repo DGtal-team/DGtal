facd83c2242497f0e15a9ed2bdb96f3a6baed9bd
dcoeurjo <dcoeurjo@5422497e-619f-4381-bc00-4cdae604c9f6>
2010-12-31 08:14:25 +0000

    Missing files added.
    git-svn-id: https://svn.liris.cnrs.fr/dgtal/trunk@730 5422497e-619f-4381-bc00-4cdae604c9f6

---
+++ exercises/1_basic_setup/b_pointvectors.cpp
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <iostream>
+
+#include <DGtal/base/Common.h>
+
+
+int main(int argc, char **argv)
+{
+
+  /**
+   * Create two vectors of "double" in a 6D space.
+   */
+
+
+  /**
+   * Init these two vectors with random values.
+   */
+
+
+  /**
+   * Compute the sum of them to a third vector,.
+   */
+
+
+  /**
+   * Compute the maximum value of the sum.
+   */
+  
+
+  /**
+   * Display the vector values to the standart output.
+   */
+  
+
+  return 0;
+}
