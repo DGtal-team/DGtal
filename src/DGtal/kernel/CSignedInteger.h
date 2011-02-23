7e31f347f6565d9c005eb4d2b2304273cc804cb4
dcoeurjo <dcoeurjo@5422497e-619f-4381-bc00-4cdae604c9f6>
2011-02-23 23:30:41 +0000

    Removing std::size_t
    git-svn-id: https://svn.liris.cnrs.fr/dgtal/branches/IntegerRefactoringClean@891 5422497e-619f-4381-bc00-4cdae604c9f6

---
+++ src/DGtal/kernel/CSignedInteger.h
@@ -0,0 +1,121 @@
+/**
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU Lesser General Public License as
+ *  published by the Free Software Foundation, either version 3 of the
+ *  License, or  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ **/
+
+#pragma once
+
+/**
+ * @file CSignedInteger.h
+ * @author Jacques-Olivier Lachaud (\c jacques-olivier.lachaud@univ-savoie.fr )
+ * Laboratory of Mathematics (CNRS, UMR 5807), University of Savoie, France
+ * @author David Coeurjolly (\c david.coeurjolly@liris.cnrs.fr )
+ * Laboratoire d'InfoRmatique en Image et Systèmes d'information - LIRIS (CNRS, UMR 5205), CNRS, France
+ *
+ * @date 2010/07/02
+ *
+ * Header file for concept CSignedInteger.cpp
+ *
+ * This file is part of the DGtal library.
+ */
+
+#if defined(CSignedInteger_RECURSES)
+#error Recursive header files inclusion detected in CSignedInteger.h
+#else // defined(CSignedInteger_RECURSES)
+/** Prevents recursive inclusion of headers. */
+#define CSignedInteger_RECURSES
+
+#if !defined CSignedInteger_h
+/** Prevents repeated inclusion of headers. */
+#define CSignedInteger_h
+
+//////////////////////////////////////////////////////////////////////////////
+// Inclusions
+#include <iostream>
+#include "boost/concept_check.hpp"
+#include "DGtal/base/Common.h"
+#include "DGtal/utils/ConceptUtils.h"
+#include "DGtal/kernel/CInteger.h"
+#include "DGtal/kernel/IntegerTraits.h"
+//////////////////////////////////////////////////////////////////////////////
+
+namespace DGtal
+{
+
+  /////////////////////////////////////////////////////////////////////////////
+  // class CSignedInteger
+  /**
+   * Description of \b concept '\b CSignedInteger' <p>
+   * @ingroup Concepts
+   * Aim: Concept checking for Signed Integer. 
+   * 
+   * <p> Refinement of CInteger
+   *
+   *
+   * <p> Associated types :
+   *
+   * <p> Notation
+   * - \t X : A type that is a model of CSignedInteger
+   * - \t x, \t y	: Object of type X
+   *
+   * <p> Definitions
+   *
+   * <p> Valid expressions and semantics <br>
+   * <table> <tr> <td> \b Name </td> <td> \b Expression </td>
+   * <td> \b Type requirements </td> <td> \b Return type </td>
+   * <td> \b Precondition </td> <td> \b Semantics </td> 
+   * <td> \b Postcondition </td> <td> \b Complexity </td>
+   * </tr>
+   * <tr> 
+   * <td> </td> <td> </td> <td> </td> <td> </td>
+   * <td> </td> <td> </td> <td> </td> <td> </td>
+   * </tr>
+   * </table>
+   *
+   * <p> Invariants <br>
+   *
+   * <p> Models <br>
+   *
+   * <p> Notes <br>
+   */
+  template <typename T>
+  struct CSignedInteger: CInteger<T>
+  {
+    // ----------------------- Concept checks ------------------------------
+  public:
+    BOOST_CONCEPT_USAGE(CSignedInteger)
+    {
+      // Will compile iff Signed.
+      ConceptUtils::checkTrue( myIsSigned );
+    }
+    
+    // ------------------------- Private Datas --------------------------------
+  private:
+    
+    // ------------------------- Internals ------------------------------------
+  private:
+    typename IntegerTraits<T>::IsSigned myIsSigned;
+
+  }; // end of concept CSignedInteger
+  
+} // namespace DGtal
+
+
+//                                                                           //
+///////////////////////////////////////////////////////////////////////////////
+
+#endif // !defined CSignedInteger_h
+
+#undef CSignedInteger_RECURSES
+#endif // else defined(CSignedInteger_RECURSES)
