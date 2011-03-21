fe60f93e54b557cea0a7592994f8b0a5303d47d3
BertrandKerautret <BertrandKerautret@5422497e-619f-4381-bc00-4cdae604c9f6>
2011-03-21 22:53:28 +0000

    correcting beug Ticket #82
    git-svn-id: https://svn.liris.cnrs.fr/dgtal/trunk@1080 5422497e-619f-4381-bc00-4cdae604c9f6

--- cmake/CheckDGtalDependencies.cmake
+++ cmake/CheckDGtalDependencies.cmake
@@ -119,10 +119,9 @@ endif ( QGLVIEWER_FOUND  AND QT4_FOUND AND QT_QTOPENGL_FOUND)
 if( (NOT WITH_VISU3D_QGLVIEWER)  AND (NOT WITH_VISU3D_IV) )
   message(STATUS  "(optional) No 3D visualisation possible  (QGLViewer and IV )." )
   set( WITH_VISU3D FALSE)
-endif( (NOT WITH_VISU3D_QGLVIEWER)  AND (NOT WITH_VISU3D_IV) )
-else()
+else ()
   set( WITH_VISU3D TRUE )
-endif
+endif( (NOT WITH_VISU3D_QGLVIEWER)  AND (NOT WITH_VISU3D_IV) )
 
 
  
