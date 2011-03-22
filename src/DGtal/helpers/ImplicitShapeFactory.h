84906ab4aaab4c00aa8abf923a741840136233fd
dcoeurjo <dcoeurjo@5422497e-619f-4381-bc00-4cdae604c9f6>
2011-03-22 22:48:49 +0000

    Implicit function shaper and implicit shapes added
    git-svn-id: https://svn.liris.cnrs.fr/dgtal/trunk@1092 5422497e-619f-4381-bc00-4cdae604c9f6

---
+++ src/DGtal/helpers/ImplicitShapeFactory.h
@@ -0,0 +1,70 @@
+/**
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU Lesser General Public License as
+ *  published by the Free Software Foundation, either version 3 of the
+ *  License, or  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ **/
+
+#pragma once
+
+/**
+ * @file ImplicitShapeFactory.h
+ * @author David Coeurjolly (\c david.coeurjolly@liris.cnrs.fr )
+ * Laboratoire d'InfoRmatique en Image et Systèmes d'information - LIRIS (CNRS, UMR 5205), CNRS, France
+ *
+ * @date 2011/03/22
+ *
+ * Header file for module ImplicitShapeFactory.cpp
+ *
+ * This file is part of the DGtal library.
+ */
+
+#if defined(ImplicitShapeFactory_RECURSES)
+#error Recursive header files inclusion detected in ImplicitShapeFactory.h
+#else // defined(ImplicitShapeFactory_RECURSES)
+/** Prevents recursive inclusion of headers. */
+#define ImplicitShapeFactory_RECURSES
+
+#if !defined ImplicitShapeFactory_h
+/** Prevents repeated inclusion of headers. */
+#define ImplicitShapeFactory_h
+
+//////////////////////////////////////////////////////////////////////////////
+// Inclusions
+#include <iostream>
+#include "DGtal/base/Common.h"
+#include "DGtal/helpers/implicitShapes/ImplicitBall.h"
+#include "DGtal/helpers/implicitShapes/ImplicitNorm1Ball.h"
+#include "DGtal/helpers/implicitShapes/ImplicitHyperCube.h"
+#include "DGtal/helpers/implicitShapes/ImplicitRoundedHyperCube.h"
+//////////////////////////////////////////////////////////////////////////////
+
+namespace DGtal
+{
+
+  /////////////////////////////////////////////////////////////////////////////
+  // template class ImplicitShapeFactory
+  /**
+   * Description of template class 'ImplicitShapeFactory' <p>
+   * \brief Aim:
+   */
+  
+  
+
+} // namespace DGtal
+//                                                                           //
+///////////////////////////////////////////////////////////////////////////////
+
+#endif // !defined ImplicitShapeFactory_h
+
+#undef ImplicitShapeFactory_RECURSES
+#endif // else defined(ImplicitShapeFactory_RECURSES)
---
+++ src/DGtal/helpers/implicitShapes/ImplicitRoundedHyperCube.h
@@ -0,0 +1,206 @@
+/**
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU Lesser General Public License as
+ *  published by the Free Software Foundation, either version 3 of the
+ *  License, or  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ **/
+
+#pragma once
+
+/**
+ * @file ImplicitRoundedHyperCube.h
+ * @author David Coeurjolly (\c david.coeurjolly@liris.cnrs.fr )
+ * Laboratoire d'InfoRmatique en Image et Systèmes d'information - LIRIS (CNRS, UMR 5205), CNRS, France
+ *
+ * @date 2011/03/22
+ *
+ * Header file for module ImplicitRoundedHyperCube.cpp
+ *
+ * This file is part of the DGtal library.
+ */
+
+#if defined(ImplicitRoundedHyperCube_RECURSES)
+#error Recursive header files inclusion detected in ImplicitRoundedHyperCube.h
+#else // defined(ImplicitRoundedHyperCube_RECURSES)
+/** Prevents recursive inclusion of headers. */
+#define ImplicitRoundedHyperCube_RECURSES
+
+#if !defined ImplicitRoundedHyperCube_h
+/** Prevents repeated inclusion of headers. */
+#define ImplicitRoundedHyperCube_h
+
+//////////////////////////////////////////////////////////////////////////////
+// Inclusions
+#include <iostream>
+#include "DGtal/base/Common.h"
+//////////////////////////////////////////////////////////////////////////////
+
+namespace DGtal
+{
+
+  /////////////////////////////////////////////////////////////////////////////
+  template <typename TSpace>
+  class ImplicitRoundedHyperCube
+  {
+
+  public:
+    typedef TSpace Space;
+    typedef typename Space::Point Point;
+    typedef typename Space::Integer Integer;
+    
+   
+    /** 
+     * Constructor. Contructs a ball with center aCenter and width
+     * aWidth.
+     * 
+     * @param aCenter the cube center. 
+     * @param aHalfWidth the cube half-width.
+     */
+    ImplicitRoundedHyperCube(const Point &aCenter,
+			     const Integer &aHalfWidth,
+			     const double aPower): 
+      myCenter(aCenter),
+      myHalfWidth(aHalfWidth),
+      myPower(aPower)
+    {};
+    
+    /** 
+     * Destructor.
+     * 
+     */    
+    ~ImplicitRoundedHyperCube();
+
+
+    // ----------------------- Interface --------------------------------------
+  public:
+    
+    /** 
+     * Operator() of the implicit function. Given a point, it returns
+     * the function value at p. In Shapes, positive values are used to
+     * construct a set.
+     * 
+     * @param aPoint the point to evalute the function at.
+     * @return the distance of aPoint to the ball center.
+     */
+    inline
+    double operator()(const Point &aPoint) const
+    {
+      Point dec = (aPoint - myCenter);
+      double partialpower=0;
+      for(Dimension i = 0; i < Point::dimension; ++i)
+	partialpower +=  std::pow(std::abs(IntegerTraits<typename Point::Coordinate>::castToDouble(dec[i])), 
+				  myPower);
+      
+      return std::pow(IntegerTraits<Integer>::castToDouble(myHalfWidth), myPower) - 
+	partialpower;      
+    }
+
+    /** 
+     * Returns the lower bound of the Shape bounding box.
+     * 
+     * 
+     * @return the lower bound point.
+     */
+    inline
+    Point getLowerBound() const
+    {
+      return (myCenter - Point::diagonal(myHalfWidth));
+    }
+    
+    /** 
+     * Returns the upper bound of the Shape bounding box.
+     * 
+     * 
+     * @return the upper bound point.
+     */
+    inline
+    Point getUpperBound() const
+    {
+      return (myCenter + Point::diagonal(myHalfWidth)); 
+    }
+    
+    // ----------------------- Interface --------------------------------------
+  public:
+    
+    /**
+     * Writes/Displays the object on an output stream.
+     * @param out the output stream where the object is written.
+     */
+    void selfDisplay ( std::ostream & out ) const;
+
+    /**
+     * Checks the validity/consistency of the object.
+     * @return 'true' if the object is valid, 'false' otherwise.
+     */
+    bool isValid() const;
+
+    // ------------------------- Protected Datas ------------------------------
+  private:
+    // ------------------------- Private Datas --------------------------------
+  private:
+   
+    ///Cube center
+    Point myCenter;
+
+    ///Cube HalfWidth
+    Integer myHalfWidth;
+
+    ///Cube Power 
+    double myPower;
+   
+    // ------------------------- Hidden services ------------------------------
+  protected:
+
+    /**
+     * Constructor.
+     * Forbidden by default (protected to avoid g++ warnings).
+     */
+    ImplicitRoundedHyperCube();
+
+  private:
+
+    /**
+     * Assignment.
+     * @param other the object to copy.
+     * @return a reference on 'this'.
+     * Forbidden by default.
+     */
+    ImplicitRoundedHyperCube & operator= ( const ImplicitRoundedHyperCube & other );
+    
+    
+  }; // end of class ImplicitRoundedHyperCube
+
+
+  /**
+   * Overloads 'operator<<' for displaying objects of class 'ImplicitRoundedHyperCube'.
+   * @param out the output stream where the object is written.
+   * @param object the object of class 'ImplicitRoundedHyperCube' to write.
+   * @return the output stream after the writing.
+   */
+  template <typename T>
+  std::ostream&
+  operator<< ( std::ostream & out, const ImplicitRoundedHyperCube<T> & object );
+
+} // namespace DGtal
+
+
+///////////////////////////////////////////////////////////////////////////////
+// Includes inline functions.
+#include "DGtal/helpers/implicitShapes/ImplicitRoundedHyperCube.ih"
+
+//                                                                           //
+///////////////////////////////////////////////////////////////////////////////
+
+#endif // !defined ImplicitRoundedHyperCube_h
+
+#undef ImplicitRoundedHyperCube_RECURSES
+#endif // else defined(ImplicitRoundedHyperCube_RECURSES)
