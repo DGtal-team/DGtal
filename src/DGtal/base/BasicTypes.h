caa8acac0be8384aca337945959b4ea702992b5c
dcoeurjo <dcoeurjo@5422497e-619f-4381-bc00-4cdae604c9f6>
2011-03-22 16:42:18 +0000

    Working on REDT (new interface)
    git-svn-id: https://svn.liris.cnrs.fr/dgtal/trunk@1086 5422497e-619f-4381-bc00-4cdae604c9f6

--- src/DGtal/base/BasicTypes.h
+++ src/DGtal/base/BasicTypes.h
@@ -53,10 +53,12 @@
 namespace DGtal
 {
   
+  typedef boost::uint8_t uint8_t;
   typedef boost::uint16_t uint16_t;
   typedef boost::uint32_t uint32_t;
   typedef boost::uint64_t uint64_t;
   
+  typedef boost::int8_t int8_t;
   typedef boost::int16_t int16_t;
   typedef boost::int32_t int32_t;
   typedef boost::int64_t int64_t;
