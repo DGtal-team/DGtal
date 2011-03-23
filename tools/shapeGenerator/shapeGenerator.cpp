ee01a8bebebc7df5e0dd591dacd94cb853186a44
dcoeurjo <dcoeurjo@5422497e-619f-4381-bc00-4cdae604c9f6>
2011-03-23 18:06:10 +0000

    shapegenerator added (WIP)
    git-svn-id: https://svn.liris.cnrs.fr/dgtal/trunk@1103 5422497e-619f-4381-bc00-4cdae604c9f6

---
+++ tools/shapeGenerator/shapeGenerator.cpp
@@ -0,0 +1,70 @@
+/**
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU Lesser General Public License as
+ *  published by the Free Software Foundation, either version 3 of the
+ *  License, or  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ **/
+/**
+ * @file visuDistanceTransform.cpp
+ * @ingroup Tools
+ * @author David Coeurjolly (\c david.coeurjolly@liris.cnrs.fr)
+ * LIRIS (CNRS, UMR 5205), 
+ *
+ * @date 2011/01/04
+ *
+ * DGtal shape generator
+ *
+ * This file is part of the DGtal library.
+ */
+
+///////////////////////////////////////////////////////////////////////////////
+#include <iostream>
+
+#include "DGtal/base/Common.h"
+#include "DGtal/io/writers/VolWriter.h"
+#include "DGtal/io/writers/RawWriter.h"
+#include "DGtal/io/writers/PNMWriter.h"
+
+
+#include <boost/program_options/options_description.hpp>
+#include <boost/program_options/parsers.hpp>
+#include <boost/program_options/variables_map.hpp>
+
+using namespace DGtal;
+using namespace Z3i;
+using namespace Z2i;
+
+///////////////////////////////////////////////////////////////////////////////
+namespace po = boost::program_options;
+
+int main( int argc, char** argv )
+{
+  // parse command line ----------------------------------------------
+  po::options_description general_opt("Allowed options are: ");
+  general_opt.add_options()
+    ("help,h", "display this message")
+    ("shape,i", po::value<std::string>(), "Shape type")
+    ("list,l",  "List all available shapes")
+    ("radius,r",  po::value<int>()->default_value(0), "Radius or Halfwidth of the shape" );
+  
+  po::variables_map vm;
+  po::store(po::parse_command_line(argc, argv, general_opt), vm);  
+  po::notify(vm);    
+  if(vm.count("help")||argc<=1)
+    {
+      std::cout << "Usage: " <<  " \n"
+		<< "Generate shapes using DGtal library"
+		<< general_opt << "\n";
+      return 0;
+    }
+
+}
