f10cd32806268ff787d7d863ca6e257ce1cfd82f
BertrandKerautret <BertrandKerautret@5422497e-619f-4381-bc00-4cdae604c9f6>
2011-04-01 11:53:53 +0000

    Adding PointListReader
    git-svn-id: https://svn.liris.cnrs.fr/dgtal/trunk@1182 5422497e-619f-4381-bc00-4cdae604c9f6

---
+++ tests/io/testPointListReader.cpp
@@ -0,0 +1,96 @@
+/**
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU Lesser General Public License as
+ *  published by the Free Software Foundation, either version 3 of the
+ *  License, or  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ **/
+
+/**
+ * @file testPointListReader.cpp
+ * @ingroup Tests
+ * @author Bertrand Kerautret (\c kerautre@loria.fr )
+ * LORIA (CNRS, UMR 7503), University of Nancy, France
+ *
+ * @date 2011/04/01
+ *
+ * Functions for testing class PointListReader.
+ *
+ * This file is part of the DGtal library.
+ */
+
+///////////////////////////////////////////////////////////////////////////////
+#include <iostream>
+#include <fstream>
+#include "DGtal/base/Common.h"
+#include "DGtal/io/Readers/PointListReader.h"
+#include "ConfigTest.h"
+#include "DGtal/helpers/StdDefs.h"
+
+///////////////////////////////////////////////////////////////////////////////
+
+using namespace std;
+using namespace DGtal;
+
+
+///////////////////////////////////////////////////////////////////////////////
+// Functions for testing class PointListReader.
+///////////////////////////////////////////////////////////////////////////////
+/**
+ * Example of a test. To be completed.
+ *
+ */
+bool testPointListReader()
+{
+  unsigned int nbok = 0;
+  unsigned int nb = 0;
+  
+  trace.beginBlock ( "Testing block ..." );
+  nbok += true ? 1 : 0; 
+  nb++;
+  std::string filename = testPath + "samples/pointList1.pl";
+  ifstream infile;
+  infile.open (filename.c_str(), ifstream::in);
+  std::vector<uint> vectPos;
+  vectPos.push_back(1);
+  vectPos.push_back(2);
+  vector<Z2i::Point> vectPoints = PointListReader<Z2i::Point>::getPointsFromInputStream(infile,
+											vectPos);
+  for(uint k=0;k < vectPoints.size(); k++){
+    trace.info() << " pt: "<< vectPoints.at(k)<< endl;
+  }
+  
+  trace.info() << "(" << nbok << "/" << nb << ") "
+	       << "true == true" << std::endl;
+  trace.endBlock();
+  
+  return nbok == nb;
+}
+
+///////////////////////////////////////////////////////////////////////////////
+// Standard services - public :
+
+int main( int argc, char** argv )
+{
+  trace.beginBlock ( "Testing class PointListReader" );
+  trace.info() << "Args:";
+  for ( int i = 0; i < argc; ++i )
+    trace.info() << " " << argv[ i ];
+  trace.info() << endl;
+  
+  
+  bool res = testPointListReader(); // && ... other tests
+  trace.emphase() << ( res ? "Passed." : "Error." ) << endl;
+  trace.endBlock();
+  return res ? 0 : 1;
+}
+//                                                                           //
+///////////////////////////////////////////////////////////////////////////////
