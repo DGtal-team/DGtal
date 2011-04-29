334530620a4cc8b18b69bbe9efa1cfe5ddac70a7
BertrandKerautret <BertrandKerautret@5422497e-619f-4381-bc00-4cdae604c9f6>
2011-04-29 13:21:57 +0000

    Adding PNMReader (only in 2D with grayscale format: P5)
    git-svn-id: https://svn.liris.cnrs.fr/dgtal/trunk@1295 5422497e-619f-4381-bc00-4cdae604c9f6

---
+++ tests/io/testPNMReader.cpp
@@ -0,0 +1,92 @@
+/**
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU Lesser General Public License as
+ *  published by the Free Software Foundation, either version 3 of the
+ *  License, or  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ **/
+
+/**
+ * @file testPNMReader.cpp
+ * @ingroup Tests
+ * @author Bertrand Kerautret (\c kerautre@loria.fr )
+ * LORIA (CNRS, UMR 7503), University of Nancy, France
+ *
+ * @date 2011/04/29
+ *
+ * Functions for testing class PNMReader.
+ *
+ * This file is part of the DGtal library.
+ */
+
+///////////////////////////////////////////////////////////////////////////////
+#include <iostream>
+#include "DGtal/base/Common.h"
+#include "DGtal/io/readers/PNMReader.h"
+#include "DGtal/kernel/images/ImageSelector.h"
+#include "DGtal/helpers/StdDefs.h"
+#include "DGtal/kernel/imagesSetsUtils/SetFromImage.h"
+#include "ConfigTest.h"
+
+///////////////////////////////////////////////////////////////////////////////
+
+using namespace std;
+using namespace DGtal;
+
+///////////////////////////////////////////////////////////////////////////////
+// Functions for testing class PNMReader.
+///////////////////////////////////////////////////////////////////////////////
+/**
+ * Example of a test. To be completed.
+ *
+ */
+bool testPNMReader()
+{
+  unsigned int nbok = 0;
+  unsigned int nb = 0;  
+  trace.beginBlock ( "Testing block ..." );
+  nbok += true ? 1 : 0; 
+  nb++;
+  std::string filename = testPath + "samples/circleR10.pgm";
+  typedef ImageSelector < Z2i::Domain, uint>::Type Image;
+  Image image = PNMReader<Image>::importPGMImage( filename ); 
+  
+  Z2i::DigitalSet set2d (image.domain());
+  SetFromImage<Z2i::DigitalSet>::append<Image>(set2d, image, 0, 255);
+   
+  DGtalBoard board;
+  board << image.domain() << set2d; // display domain and set
+  
+  board.saveEPS( "testPNMReader.eps");
+  trace.info() << "(" << nbok << "/" << nb << ") "
+	       << "true == true" << std::endl;
+  trace.endBlock();  
+  return nbok == nb;
+}
+
+///////////////////////////////////////////////////////////////////////////////
+// Standard services - public :
+
+int main( int argc, char** argv )
+{
+  trace.beginBlock ( "Testing class PNMReader" );
+  trace.info() << "Args:";
+  for ( int i = 0; i < argc; ++i )
+    trace.info() << " " << argv[ i ];
+  trace.info() << endl;
+
+  bool res = testPNMReader(); // && ... other tests
+  trace.emphase() << ( res ? "Passed." : "Error." ) << endl;
+  trace.endBlock();
+  return res ? 0 : 1;
+}
+//                                                                           //
+///////////////////////////////////////////////////////////////////////////////
