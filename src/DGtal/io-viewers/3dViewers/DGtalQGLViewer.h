33fdbad8c9165edfe012e9b6a6a68cdcc7b6867b
BKerautret <kerautre@loria.fr>
2011-05-14 22:01:15 +0200

    Correcting normal surfel in KS Cell

--- src/DGtal/io-viewers/3dViewers/DGtalQGLViewer.h
+++ src/DGtal/io-viewers/3dViewers/DGtalQGLViewer.h
@@ -212,7 +212,7 @@ public:
 
   
   void addKSSurfel(double x1, double y1, double z1,  double x2, double y2, double z2,
-		   double x3, double y3, double z3,  double x4, double y4, double z4, QColor aColor=QColor(20,200,200,255));
+		   double x3, double y3, double z3,  double x4, double y4, double z4, QColor aColor=QColor(180,180,250,255));
   
   void addKSPointel(double x, double y, double z, double size=0.1, const QColor &color=QColor(200,20,20,255));
   void addKSLinel(double x1, double y1, double z1,
