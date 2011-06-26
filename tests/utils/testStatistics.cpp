be1d26de3c5f4f0ee09b68c4c8e3a77e759109ea
BKerautret <kerautre@loria.fr>
2011-06-27 00:23:41 +0200

    Complementary test in Statistics

--- tests/utils/testStatistics.cpp
+++ tests/utils/testStatistics.cpp
@@ -53,7 +53,6 @@ bool testStatistics()
 
   Statistic<double> stat;
   
-  
   for(unsigned int k=0; k < 1000; k++)
     stat.addValue((double)k);
   
@@ -66,6 +65,45 @@ bool testStatistics()
   nbok += (stat.max()==999) ? 1 : 0; 
   trace.info() << "Min value = "<<stat.min()<<std::endl;
   nbok += (stat.min()==0)  ? 1 : 0; 
+ 
+  trace.info() << "(" << nbok << "/" << nb << ") "
+	       << "true == true" << std::endl;
+  trace.endBlock();
+  
+  return nbok == nb;
+}
+
+/**
+ * Example of a test. To be completed.
+ *
+ */
+bool testStatisticsSaving()
+{
+  unsigned int nbok = 0;
+  unsigned int nb = 4;
+  
+  trace.beginBlock ( "Testing Statistics with saving option ..." );
+
+  Statistic<double> stat(true);
+  
+  
+  for(unsigned int k=0; k < 100; k++)
+    stat.addValue(99);
+  stat.addValue(88);
+  for(unsigned int k=0; k < 100; k++)
+    stat.addValue(77);
+  
+  stat.terminate();
+  
+  trace.info() << "Mean value = "<<stat.mean()  << std::endl;
+  nbok += (stat.mean()==88) ? 1 : 0; 
+  trace.info() << "Variance value = "<<stat.variance()<<std::endl;
+  trace.info() << "Max value = "<<stat.max()<<std::endl;
+  nbok += (stat.max()==99) ? 1 : 0; 
+  trace.info() << "Min value = "<<stat.min()<<std::endl;
+  nbok += (stat.min()==77)  ? 1 : 0; 
+  trace.info() << "Median value = "<<stat.median()<<std::endl;
+  nbok += (stat.median()==88)  ? 1 : 0; 
  
   trace.info() << "(" << nbok << "/" << nb << ") "
 	       << "true == true" << std::endl;
@@ -87,8 +125,19 @@ int main( int argc, char** argv )
 
   bool res = testStatistics(); // && ... other tests
   trace.emphase() << ( res ? "Passed." : "Error." ) << endl;
+
+  trace.beginBlock ( "Testing class Statistics (with option for saving samples)" );
+  trace.info() << "Args:";
+  for ( int i = 0; i < argc; ++i )
+    trace.info() << " " << argv[ i ];
+  trace.info() << endl;
+
+  bool res2 = testStatisticsSaving(); // && ... other tests
+  trace.emphase() << ( res2 ? "Passed." : "Error." ) << endl;
+
+
   trace.endBlock();
-  return res ? 0 : 1;
+  return (res &&res2) ? 0 : 1;
 }
 //                                                                           //
 ///////////////////////////////////////////////////////////////////////////////
