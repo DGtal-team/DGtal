053288893cd177d6a504eb99cc350ea3704f37e3
Tristan Roussillon <tristan.roussillon@liris.cnrs.fr>
2011-06-28 15:58:57 +0200

    first version of MostCentered, etc. (does nothing)

---
+++ src/DGtal/geometry/2d/MostCenteredMaximalSegmentEstimator.h
@@ -0,0 +1,192 @@
+/**
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU Lesser General Public License as
+ *  published by the Free Software Foundation, either version 3 of the
+ *  License, or  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ **/
+
+#pragma once
+
+/**
+ * @file MostCenteredMaximalSegmentEstimator.h
+ * @author Tristan Roussillon (\c tristan.roussillon@liris.cnrs.fr )
+ * Laboratoire d'InfoRmatique en Image et Systèmes d'information - LIRIS (CNRS, UMR 5205), CNRS, France
+ *
+ * @date 2011/01/26
+ *
+ * Header file for module MostCenteredMaximalSegmentEstimator.cpp
+ *
+ * This file is part of the DGtal library.
+ */
+
+#if defined(MostCenteredMaximalSegmentEstimator_RECURSES)
+#error Recursive header files inclusion detected in MostCenteredMaximalSegmentEstimator.h
+#else // defined(MostCenteredMaximalSegmentEstimator_RECURSES)
+/** Prevents recursive inclusion of headers. */
+#define MostCenteredMaximalSegmentEstimator_RECURSES
+
+#if !defined MostCenteredMaximalSegmentEstimator_h
+/** Prevents repeated inclusion of headers. */
+#define MostCenteredMaximalSegmentEstimator_h
+
+//////////////////////////////////////////////////////////////////////////////
+// Inclusions
+#include <iostream>
+#include <list>
+
+#include "DGtal/geometry/2d/SegmentComputerFunctor.h"
+#include "DGtal/geometry/2d/MaximalSegments.h"
+
+#include "DGtal/base/Exceptions.h"
+#include "DGtal/base/Common.h"
+//////////////////////////////////////////////////////////////////////////////
+
+namespace DGtal
+{
+
+  /////////////////////////////////////////////////////////////////////////////
+  // template class MostCenteredMaximalSegmentEstimator
+  /**
+   * Description of template class 'MostCenteredMaximalSegmentEstimator' <p>
+   * \brief Aim:Computes a quantity to each element of a range associated to 
+   * the most centered maximal segment  
+   */
+  template <typename SegmentComputer, typename Functor>
+  class MostCenteredMaximalSegmentEstimator
+  {
+
+    // ----------------------- Types ------------------------------
+	public:
+
+    typedef typename SegmentComputer::Iterator ConstIterator;
+    typedef typename Functor::Value Quantity;
+
+    // ----------------------- Standard services ------------------------------
+  public:
+
+    /**
+     * Default constructor.
+     */
+    MostCenteredMaximalSegmentEstimator() {};
+    /**
+     * Constructor.
+     * @param h grid size (must be >0).
+     * @param itb, begin iterator
+     * @param ite, end iterator
+     * @param aSegmentComputer
+     * @param isClosed true if the input range is closed.
+     */
+    MostCenteredMaximalSegmentEstimator(
+      const double h, 
+      const ConstIterator& itb, const ConstIterator& ite,
+      const SegmentComputer& aSegmentComputer, 
+      const Functor& aFunctor,
+      const bool& isClosed);
+
+    /**
+     * Destructor.
+     */
+    ~MostCenteredMaximalSegmentEstimator() {};
+
+    // ----------------------- Interface --------------------------------------
+  public:
+
+    /**
+     * Initialisation.
+     * @param h grid size (must be >0).
+     * @param itb, begin iterator
+     * @param ite, end iterator
+     * @param aSegmentComputer
+     * @param isClosed true if the input range is viewed as closed.
+     */
+    void init(
+      const double h, 
+      const ConstIterator& itb, const ConstIterator& ite,
+      const SegmentComputer& aSegmentComputer, 
+      const Functor& aFunctor, 
+      const bool& isClosed);
+
+    /**
+     * @return the estimated quantity at *it
+     */
+    Quantity eval(const ConstIterator& it) const;
+
+    /**
+     * @return the estimated quantity
+     * from itb till ite (exculded)
+     */
+    template <typename OutputIterator>
+    OutputIterator eval(const ConstIterator& itb, const ConstIterator& ite, 
+                        OutputIterator result) const; 
+
+
+    /**
+     * Checks the validity/consistency of the object.
+     * @return 'true' if the object is valid, 'false' otherwise.
+     */
+    bool isValid() const;
+
+    // ------------------------- Protected Datas ------------------------------
+  protected:
+
+    // ------------------------- Private Datas --------------------------------
+  private:
+
+		double myH; 
+		bool myFlagIsClosed;
+
+    Functor myFunctor;
+
+		ConstIterator myBegin;
+		ConstIterator myEnd;
+
+    //MaximalSegments<SegmentComputer> myMS; 
+
+    // ------------------------- Hidden services ------------------------------
+  protected:
+
+
+
+  private:
+
+    /**
+     * Copy constructor.
+     * @param other the object to clone.
+     * Forbidden by default.
+     */
+    MostCenteredMaximalSegmentEstimator ( const MostCenteredMaximalSegmentEstimator & other );
+
+    /**
+     * Assignment.
+     * @param other the object to copy.
+     * @return a reference on 'this'.
+     * Forbidden by default.
+     */
+    MostCenteredMaximalSegmentEstimator & operator= ( const MostCenteredMaximalSegmentEstimator & other );
+
+
+  }; // end of class MostCenteredMaximalSegmentEstimator
+
+} // namespace DGtal
+
+
+///////////////////////////////////////////////////////////////////////////////
+// Includes inline functions.
+#include "DGtal/geometry/2d/MostCenteredMaximalSegmentEstimator.ih"
+
+//                                                                           //
+///////////////////////////////////////////////////////////////////////////////
+
+#endif // !defined MostCenteredMaximalSegmentEstimator_h
+
+#undef MostCenteredMaximalSegmentEstimator_RECURSES
+#endif // else defined(MostCenteredMaximalSegmentEstimator_RECURSES)
---
+++ tests/geometry/2d/testMostCenteredMSEstimator.cpp
@@ -0,0 +1,136 @@
+/**
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU Lesser General Public License as
+ *  published by the Free Software Foundation, either version 3 of the
+ *  License, or  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ **/
+//LICENSE-END
+/**
+ * @file testMostCenteredMSEstimator.cpp
+ * @ingroup Tests
+ * @author Tristan Roussillon (\c tristan.roussillon@liris.cnrs.fr )
+ * Laboratoire d'InfoRmatique en Image et Systèmes d'information - LIRIS (CNRS, UMR 5205), CNRS, France
+ *
+ *
+ * @date 2011/06/28
+ *
+ * This file is part of the DGtal library
+ */
+
+/**
+ * Description of testMostCenteredMSEstimator <p>
+ */
+
+#include <cstdio>
+#include <cmath>
+#include <fstream>
+#include <vector>
+#include <iostream>
+#include <iterator>
+
+
+
+
+#include "DGtal/base/Common.h"
+#include "DGtal/base/Exceptions.h"
+#include "DGtal/io/DGtalBoard.h"
+
+#include "DGtal/topology/KhalimskySpaceND.h"
+
+#include "DGtal/geometry/2d/GridCurve.h"
+#include "DGtal/geometry/2d/ArithmeticalDSS.h"
+#include "DGtal/geometry/2d/MostCenteredMaximalSegmentEstimator.h"
+
+
+#include "ConfigTest.h"
+
+
+using namespace DGtal;
+using namespace std;
+using namespace LibBoard;
+
+///////////////////////////////////////////////////////////////////////////////
+// Functions for testing class MostCenteredMaximalSegmentEstimator
+///////////////////////////////////////////////////////////////////////////////
+
+/**
+ * Test 
+ *
+ */
+bool testDec4(string filename)
+{
+
+
+
+  trace.info() << "Reading GridCurve " << endl;
+  
+  ifstream instream; // input stream
+  instream.open (filename.c_str(), ifstream::in);
+  typedef KhalimskySpaceND<2> Kspace; //space
+  GridCurve<Kspace> c(instream); //building grid curve
+  typedef GridCurve<Kspace >::PointsRange Range;//range
+  Range r = c.getPointsRange();//building range
+
+  trace.info() << "Building Estimator " << endl;
+
+  typedef Range::ConstIterator ConstIterator;//constIterator
+  typedef ArithmeticalDSS<ConstIterator,Kspace::Integer,4> SegmentComputer;//segmentComputer
+  typedef TangentFromArithmeticalDSSFunctor<SegmentComputer> Functor; //functor
+	typedef MostCenteredMaximalSegmentEstimator<SegmentComputer,Functor> Estimator;//estimator
+
+  SegmentComputer sc;
+  Functor f; 
+
+  Estimator e(1,r.begin(),r.end(),sc,f,false);
+
+//eval at one element
+  for (ConstIterator i = r.begin(); i != r.end(); ++i) {
+    cout << e.eval(i) << " "; 
+  }
+  cout << endl;
+
+//eval for each element
+  vector<double> v(c.myData.size()); 
+  e.eval(r.begin(),r.end(),v.begin());
+
+  for (vector<double>::iterator i = v.begin(); i != v.end(); ++i) {
+    cout << *i << " "; 
+  }
+  cout << endl;
+
+	return false;
+}
+
+/////////////////////////////////////////////////////////////////////////
+//////////////// MAIN ///////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////////
+
+int main(int argc, char **argv)
+{
+  
+  trace.beginBlock ( "Testing class MostCenteredMaximalSegmentEstimator" );
+  trace.info() << "Args:";
+  for ( int i = 0; i < argc; ++i )
+    trace.info() << " " << argv[ i ];
+  trace.info() << endl;
+
+  std::string sinus2D4 = testPath + "samples/sinus2D4.dat";
+
+  bool res = testDec4(sinus2D4)
+//other tests
+;
+  trace.emphase() << ( res ? "Passed." : "Error." ) << endl;
+  trace.endBlock();
+
+  return res ? 0 : 1;
+
+}
