7327c6e4f0b2c27f0b89b2d8932b57339e618501
Jacques-Olivier Lachaud <jacques-olivier.lachaud@univ-savoie.fr>
2011-07-01 22:41:15 +0200

    GaussDigitizer and GridCurve construction from digitization.

--- src/DGtal/helpers/Surfaces.h
+++ src/DGtal/helpers/Surfaces.h
@@ -257,6 +257,36 @@ namespace DGtal
 			  const PointPredicate & pp,
 			  const SCell & start_surfel );
 
+    /**
+       This method is only 2D.
+       
+       This method uses random tries to find a first linel separating
+       an interior pixel from an exterior one. It then follows direct
+       orientations to extract the 4-connected set of points.
+
+       @param aVectorOfPoints (returns) the sequence of points of the
+       boundary component of the digitized shape containing [start_surfel].
+
+       @param K any space of dimension 2.
+
+       @param surfel_adj the surfel adjacency chosen for the tracking.
+
+       @param pp an instance of a model of CPointPredicate, for
+       instance a SetPredicate for a digital set representing a shape,
+       which should be at least partially included in the bounds of
+       space [K].
+
+       @param start_surfel a signed surfel which should be between an
+       element of [shape] and an element not in [shape].
+    */
+    template <typename PointPredicate>
+    static
+    void 
+    track2DBoundaryPoints( std::vector<Point> & aVectorOfPoints,
+			   const KSpace & K, 
+			   const SurfelAdjacency<KSpace::dimension> & surfel_adj,
+			   const PointPredicate & pp,
+			   const SCell & start_surfel );
 
 
     /**
