b7681deeee7d475f09898255d1b47a47e3fa6b44
David Coeurjolly <david.coeurjolly@liris.cnrs.fr>
2011-08-30 16:00:28 +0200

    Missing folders

---
+++ build/examples/io/boards/CMakeFiles/dgtalboard-3-custom-points.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/dgtalboard-3-custom-points.dir/dgtalboard-3-custom-points.cpp.o"
+  "dgtalboard-3-custom-points.pdb"
+  "dgtalboard-3-custom-points"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/dgtalboard-3-custom-points.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
---
+++ build/examples/io/viewers/CMakeFiles/viewer3D-6-clipping.dir/DependInfo.cmake
@@ -0,0 +1,27 @@
+# The set of languages for which implicit dependencies are needed:
+SET(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+SET(CMAKE_DEPENDS_CHECK_CXX
+  "/Users/davidcoeurjolly/Sources/DGtal/examples/io/viewers/viewer3D-6-clipping.cpp" "/Users/davidcoeurjolly/Sources/DGtal/build/examples/io/viewers/CMakeFiles/viewer3D-6-clipping.dir/viewer3D-6-clipping.cpp.o"
+  )
+SET(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+SET(CMAKE_TARGET_DEFINITIONS
+  "UNIX"
+  "APPLE"
+  "INLINE=inline"
+  "WITH_VISU3D_QGLVIEWER"
+  "QT_OPENGL_LIB"
+  "QT_GUI_LIB"
+  "QT_XML_LIB"
+  "QT_CORE_LIB"
+  )
+
+# Targets to which this target links.
+SET(CMAKE_TARGET_LINKED_INFO_FILES
+  "/Users/davidcoeurjolly/Sources/DGtal/build/src/CMakeFiles/DGtal-visu3d.dir/DependInfo.cmake"
+  "/Users/davidcoeurjolly/Sources/DGtal/build/src/CMakeFiles/DGtal.dir/DependInfo.cmake"
+  )
