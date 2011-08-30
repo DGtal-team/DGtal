68d0cf8118d5b51df88762fb43e78ecf07ec6401
David Coeurjolly <david.coeurjolly@liris.cnrs.fr>
2011-08-30 16:01:18 +0200

    Missing folder

---
+++ examples/io/viewers/viewer3D-4-modes.cpp
@@ -0,0 +1,82 @@
+/**
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU Lesser General Public License as
+ *  published by the Free Software Foundation, either version 3 of the
+ *  License, or  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ **/
+
+/**
+ * @file viewer3D-4-modes.cpp
+ * @ingroup examples/3dViewer
+ * @author Bertrand Kerautret (\c kerautre@loria.fr )
+ * LORIA (CNRS, UMR 7503), University of Nancy, France
+ *
+ * @date 2011/19/03
+ *
+ * Simple example of class Viewer3D.
+ *
+ * This file is part of the DGtal library.
+ */
+
+///////////////////////////////////////////////////////////////////////////////
+#include <iostream>
+#include <QtGui/qapplication.h>
+#include "DGtal/io/viewers/Viewer3D.h"
+#include "DGtal/base/Common.h"
+#include "DGtal/helpers/StdDefs.h"
+#include "DGtal/helpers/Shapes.h"
+
+///////////////////////////////////////////////////////////////////////////////
+
+using namespace std;
+using namespace DGtal;
+using namespace Z3i;
+
+
+///////////////////////////////////////////////////////////////////////////////
+// Standard services - public :
+
+int main( int argc, char** argv )
+{
+
+ QApplication application(argc,argv);
+ Viewer3D viewer;
+ viewer.show();
+
+ 
+ 
+ Point p1( -1, -1, -2 );
+ Point p2( 2, 2, 3 );
+ Domain domain( p1, p2 );
+ Point p3( 1, 1, 1 );
+ Point p4( 2, -1, 3 );
+ Point p5( -1, 2, 3 );
+ Point p6( 0, 0, 0 );
+ Point p0( 0, 2, 1 );
+ 
+ // viewer <<  SetMode3D( p1.styleName(), "Grid" );
+
+ viewer << p1 << p2 << p3<< p4<< p5 << p6 << p0;
+  
+
+ //viewer << SetMode3D(domain.styleName(), "PavingGrids");
+ viewer << domain << Display3D::updateDisplay;
+ 
+
+ return application.exec();
+}
+//                                                                           //
+///////////////////////////////////////////////////////////////////////////////
+
+
+
+
