7d4d9ba943c428ce68257ce1d23472fdb81fd639
Xavier Provençal <xavier.provencal@univ-savoie.fr>
2011-08-31 14:18:22 +0200

    CombinatorialDSS OK and modif to /geometry's documentation

--- src/DGtal/geometry/nd/volumetric/SeparableMetricTraits.h
+++ src/DGtal/geometry/nd/volumetric/SeparableMetricTraits.h
@@ -18,6 +18,7 @@
 
 /**
  * @file SeparableMetricTraits.h
+ * @brief Basic functions associated to metrics used by separable volumetric algorithms.
  * @author David Coeurjolly (\c david.coeurjolly@liris.cnrs.fr )
  * Laboratoire d'InfoRmatique en Image et Systèmes d'information - LIRIS (CNRS, UMR 5205), CNRS, France
  *
@@ -26,6 +27,8 @@
  * Header file for module SeparableMetricTraits.cpp
  *
  * This file is part of the DGtal library.
+ *
+ * @see testDistanceTransformationND.cpp
  */
 
 #if defined(SeparableMetricTraits_RECURSES)
