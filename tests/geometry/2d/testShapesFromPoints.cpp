af9f18a063cf7a5f74598afda2d9e43d6f5b2385
Tristan Roussillon <tristan.roussillon@liris.cnrs.fr>
2011-09-22 17:52:13 +0200

    new CircleFrom3Points class + its unit test

---
+++ tests/geometry/2d/testShapesFromPoints.cpp
@@ -0,0 +1,132 @@
+/**
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU Lesser General Public License as
+ *  published by the Free Software Foundation, either version 3 of the
+ *  License, or  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ **/
+
+/**
+ * @file testShapeFromPoints.cpp
+ * @author Tristan Roussillon (\c
+ * tristan.roussillon@liris.cnrs.fr ) Laboratoire d'InfoRmatique en
+ * Image et Systèmes d'information - LIRIS (CNRS, UMR 5205), CNRS,
+ * France
+ *
+ *
+ * @date 2011/06/27
+
+ * Functions for testing various shapes from points
+ *
+ * This file is part of the DGtal library.
+ */
+
+///////////////////////////////////////////////////////////////////////////////
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <exception>
+#include <boost/program_options.hpp>
+
+#include "DGtal/base/Common.h"
+#include "DGtal/kernel/PointVector.h"
+#include "DGtal/geometry/2d/CircleFrom3Points.h"
+
+#include "DGtal/io/boards/Board2D.h"
+
+#include "ConfigTest.h"
+
+///////////////////////////////////////////////////////////////////////////////
+
+using namespace std;
+using namespace DGtal;
+
+///////////////////////////////////////////////////////////////////////////////
+// testing Functions
+///////////////////////////////////////////////////////////////////////////////
+
+/**
+ * Simple tests
+ *
+ */
+template <typename Coordinate>
+bool testCircleFrom3Points()
+{
+
+  trace.beginBlock("Simple test"); 
+  
+  typedef PointVector<2,Coordinate> Point; 
+  CircleFrom3Points<Point> c; 
+  Point o(0,0); 
+  Board2D board; 
+  board << SetMode(o.styleName(), "Grid") << o; 
+
+  {
+    c.init( Point(0,1), Point(150,18), Point(250,-48) ); 
+    trace.info() << c << endl;
+    trace.info() << o << " is at distance " << c.signedDistance(o) << endl;
+    if (c.signedDistance(o) != -4026300) return false; 
+    board << c; 
+    board.saveEPS("circle1.eps", Board2D::BoundingBox, 5000 );
+  }
+  {
+    c.init( Point(0,1), Point(150,18), Point(100,48) ); 
+    trace.info() << c << endl;
+    trace.info() << o << " is at distance " << c.signedDistance(o) << endl;
+    if (c.signedDistance(o) != 442200) return false; 
+    board << c; 
+    board.saveEPS("circle2.eps", Board2D::BoundingBox, 5000 );
+  }
+  {
+    c.init( Point(0,1), Point(150,18), Point(100,48) ); 
+    trace.info() << c << endl;
+    trace.info() << Point(150,18) << " is at distance " << c.signedDistance(Point(150,18)) << endl;
+    if (c.signedDistance(Point(150,18)) != 0) return false; 
+  }
+  {
+    c.init( Point(0,0), Point(150,20), Point(15,2) ); 
+    trace.info() << c << endl;
+    trace.info() << o << " is at distance " << c.signedDistance(o) << endl;
+    if (c.signedDistance(o) != 0) return false; 
+    board << c; 
+    board.saveEPS("circle4.eps", Board2D::BoundingBox, 5000 );
+  }
+
+  trace.endBlock(); 
+  
+  return true; 
+}
+
+///////////////////////////////////////////////////////////////////////////////
+// Standard services - public :
+
+int main( int argc, char** argv )
+{
+  trace.beginBlock ( "Testing shapes from points" );
+  trace.info() << "Args:";
+  for ( int i = 0; i < argc; ++i )
+    trace.info() << " " << argv[ i ];
+  trace.info() << endl;
+
+
+  bool res = testCircleFrom3Points<int>()
+  && testCircleFrom3Points<double>()
+  && testCircleFrom3Points<BigInteger>()
+; 
+
+
+  trace.emphase() << ( res ? "Passed." : "Error." ) << endl;
+  trace.endBlock();
+  
+  return res ? 0 : 1;
+}
+//                                                                           //
+///////////////////////////////////////////////////////////////////////////////
