db7c71010a0e9e6612fc0a5cb3705f64067a0e37
Tristan Roussillon <tristan.roussillon@liris.cnrs.fr>
2011-09-28 17:35:24 +0200

    tests again

--- tests/geometry/2d/testPreimage.cpp
+++ tests/geometry/2d/testPreimage.cpp
@@ -63,121 +63,37 @@ using namespace DGtal;
 int main()
 {
 
-  typedef int Coordinate;
-  typedef PointVector<2, Coordinate> Point;
-  typedef StraightLineFrom2Points<Point> StraightLine;
+  std::string filename = testPath + "samples/DSS.dat";
+  ifstream instream; // input stream
+  instream.open (filename.c_str(), ifstream::in);
+  
+  typedef KhalimskySpaceND<2,int> KSpace; 
+  GridCurve<KSpace> c; //grid curve
+  c.initFromVectorStream(instream);
+
+
+  trace.beginBlock("Simple preimage test");
+
+  typedef StraightLineFrom2Points<GridCurve<KSpace>::Point> StraightLine;
+  StraightLine aStraightLine; //instance of straight line
   typedef Preimage2D<StraightLine> Preimage2D;
 
-  //data
-  std::vector<Point> bInf, bSup;
+  GridCurve<KSpace>::IncidentPointsRange r = c.getIncidentPointsRange(); //range
+  GridCurve<KSpace>::IncidentPointsRange::ConstIterator it (r.begin()); //iterators
+  GridCurve<KSpace>::IncidentPointsRange::ConstIterator itEnd (r.end()); 
 
-//////////////////////////// test 1 //////////////////////////////////////
-  bInf.push_back(Point(0, 10));
-  bInf.push_back(Point(1, 11));
-  bInf.push_back(Point(2, 11));
-  bInf.push_back(Point(3, 12));
-
-  bSup.push_back(Point(0, 12));
-  bSup.push_back(Point(1, 13));
-  bSup.push_back(Point(2, 13));
-  bSup.push_back(Point(3, 14));
-
-  Board2D board;
-  board.setUnit(Board2D::UCentimeter);
-
-
-  //SimpleTest
-  trace.beginBlock("Simple Preimage test");
+  //preimage computation
+  Preimage2D thePreimage(it->first, it->second, aStraightLine);
+  ++it; 
+  while ( (it != itEnd) &&
+              (thePreimage.addFront(it->first, it->second)) )
   {
-    int i = 0;
-    Preimage2D thePreimage(bInf.at(i), bSup.at(i), StraightLine());
-    
-    //draw range
-    Point P(bInf.at(i));
-    Point Q(bSup.at(i));
-    board << P << Q; 
-    board.drawLine(P[0], P[1], Q[0], Q[1]);
-
-    i++;
-    int n;
-    n = bInf.size();
-    while ( (i < n) &&
-        (thePreimage.addFront(bInf.at(i), bSup.at(i))) )
-    {
-
-      trace.info() << "--- Constraint number " << i << " ---" << std::endl;
-      trace.info() << thePreimage << std::endl;
-
-      //draw range
-      Point P2(bInf.at(i));
-      Point Q2(bSup.at(i));
-      board << P2 << Q2; 
-      board.drawLine(P2[0], P2[1], Q2[0], Q2[1]);
-
-      i++;
-    }
-
-    board << thePreimage; 
-    board.saveEPS( "testPreimage-simple.eps", Board2D::BoundingBox, 5000 );
+  trace.info() << (it - r.begin()) << endl << thePreimage << endl;
+    ++it;
   }
+
   trace.endBlock();
-
-
-  bInf.push_back(Point(4, 10));
-  bInf.push_back(Point(5, 10));
-  bInf.push_back(Point(6, 11));
-  bInf.push_back(Point(7, 12));
-  bInf.push_back(Point(8, 12));
-  bInf.push_back(Point(9, 13));
-
-  bSup.push_back(Point(4, 13));
-  bSup.push_back(Point(5, 12));
-  bSup.push_back(Point(6, 13));
-  bSup.push_back(Point(7, 14));
-  bSup.push_back(Point(8, 14));
-  bSup.push_back(Point(9, 15));
-
-  //preimage
-  trace.beginBlock("test limit case");
-
-  board.clear(); 
-  
-  {
-    int i = 0;
-    Preimage2D thePreimage2(bInf.at(i), bSup.at(i), StraightLine());
-
-    //draw range
-    Point P(bInf.at(i));
-    Point Q(bSup.at(i));
-    board << P << Q; 
-    board.drawLine(P[0], P[1], Q[0], Q[1]);
-
-    i++;
-    int n;
-    n = bInf.size();
-    while ( (i < n) &&
-        (thePreimage2.addFront(bInf.at(i), bSup.at(i))) )
-    {
-
-      trace.info() << "--- Constraint number " << i << " ---" << std::endl;
-      trace.info() << thePreimage2 << std::endl;
-
-      //draw range
-      Point P2(bInf.at(i));
-      Point Q2(bSup.at(i));
-      board << P2 << Q2;
-      board.drawLine(P2[0], P2[1], Q2[0], Q2[1]);
-
-      i++;
-    }
-
-    //draw preimage
-    board << thePreimage2; 
-    board.saveEPS("testPreimage-limitCase.eps");
-
-  }
-  trace.endBlock();
-
+/*
 //////////////////////////// test 2 //////////////////////////////////////
 
   bInf.clear();
@@ -252,7 +168,7 @@ int main()
 
   }
   trace.endBlock();
-
+*/
 
   return 0;
 }
