74c795483029a28d9458ecf9225f3bd9ab25e331
Tristan Roussillon <tristan.roussillon@liris.cnrs.fr>
2011-09-28 10:57:54 +0200

    other tests (not finished)

--- tests/geometry/2d/testGeometricalDSS.cpp
+++ tests/geometry/2d/testGeometricalDSS.cpp
@@ -33,10 +33,15 @@
 #include "DGtal/kernel/PointVector.h"
 #include "DGtal/topology/KhalimskySpaceND.h"
 #include "DGtal/geometry/2d/GridCurve.h"
-#include "DGtal/geometry/2d/GeometricalDSS.h"
 
 #include "DGtal/geometry/CBidirectionalSegmentComputer.h"
 #include "DGtal/io/boards/CDrawableWithBoard2D.h"
+
+#include "DGtal/geometry/2d/GeometricalDSS.h"
+
+#include "DGtal/geometry/2d/GreedySegmentation.h"
+#include "DGtal/geometry/2d/SaturatedSegmentation.h"
+
 
 #include "ConfigTest.h"
 
@@ -49,8 +54,7 @@ using namespace DGtal;
 // Functions for testing class GeometricalDSS.
 ///////////////////////////////////////////////////////////////////////////////
 /**
- * Example of a test. To be completed.
- *
+ * Basic methods
  */
 template <typename TCurve>
 bool testGeometricalDSS(const TCurve& curve)
@@ -63,58 +67,86 @@ bool testGeometricalDSS(const TCurve& curve)
   unsigned int nbok = 0;
   unsigned int nb = 0;
   
-  trace.beginBlock ( "Testing block ..." );
+  trace.beginBlock ( "Constructors, copy, assignement" );
+  {
+    Range r = curve.getIncidentPointsRange(); //range
 
-  Range r = curve.getIncidentPointsRange(); //range
+    GeometricalDSS<ConstIterator> s1, s2, s3;
+    s2.init(r.begin()); 
+    s3.init(++r.begin()); 
+    GeometricalDSS<ConstIterator> s4(s2); 
+    GeometricalDSS<ConstIterator> s5(s3);
+    s3 = s1; 
+    
+    trace.info() << s1.isValid() << s1 << endl; 
+    trace.info() << s2.isValid() << s2 << endl; 
+    trace.info() << s3.isValid() << s3 << endl; 
+    trace.info() << s4.isValid() << s4 << endl; 
+    trace.info() << s5.isValid() << s5 << endl; 
 
-  GeometricalDSS<ConstIterator> s, t;
+    bool myFlag = (!s1.isValid())&&(!s3.isValid())
+    &&(s2.isValid())&&(s4.isValid())&&(s5.isValid())
+    &&(s2 == s4)&&(s3 != s5)&&(s1 == s3)&&(s2 != s5);
 
-  trace.info() << "forward extension " << endl; 
-  ConstIterator itBegin (r.begin()); 
-  ConstIterator itEnd (r.end()); 
-  s.init( itBegin );
-  while ( (s.end() != itEnd) && (s.isExtendable()) && (s.extend()) ) {}
-  trace.info() << s << endl; 
-  double a, b, c; 
-  s.getParameters(a,b,c); 
-  trace.info() << a << " " << b << " " << c << endl; 
+    nbok += myFlag ? 1 : 0; 
+    nb++;
+    trace.info() << "(" << nbok << "/" << nb << ") " << endl;
+  }
+  trace.endBlock();
+  
+  trace.beginBlock ( "Extension operations" );
+  {
+    Range r = curve.getIncidentPointsRange(); //range
 
-  t.init( (itBegin + (itEnd - itBegin)/2) ); 
-  while ( (t.end() != itEnd) && (t.extend()) 
-       && (t.begin() != itBegin) && (t.extendOppositeEnd()) ) {}
-  trace.info() << t << endl; 
+    GeometricalDSS<ConstIterator> s, t;
 
-  trace.info() << "backward extension " << endl; 
-  typename GeometricalDSS<ConstIterator>::Reverse rs = s.getReverse(); 
-  ConstReverseIterator ritBegin (r.rbegin()); 
-  ConstReverseIterator ritEnd (r.rend()); 
-  rs.init( ritBegin );
-  while ( (rs.end() != ritEnd) && (rs.isExtendable()) && (rs.extend()) ) {}
-  trace.info() << rs << endl; 
-  double ap, bp, cp; 
-  rs.getParameters(ap,bp,cp); 
-  trace.info() << ap << " " << bp << " " << cp << endl; 
+    trace.info() << "forward extension " << endl; 
+    ConstIterator itBegin (r.begin()); 
+    ConstIterator itEnd (r.end()); 
+    s.init( itBegin );
+    while ( (s.end() != itEnd) && (s.isExtendable()) && (s.extend()) ) {}
+    trace.info() << s << endl; 
+    double a, b, c; 
+    s.getParameters(a,b,c); 
+    trace.info() << a << " " << b << " " << c << endl; 
 
-  typename GeometricalDSS<ConstIterator>::Reverse rt = t.getReverse(); 
-  rt.init( (ritBegin + (ritEnd - ritBegin)/2) ); 
-  while ( (rt.end() != ritEnd) && (rt.extend()) 
-       && (rt.begin() != ritBegin) && (rt.extendOppositeEnd()) ) {}
-  trace.info() << rt << endl; 
+    t.init( (itBegin + (itEnd - itBegin)/2) ); 
+    while ( (t.end() != itEnd) && (t.extend()) 
+         && (t.begin() != itBegin) && (t.extendOppositeEnd()) ) {}
+    trace.info() << t << endl; 
 
-  trace.info() << "comparison... " << endl; 
-  bool myFlag = ( (s == t)&&(rs == rt) )
-&& ( s.getUf() == rs.getUf() )
-&& ( s.getUl() == rs.getUl() )
-&& ( s.getLf() == rs.getLf() )
-&& ( s.getLl() == rs.getLl() )
-&& (a == ap)
-&& (b == bp)
-&& (c == cp)
-; 
+    trace.info() << "backward extension " << endl; 
+    typename GeometricalDSS<ConstIterator>::Reverse rs = s.getReverse(); 
+    ConstReverseIterator ritBegin (r.rbegin()); 
+    ConstReverseIterator ritEnd (r.rend()); 
+    rs.init( ritBegin );
+    while ( (rs.end() != ritEnd) && (rs.isExtendable()) && (rs.extend()) ) {}
+    trace.info() << rs << endl; 
+    double ap, bp, cp; 
+    rs.getParameters(ap,bp,cp); 
+    trace.info() << ap << " " << bp << " " << cp << endl; 
 
-  nbok += myFlag ? 1 : 0; 
-  nb++;
-  trace.info() << "(" << nbok << "/" << nb << ") " << endl;
+    typename GeometricalDSS<ConstIterator>::Reverse rt = t.getReverse(); 
+    rt.init( (ritBegin + (ritEnd - ritBegin)/2) ); 
+    while ( (rt.end() != ritEnd) && (rt.extend()) 
+         && (rt.begin() != ritBegin) && (rt.extendOppositeEnd()) ) {}
+    trace.info() << rt << endl; 
+
+    trace.info() << "comparison... " << endl; 
+    bool myFlag = ( (s == t)&&(rs == rt) )
+    && ( s.getUf() == rs.getUf() )
+    && ( s.getUl() == rs.getUl() )
+    && ( s.getLf() == rs.getLf() )
+    && ( s.getLl() == rs.getLl() )
+    && (a == ap)
+    && (b == bp)
+    && (c == cp)
+    ; 
+
+    nbok += myFlag ? 1 : 0; 
+    nb++;
+    trace.info() << "(" << nbok << "/" << nb << ") " << endl;
+  }
   trace.endBlock();
   
   return nbok == nb;
@@ -142,7 +174,7 @@ bool drawingTestGeometricalDSS(const TCurve& curve)
 
   Board2D board; 
   board << r << s; 
-  board.saveEPS("drawingTestGeometricalDSS.eps"); 
+  board.saveEPS("GeometricalDSSdrawingTest.eps"); 
   return true; 
 }
 
@@ -155,7 +187,64 @@ void testGeometricalDSSConceptChecking()
    BOOST_CONCEPT_ASSERT(( CBidirectionalSegmentComputer<GeomDSS> ));
 }
 
+template <typename TCurve>
+bool testSegmentation(const TCurve& curve)
+{
 
+  typedef typename TCurve::IncidentPointsRange Range; //range
+  Range r = curve.getIncidentPointsRange(); //range
+  
+  typedef typename Range::ConstIterator ConstIterator; //iterator
+  typedef GeometricalDSS<ConstIterator> SegmentComputer; //segment computer
+  
+  unsigned int nbok = 0;
+  unsigned int nb = 0;  
+
+  
+  trace.beginBlock ( "Greedy segmentation" );
+  {
+    typedef GreedySegmentation<SegmentComputer> Segmentation;
+    Segmentation theSegmentation( r.begin(), r.end(), SegmentComputer() );
+    
+    Board2D board; 
+    board << r; 
+      
+    typename Segmentation::SegmentComputerIterator it = theSegmentation.begin();
+    typename Segmentation::SegmentComputerIterator itEnd = theSegmentation.end();
+    unsigned int c = 0; 
+    for ( ; it != itEnd; ++it, ++c) {
+      board << (*it); 
+    }
+    
+    board.saveEPS("GeometricalDSSGreedySegmentationTest.eps"); 
+    board.saveSVG("GeometricalDSSGreedySegmentationTest.svg"); 
+    
+    nbok += (c==1) ? 1 : 0; 
+    nb++;
+    trace.info() << "(" << nbok << "/" << nb << ") " << endl;
+  }
+  trace.endBlock();
+/*
+  trace.beginBlock ( "Saturated segmentation" );
+  {
+    typedef SaturatedSegmentation<SegmentComputer> Segmentation;
+    Segmentation theSegmentation( r.begin(), r.end(), SegmentComputer() );
+    
+    typename Segmentation::SegmentComputerIterator it = theSegmentation.begin();
+    typename Segmentation::SegmentComputerIterator itEnd = theSegmentation.end();
+    unsigned int c = 0; 
+    for ( ; it != itEnd; ++it, ++c) {
+      trace.info() << (*it) << std::endl;   //standard output
+    }
+    
+    nbok += (c==1) ? 1 : 0; 
+    nb++;
+    trace.info() << "(" << nbok << "/" << nb << ") " << endl;
+  }
+  trace.endBlock();
+  */
+  return (nbok == nb);
+}
 ///////////////////////////////////////////////////////////////////////////////
 // Standard services - public :
 
@@ -167,19 +256,37 @@ int main( int argc, char** argv )
     trace.info() << " " << argv[ i ];
   trace.info() << endl;
 
-  testGeometricalDSSConceptChecking();
-
-  std::string filename = testPath + "samples/DSS.dat";
-  ifstream instream; // input stream
-  instream.open (filename.c_str(), ifstream::in);
+  bool res; 
   
-  typedef KhalimskySpaceND<2,int> KSpace; 
-  GridCurve<KSpace> c; //grid curve
-  c.initFromVectorStream(instream);
+  {//concept checking
+    testGeometricalDSSConceptChecking();
+  }
+  
+  {//basic operations
+    std::string filename = testPath + "samples/DSS.dat";
+    ifstream instream; // input stream
+    instream.open (filename.c_str(), ifstream::in);
+    
+    typedef KhalimskySpaceND<2,int> KSpace; 
+    GridCurve<KSpace> c; //grid curve
+    c.initFromVectorStream(instream);
 
-  bool res = testGeometricalDSS(c)
-&& drawingTestGeometricalDSS(c)
-; // && ... other tests
+    res = testGeometricalDSS(c)
+  && drawingTestGeometricalDSS(c); 
+  }
+  
+  {//segmentations
+    std::string filename = testPath + "samples/sinus2D4.dat";
+    ifstream instream; // input stream
+    instream.open (filename.c_str(), ifstream::in);
+    
+    typedef KhalimskySpaceND<2,int> KSpace; 
+    GridCurve<KSpace> c; //grid curve
+    c.initFromVectorStream(instream);
+    
+    res = res && testSegmentation(c); 
+  }
+  
   trace.emphase() << ( res ? "Passed." : "Error." ) << endl;
   trace.endBlock();
   return res ? 0 : 1;
