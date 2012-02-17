755d7d6846ae7804c30b77420afbabbe445b4c4c
Tristan Roussillon <tristan.roussillon@liris.cnrs.fr>
2012-02-17 12:01:33 +0100

    succesful testing of ImageHelper functions

--- tests/kernel/testImagesSetsUtilities.cpp
+++ tests/kernel/testImagesSetsUtilities.cpp
@@ -22,7 +22,12 @@
  *
  * @date 2011/03/26
  *
- * Functions for testing class ImagesSetsUtilities.
+ * @author Tristan Roussillon (\c tristan.roussillon@liris.cnrs.fr )
+ * Laboratoire d'InfoRmatique en Image et Systèmes d'information - LIRIS (CNRS, UMR 5205), CNRS, France
+ *
+ * @date 2012/02/15
+ *
+ * Functions for testing ImageHelper functions.
  *
  * This file is part of the DGtal library.
  */
@@ -31,7 +36,6 @@
 #include <iostream>
 #include "DGtal/base/Common.h"
 
-#include "DGtal/io/readers/PNMReader.h"
 #include "DGtal/images/imagesSetsUtils/ImageFromSet.h"
 #include "DGtal/images/imagesSetsUtils/SetFromImage.h"
 
@@ -53,42 +57,76 @@ using namespace DGtal;
 using namespace Z2i;
 
 ///////////////////////////////////////////////////////////////////////////////
-// Functions for testing class ImagesSetsUtilities.
+// Functions for testing ImageHelper functions.
 ///////////////////////////////////////////////////////////////////////////////
+
+template <typename P>
+struct Norm1
+{
+  typedef P Point; 
+  typedef typename P::Coordinate Value; 
+
+  Value operator()(const Point& p)
+  {
+    return p.norm1(); 
+  }
+}; 
+
 /**
  * Example of a test. To be completed.
  *
  */
-bool testImagesSetsUtilities()
+bool testImageFromSet()
 {
+
+  typedef ImageContainerBySTLVector<Domain,int> Image;
+
   unsigned int nbok = 0;
   unsigned int nb = 0;
   
-  trace.beginBlock ( "Testing ImageFormSet ..." );
+  trace.beginBlock ( "Testing ImageFromSet ..." );
   
   Point a(0,0);
   Point b(23,435);
   Point c(12,12);
 
-  typedef ImageContainerBySTLVector<Domain,int> Image;
-  Image image(Domain(a,b));
-  DigitalSet aSet(Domain(a,b));
+  Domain d(a,b); 
+  DigitalSet aSet(d);
   aSet.insert(c);
   
-  //test of the append method
-  ImageFromSet<Image>::append<DigitalSet>(image, aSet, 128);
-  nbok += (image(c) == 128) ? 1 : 0; 
+  Image image(d);
+  //assign to the points belonging to aSet the value 128
+  imageFromRangeAndValue(aSet, image, 128);  
+  //ie. the value of c is 128 but the value of a remains 0
+  nbok += ( (image(c) == 128)&&(image(a) == 0) ) ? 1 : 0; 
   nb++;
+  trace.info() << "(" << nbok << "/" << nb << ") " << std::endl;
 
-  //test of the create method
-  Image imageBis =   ImageFromSet<Image>::create<DigitalSet>(aSet, 128);
-  nbok += (imageBis(c) == 128) ? 1 : 0; 
+  Image image2(d); 
+  Norm1<Point> n; 
+  //fill image2 from n
+  imageFromFunctor(image2, n); 
+  nbok += ( (image2(c) == c.norm1())
+	    &&(image2(a) == a.norm1())
+	    &&(image2(b) == b.norm1()) )? 1 : 0; 
   nb++;
-
-
+  trace.info() << "(" << nbok << "/" << nb << ") " << std::endl;
   
-  trace.info() << "(" << nbok << "/" << nb << ") "
-         << "true == true" << std::endl;
+  Image image3 = image; 
+  //fill image3 from image2
+  imageFromImage(image3, image2); 
+  //image2 and image3 should be equal, 
+  //but both different from image
+  Image::ConstRange rimg = image.range(); 
+  Image::ConstRange rimg2 = image2.range(); 
+  Image::ConstRange rimg3 = image3.range(); 
+  bool flag2 = std::equal(rimg.begin(), rimg.end(), rimg2.begin());
+  bool flag3 = std::equal(rimg.begin(), rimg.end(), rimg3.begin()); 
+  bool flag23 = std::equal(rimg2.begin(), rimg2.end(), rimg3.begin()); 
+  nbok += ( (!flag2) && (!flag3) && flag23 )?1:0;
+  nb++;  
+  trace.info() << "(" << nbok << "/" << nb << ") " << std::endl;
+
   trace.endBlock();
   
   return nbok == nb;
@@ -101,59 +139,85 @@ bool testSetFromImage()
   
   trace.beginBlock ( "Testing SetFromImage ..." );
   
+  //some points
   Point a(0,0);
-  Point b(2,2);
-  Point c(1,1);
+  Point b(3,3);
+  Point p(1,1);
+  Point q(2,2);
+  Point r(1,2);
 
+  //image construction
   typedef ImageContainerBySTLMap<Domain,int> Image;
   Domain d(a,b); 
   Image image(d,1);
-  image.setValue(c,128);
+  image.setValue(p,127);
+  image.setValue(q,128);
+  image.setValue(r,10);
   
+  //image content
+  Image::ConstRange range = image.range(); 
+  std::copy(range.begin(), range.end(), ostream_iterator<Image::Value>(cout, " ") ); 
+  cout << endl; 
+
+  //set tests
+
   DigitalSet aSet(d);
   DigitalSetInserter<DigitalSet> inserter(aSet); 
-  setFromImage( image, inserter, 127 ); 
+  //all points whose value <= 126
+  setFromImage( image, inserter, 126 ); 
+  //ie, all points except p and q
+  nbok += ( (aSet.find(p) == aSet.end())
+	    &&(aSet.find(q) == aSet.end())
+	    &&(aSet.size()==(d.size()-2)) ) ? 1 : 0; 
+  nb++;
+  trace.info() << "(" << nbok << "/" << nb << ") " << std::endl; 
 
-  trace.info()<< "DigitalSet:= ";
-  for(DigitalSet::ConstIterator it= aSet.begin(),itend=aSet.end();
-      it != itend;
-      ++it)
-    trace.info() << *it<< " ";
-  trace.info()<<endl;
+  DigitalSet aSet2(d);
+  DigitalSetInserter<DigitalSet> inserter2(aSet2); 
+  //all points whose value <= 127
+  setFromImage( image, inserter2, 127 ); 
+  //ie, all points except q
+  nbok += ( (aSet2.find(q) == aSet2.end())
+	    &&(aSet2.size()==(d.size()-1)) ) ? 1 : 0; 
+  nb++;
+  trace.info() << "(" << nbok << "/" << nb << ") " << std::endl; 
 
-  nbok += ( (aSet.find(c) == aSet.end())&&(aSet.size()==(d.size()-1)) ) ? 1 : 0; 
+  DigitalSet aSet3(d);
+  DigitalSetInserter<DigitalSet> inserter3(aSet3);
+  //all points whose value <= 128 
+  setFromImage( image, inserter3, 128 ); 
+  //ie, all points
+  nbok += ( aSet3.size()==d.size() ) ? 1 : 0; 
+  nb++;
+  trace.info() << "(" << nbok << "/" << nb << ") " << std::endl; 
+
+  DigitalSet aSet4(d);
+  DigitalSetInserter<DigitalSet> inserter4(aSet4);
+  //all points whose value is between 2 and 100 
+  setFromImage( image, inserter4, 2, 100 ); 
+  //ie, only point r
+  nbok += ( (aSet4.find(r)!=aSet4.end())&&(aSet4.size()==1) ) ? 1 : 0; 
+  nb++;
+  trace.info() << "(" << nbok << "/" << nb << ") " << std::endl; 
+
+  DigitalSet aSet5(d);
+  DigitalSetInserter<DigitalSet> inserter5(aSet5);
+  //predicate construction
+  typedef std::equal_to<Image::Value> EqualBinaryFunctor; 
+  typedef std::binder2nd<EqualBinaryFunctor> ValuePredicate; 
+  ValuePredicate equalTo1 (EqualBinaryFunctor(),1); 
+  PointPredicateFromPointFunctor<Image, ValuePredicate> pred(image, equalTo1); 
+  //all points whose value is 1
+  setFromPointsRangeAndPredicate( d.begin(), d.end(), inserter5, pred ); 
+  //ie all points except p, q, and r
+  nbok += ( (aSet5.find(p) == aSet5.end())
+	    &&(aSet5.find(q) == aSet5.end())
+	    &&(aSet5.find(r) == aSet5.end())
+	    &&(aSet5.size()==(d.size()-3)) ) ? 1 : 0; 
   nb++;
   trace.info() << "(" << nbok << "/" << nb << ") " << std::endl; 
 
 
-
-  // trace.beginBlock ( "Testing SetFromImage using pgm test file ..." );
-  
-  // std::string filename = testPath + "samples/circleR10modif.pgm";
-  // Image image2 = PNMReader<Image>::importPGM( filename ); 
-  // Z2i::DigitalSet setFromImg (image2.domain());
-
-  // //  SetFromImage<Z2i::DigitalSet>::append<Image>(setFromImg, image2, 0, 255);
-  // DigitalSetInserter<DigitalSet> inserter2(setFromImg); 
-  // setFromImage( image2, inserter2 ); 
-
-  
-  // for( Z2i::DigitalSet::Iterator it= setFromImg.begin(); 
-  //      it!=setFromImg.end(); it++){
-  //   trace.info()<< *it << endl;
-  // }
-  // trace.info()<< "Size=" << setFromImg.size();
-  
-  // Image setImage = ImageFromSet<Image>::create(setFromImg, 1,
-  //                false, 
-  //                 setFromImg.begin(), 
-  //                 setFromImg.end());
-  
-  // trace.info() << "Image at 0,12:" << setImage(Point(0,12))<< endl;
-  // trace.info() << "(" << nbok << "/" << nb << ") "
-  //        << "true == true" << std::endl;
-  // trace.endBlock();
-  
   return nbok == nb;
 }
 
@@ -162,13 +226,13 @@ bool testSetFromImage()
 
 int main( int argc, char** argv )
 {
-  trace.beginBlock ( "Testing class ImagesSetsUtilities" );
+  trace.beginBlock ( "Testing class ImageHelper functions" );
   trace.info() << "Args:";
   for ( int i = 0; i < argc; ++i )
     trace.info() << " " << argv[ i ];
   trace.info() << endl;
 
-  bool res = testImagesSetsUtilities() && testSetFromImage(); // && ... other tests
+  bool res = testImageFromSet() && testSetFromImage();
   trace.emphase() << ( res ? "Passed." : "Error." ) << endl;
   trace.endBlock();
   return res ? 0 : 1;
