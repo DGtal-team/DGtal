14a2026ec01ccaf1ecac84da4d52aa677ab497d2
David Coeurjolly <david.coeurjolly@liris.cnrs.fr>
2012-02-21 13:24:55 +0100

    Forcing Boost_FOUND to false

--- cmake/CheckDGtalDependencies.cmake
+++ cmake/CheckDGtalDependencies.cmake
@@ -7,14 +7,12 @@
 # -----------------------------------------------------------------------------
 set(Boost_USE_STATIC_LIBS   ON)
 set(Boost_USE_MULTITHREADED ON)
-
-
-
+set(Boost_FOUND FALSE)
 FIND_PACKAGE(Boost 1.40.0 REQUIRED COMPONENTS program_options)
 if ( Boost_FOUND )
   message(STATUS "Boost and boost_program_options found.")
   include_directories( ${Boost_INCLUDE_DIR} )
-   SET(DGtalLibDependencies ${DGtalLibDependencies} 
+  SET(DGtalLibDependencies ${DGtalLibDependencies} 
      ${Boost_LIBRAIRIES}  
      ${Boost_PROGRAM_OPTIONS_LIBRARY})
    SET(DGtalLibInc ${Boost_INCLUDE_DIR})
