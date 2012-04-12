f9b357f425b4ed46de99ab50804a831068f728eb
Jeremy Levallois <jeremy.levallois@liris.cnrs.fr>
2012-04-13 00:36:16 +0200

    Adding RealVector and RealPoint into Z2i & Z3i namespaces

--- src/DGtal/helpers/StdDefs.h
+++ src/DGtal/helpers/StdDefs.h
@@ -86,6 +86,8 @@ namespace DGtal
     typedef DigitalTopology< Adj8, Adj4 > DT8_4;
     typedef Space::Point Point;
     typedef Space::Vector Vector;
+    typedef Space::RealVector RealVector;
+    typedef Space::RealPoint RealPoint;
     typedef HyperRectDomain< Space > Domain; 
     typedef DigitalSetSelector< Domain, BIG_DS+HIGH_BEL_DS >::Type DigitalSet;
     typedef Object<DT4_8, DigitalSet> Object4_8;
@@ -140,7 +142,9 @@ namespace DGtal
     typedef DigitalTopology< Adj26, Adj6 > DT26_6;
     typedef Space::Point Point;
     typedef Space::Vector Vector;
-    typedef HyperRectDomain< Space > Domain; 
+    typedef Space::RealVector RealVector;
+    typedef Space::RealPoint RealPoint;
+    typedef HyperRectDomain< Space > Domain;
     typedef DigitalSetSelector< Domain, BIG_DS+HIGH_BEL_DS >::Type DigitalSet;
     typedef Object<DT6_18, DigitalSet> Object6_18;
     typedef Object<DT6_18, DigitalSet>::ComplementObject ComplementObject6_18;
