e9ecddece434a9682fa52134c5b72a53acd6fd4e
Jeremy Gaillard <jeremy.gaillard@insa-lyon.fr>
2012-06-19 14:03:11 +0200

    Test works, most of the functions too. Code needs clearing up. Still lacks vertexMap struct

---
+++ tests/topology/testBreadthFirstPropagation.cpp
@@ -0,0 +1,158 @@
+/**
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU Lesser General Public License as
+ *  published by the Free Software Foundation, either version 3 of the
+ *  License, or  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ **/
+
+/**
+ * @file testAdjacency.cpp
+ * @ingroup Tests
+ * @author Jérémy Gaillard (\c jeremy.gaillard@insa-lyon.fr )
+ *
+ * @date 2012/06/18
+ *
+ * Functions for testing objects as graph.
+ *
+ * This file is part of the DGtal library.
+ */
+ 
+ ///////////////////////////////////////////////////////////////////////////////
+#include <iostream>
+#include "DGtal/base/Common.h"
+#include "DGtal/io/boards/Board2D.h"
+#include "DGtal/io/Color.h"
+#include "DGtal/io/colormaps/GradientColorMap.h"
+#include "DGtal/shapes/Shapes.h"
+#include "DGtal/io/Color.h"
+#include "DGtal/io/colormaps/GradientColorMap.h"
+#include <set>
+#include <iterator>
+///////////////////////////////////////////////////////////////////////////////
+
+
+using namespace std;
+using namespace DGtal;
+
+///////////////////////////////////////////////////////////////////////////////
+// Functions for testing objects as graph.
+///////////////////////////////////////////////////////////////////////////////
+
+typedef ImageSelector < Z2i::Domain, int>::Type Image;
+bool testBreadthFirstPropagation()
+{
+  typedef Z2i::Point Point;
+  typedef Z2i::Domain Domain;
+  typedef Z2i::DigitalSet DigitalSet;
+  
+  Point p1( -50, -50 );
+  Point p2( 50, 50 );
+  Domain domain( p1, p2 );
+  Point c1( -2, -1 );
+  Point c1bis( -2, 4 );
+  Point c2( 5, 0 );
+  Point c3( -5, 4 );
+  Point c4( -14, 5 );
+  Point c5( -30, -15 );
+  Point c6( -10, -20 );
+  Point c7( 12, -1 );
+  DigitalSet shape_set( domain );
+  
+  Shapes<Domain>::addNorm2Ball( shape_set, c1, 9 );
+  Shapes<Domain>::addNorm1Ball( shape_set, c4, 9 );
+  Shapes<Domain>::addNorm1Ball( shape_set, c5, 10 );
+  Shapes<Domain>::addNorm2Ball( shape_set, c6, 12 );
+  Shapes<Domain>::addNorm1Ball( shape_set, c7, 4 );
+
+  Z2i::Object4_8 obj(Z2i::dt4_8, shape_set);
+  
+  GradientColorMap<int> cmap_grad( 0, shape_set.size() );
+  cmap_grad.addColor( Color( 0, 0, 200 ) );
+  cmap_grad.addColor( Color( 0, 0, 50 ) );
+  
+  Board2D board;
+  board << SetMode( domain.className(), "Paving" )
+  << domain
+  << SetMode( p1.className(), "Paving" );
+  
+  Image image = ImageFromSet<Image>::create(shape_set, 1);
+  
+  Z2i::Object4_8::ConstIterator it = obj.begin();
+  
+  Point start = *obj.begin();
+  
+  set<Point> visitedPoints;
+  queue<Point> pointsToCompute;
+  
+  visitedPoints.insert(start);
+  pointsToCompute.push(start);
+  
+  Point p;
+  int value = 2;
+  
+  image.setValue(start, value);
+  
+  typedef typename std::vector<Point>/*Z2i::Object4_8::SmallObject::DigitalSet*/ Container;
+  
+  while( !pointsToCompute.empty() )
+  {
+    value ++;
+    p = pointsToCompute.front();
+    pointsToCompute.pop();
+    Container neighbors;//( domain );
+    std::back_insert_iterator<Container> insertit ( neighbors );
+    obj.writeNeighbors(insertit, p);
+    for( typename Container::iterator it = neighbors.begin(); it != neighbors.end(); it++ )
+    {
+      if( visitedPoints.insert(*it).second == true )
+      {
+        pointsToCompute.push(*it);
+        image.setValue(*it, value);
+      }
+    }
+  }
+  
+  
+  string specificStyle = p1.className() + "/Paving";
+  
+  for ( DigitalSet::ConstIterator it = shape_set.begin();
+  it != shape_set.end();
+  ++it )
+  {
+    if( image(*it) >= 0 )
+    {
+      board << CustomStyle( specificStyle,
+        new CustomColors( Color::Black,
+        cmap_grad( image(*it) ) ) )
+      << *it;
+    }
+    else
+    {
+      board << CustomStyle( specificStyle,
+        new CustomColors( Color::Black,
+        cmap_grad( 0 ) ) )
+      << *it;
+    }
+  }
+  
+  board.saveEPS( "testGraph.eps");
+}
+
+int main( int argc, char** argv )
+{
+  testBreadthFirstPropagation();
+  return 0;
+}
+
+
+
+
