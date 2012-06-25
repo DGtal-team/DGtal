f29950c05a09dea633d0586cdb481f4f4d297430
Tristan Roussillon <tristan.roussillon@liris.cnrs.fr>
2012-06-25 18:05:29 +0200

    curvature, normal, tangent from GeometricalDCA

--- tests/geometry/curves/estimation/testSegmentComputerFunctor.cpp
+++ tests/geometry/curves/estimation/testSegmentComputerFunctor.cpp
@@ -36,6 +36,7 @@
 
 
 #include "DGtal/geometry/curves/ArithmeticalDSS.h"
+#include "DGtal/geometry/curves/GeometricalDCA.h"
 #include "DGtal/geometry/curves/estimation/SegmentComputerFunctor.h"
 
 #include "DGtal/io/boards/Board2D.h"
@@ -60,24 +61,15 @@ using namespace LibBoard;
  */
 template<typename DSSComputer>
 bool testTangentFromDSS(
-     const typename DSSComputer::ConstIterator& begin,
-     const typename DSSComputer::ConstIterator& end  )
+     const typename DSSComputer::ConstIterator& itb,
+     const typename DSSComputer::ConstIterator& ite  )
 {
 
 
   trace.info() << "feeding segment computer " << endl;
 
-  typename DSSComputer::ConstIterator i = begin;
   DSSComputer dss;  
-
-  if (i != end) {
-    dss.init(i);
-    ++i;
-    while ( (i!=end)
-          &&(dss.extendForward(i)) ) {
-      ++i;
-    }
-  }
+  DGtal::longestSegment( dss, itb, ite ); 
 
   trace.info() << dss << endl;
   trace.info() << endl;
@@ -124,6 +116,59 @@ bool testTangentFromDSS(
   return (nb == nbok); 
 }
 
+/**
+ * Estimations from DCA
+ *
+ */
+template<typename DCAComputer>
+bool testFromDCA(
+     const typename DCAComputer::ConstIterator& itb,
+     const typename DCAComputer::ConstIterator& ite  )
+{
+
+
+  trace.info() << "feeding segment computer " << endl;
+
+  DCAComputer dca;  
+  DGtal::longestSegment( dca, itb, ite ); 
+
+  trace.info() << dca << endl;
+  trace.info() << endl;
+
+  trace.info() << "building and using the functor " << endl;
+  const double epsilon = 0.00001;
+
+  int nb = 0; 
+  int nbok = 0; 
+
+  { //curvature
+    CurvatureFromDCAFunctor<DCAComputer> f; 
+    f.init(1.0); 
+    double q1 = f(dca);
+    f.init(0.1); 
+    double q2 = f(dca);    
+    trace.info() << "Curvature (h=1): " << q1 << std::endl; 
+    trace.info() << "Curvature (h=0.1): " << q2 << std::endl; 
+    nbok += ((q1 >= 0)&&(q1 < 1)&&(q2 >= 0)&&(q2 < 10)
+	     &&(std::abs(q2-(10*q1)) < epsilon))?1:0; 
+    nb++;
+    trace.info() << "(" << nbok << "/" << nb << ")" << std::endl; 
+  }
+
+  { //tangent
+    TangentFromDCAFunctor<DCAComputer> f; 
+    typedef typename TangentFromDCAFunctor<DCAComputer>::Quantity Quantity; 
+    typename DCAComputer::ConstIterator it = DGtal::getMiddleIterator(itb, ite); 
+    Quantity q1 = f( it, dca ); 
+    trace.info() << "Tangent: " << q1 << " == [PointVector] {1, 0} " << std::endl; 
+    nbok += (std::abs(q1[0]-1.0) < epsilon)
+      && (std::abs(q1[1]) < epsilon) ? 1 : 0; 
+    nb++;
+    trace.info() << "(" << nbok << "/" << nb << ")" << std::endl; 
+  }
+
+  return (nb == nbok); 
+}
 
 
 ///////////////////////////////////////////////////////////////////////////////
@@ -137,45 +182,83 @@ int main( int argc, char** argv )
     trace.info() << " " << argv[ i ];
   trace.info() << endl;
 
+  bool res = true; 
+
   //types
   typedef PointVector<2,int> Point;
-  typedef std::vector<Point> Range;
-  typedef Range::iterator ConstIterator;
-  typedef ArithmeticalDSS<ConstIterator,int,4> DSS4;  
-  typedef ArithmeticalDSS<ConstIterator,int,8> DSS8;  
 
-  //input points
-  Range curve4;
-  curve4.push_back(Point(0,0));
-  curve4.push_back(Point(1,0));
-  curve4.push_back(Point(1,1));
-  curve4.push_back(Point(2,1));
-  curve4.push_back(Point(3,1));
-  curve4.push_back(Point(3,2));
-  curve4.push_back(Point(4,2));
-  curve4.push_back(Point(5,2));
-  curve4.push_back(Point(6,2));
-  curve4.push_back(Point(6,3));
-  curve4.push_back(Point(7,3));
 
-  Range curve8;
-  curve8.push_back(Point(0,0));
-  curve8.push_back(Point(1,1));
-  curve8.push_back(Point(2,1));
-  curve8.push_back(Point(3,2));
-  curve8.push_back(Point(4,2));
-  curve8.push_back(Point(5,2));
-  curve8.push_back(Point(6,3));
-  curve8.push_back(Point(7,3));
-  curve8.push_back(Point(8,4));
-  curve8.push_back(Point(9,4));
-  curve8.push_back(Point(10,5));
+  //------------------------------------------ DSS
+  {
+    typedef std::vector<Point> Range;
+    typedef Range::iterator ConstIterator;
+    typedef ArithmeticalDSS<ConstIterator,int,4> DSS4;  
+    typedef ArithmeticalDSS<ConstIterator,int,8> DSS8;  
 
-  //tests
-  bool res = testTangentFromDSS<DSS4>(curve4.begin(), curve4.end())
-          && testTangentFromDSS<DSS8>(curve8.begin(), curve8.end())
-//add test for other functors
-;
+    //input points
+    Range curve4;
+    curve4.push_back(Point(0,0));
+    curve4.push_back(Point(1,0));
+    curve4.push_back(Point(1,1));
+    curve4.push_back(Point(2,1));
+    curve4.push_back(Point(3,1));
+    curve4.push_back(Point(3,2));
+    curve4.push_back(Point(4,2));
+    curve4.push_back(Point(5,2));
+    curve4.push_back(Point(6,2));
+    curve4.push_back(Point(6,3));
+    curve4.push_back(Point(7,3));
+
+    Range curve8;
+    curve8.push_back(Point(0,0));
+    curve8.push_back(Point(1,1));
+    curve8.push_back(Point(2,1));
+    curve8.push_back(Point(3,2));
+    curve8.push_back(Point(4,2));
+    curve8.push_back(Point(5,2));
+    curve8.push_back(Point(6,3));
+    curve8.push_back(Point(7,3));
+    curve8.push_back(Point(8,4));
+    curve8.push_back(Point(9,4));
+    curve8.push_back(Point(10,5));
+
+    //tests
+    res = res && testTangentFromDSS<DSS4>(curve4.begin(), curve4.end())
+      && testTangentFromDSS<DSS8>(curve8.begin(), curve8.end());
+  }
+  //------------------------------------------ DCA
+  {
+    typedef std::pair<Point,Point> Pair; 
+    typedef std::vector<Pair> Range;
+    typedef Range::const_iterator ConstIterator; 
+    typedef GeometricalDCA<ConstIterator> DCA;  
+
+    Range curve; 
+    curve.push_back(std::make_pair(Point(0,0),Point(0,1))); 
+    curve.push_back(std::make_pair(Point(1,0),Point(1,1))); 
+
+    curve.push_back(std::make_pair(Point(2,1),Point(2,2))); 
+    curve.push_back(std::make_pair(Point(3,1),Point(3,2))); 
+    curve.push_back(std::make_pair(Point(4,1),Point(4,2))); 
+
+    curve.push_back(std::make_pair(Point(5,2),Point(5,3))); 
+    curve.push_back(std::make_pair(Point(6,2),Point(6,3))); 
+    curve.push_back(std::make_pair(Point(7,2),Point(7,3))); 
+    curve.push_back(std::make_pair(Point(8,2),Point(8,3))); 
+    curve.push_back(std::make_pair(Point(9,2),Point(9,3))); 
+
+    curve.push_back(std::make_pair(Point(10,1),Point(10,2))); 
+    curve.push_back(std::make_pair(Point(11,1),Point(11,2))); 
+    curve.push_back(std::make_pair(Point(12,1),Point(12,2))); 
+
+    curve.push_back(std::make_pair(Point(13,0),Point(13,1))); 
+    curve.push_back(std::make_pair(Point(14,0),Point(14,1))); 
+
+    res = res && testFromDCA<DCA>(curve.begin(), curve.end())
+      && testFromDCA<DCA>(curve.begin()+2, curve.begin()+12);
+  }
+
+  //----------------------------------------------------------
   trace.emphase() << ( res ? "Passed." : "Error." ) << endl;
   trace.endBlock();
   
