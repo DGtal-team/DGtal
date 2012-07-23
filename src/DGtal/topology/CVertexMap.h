0bfe185945fa2ba78d7d670c1a92f39ce8b85807
David Coeurjolly <david.coeurjolly@liris.cnrs.fr>
2012-07-23 16:41:21 +0200

    markdown fix

--- src/DGtal/topology/CVertexMap.h
+++ src/DGtal/topology/CVertexMap.h
@@ -53,48 +53,48 @@ namespace DGtal
   /////////////////////////////////////////////////////////////////////////////
   // class CVertexMap
   /**
-    *  Description of \b concept '\b CVertexMap' <p>
-    * @ingroup Concepts
-    * \brief Aim: models of CVertexMap concept implement mapping
-    * between graph vertices and values.
-    *
-    * This concept is associated with concepts related to graph  in
-    * DGtal (CUndirectedSimpleLocalGraph and CUndirectedSimpleGraph)
-    *
-    * Associates values to vertices.
-    
- ### Associated types :
-    * - Vertex : specifies the type for an element of the domain (inner
-    *   type).
-    * - Value : specifies the type for a value (inner type).
-
-     
-###  Notation
-    * - \t X : A type that is a model of CVertexMap
-    * - \t x : Object of type X
-    * - \t v : Object of type Vertex
-    * - \t val : Object of type Value
-
- ### Definitions
- 
- 
- ###  Valid expressions and
-
-| Name      | Expression              | Type requirements                           | Return type | Precondition | Semantics                                 | Post condition | Complexity |
-|-----------+-------------------------+---------------------------------------------+-------------+--------------+-------------------------------------------+----------------+------------|
-| Set value | x.setValue(vertex, val) | vertex of type Vertex and val of type Value | void        |              | set a given value val to a given vertex v |                |            |
-| Accessor  | x(vertex)               | vertex of type Vertex                       | Value       |              | Access to the value at vertex             |                |            |
-
-    
-###  Invariants
+   *  Description of \b concept '\b CVertexMap' <p>
+   * @ingroup Concepts
+   * \brief Aim: models of CVertexMap concept implement mapping
+   * between graph vertices and values.
    *
-
-###  Models
+   * This concept is associated with concepts related to graph  in
+   * DGtal (CUndirectedSimpleLocalGraph and CUndirectedSimpleGraph)
+   *
+   * Associates values to vertices.
+   
+   ### Associated types :
+   * - Vertex : specifies the type for an element of the domain (inner
+   *   type).
+   * - Value : specifies the type for a value (inner type).
+   
+   
+   ###  Notation
+   * - \t X : A type that is a model of CVertexMap
+   * - \t x : Object of type X
+   * - \t v : Object of type Vertex
+   * - \t val : Object of type Value
+   
+   ### Definitions
+   
+   
+   ###  Valid expressions and
+   
+   | Name      | Expression              | Type requirements                           | Return type | Precondition | Semantics                                 | Post condition | Complexity |
+   |-----------|-------------------------|---------------------------------------------|-------------|--------------|-------------------------------------------|----------------|------------|
+   | Set value | x.setValue(vertex, val) | vertex of type Vertex and val of type Value | void        |              | set a given value val to a given vertex v |                |            |
+   | Accessor  | x(vertex)               | vertex of type Vertex                       | Value       |              | Access to the value at vertex             |                |            |
+   
+   
+   ###  Invariants
+   *
+   
+   ###  Models
    * ImageContainerBySTLVector, ImageContainerBySTLMap
    * ImageContainerByITKImage, ImageContainerByHashTree
    * Any adapted type from STLMapToVertexMapAdapter  
-        
- ### Notes###
+   
+   ### Notes###
    */
   template <typename T>
   struct CVertexMap:
