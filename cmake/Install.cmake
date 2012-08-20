bbd822807f3adac1a46c164d43fcfb799a840df9
David Coeurjolly <david.coeurjolly@liris.cnrs.fr>
2012-08-20 09:26:22 +0200

    Minor edit on cmakelists files

--- cmake/Install.cmake
+++ cmake/Install.cmake
@@ -16,17 +16,15 @@ INCLUDE(${CMAKE_MODULE_PATH}/DGtalConfigGenerator.cmake)
 
 
 if(WITH_VISU3D_OGRE)
-#--- Ogre viewer ressources installation
-install(DIRECTORY "${PROJECT_SOURCE_DIR}/src/DGtal/io/viewers/OGRE/Ressources" 
-  DESTINATION "${INSTALL_INCLUDE_DIR}/DGtal/io/viewers/OGRE/Ressources"
-  FILES_MATCHING PATTERN "*.*")
-message(status "Hi")
-configure_file(${PROJECT_SOURCE_DIR}/src/DGtal/io/viewers/OGRE/Ressources/plugins.cfg.in 
-  ${INSTALL_INCLUDE_DIR}/DGtal/io/viewers/OGRE/Ressources/plugins.cfg)
-install (CODE "set(INSTALL_INCLUDE_DIR ${INSTALL_INCLUDE_DIR})
+  #--- Ogre viewer ressources installation
+  install(DIRECTORY "${PROJECT_SOURCE_DIR}/src/DGtal/io/viewers/OGRE/Ressources" 
+    DESTINATION "${INSTALL_INCLUDE_DIR}/DGtal/io/viewers/OGRE/Ressources"
+    FILES_MATCHING PATTERN "*.*")
+
+  install (CODE "set(INSTALL_INCLUDE_DIR ${INSTALL_INCLUDE_DIR})
 configure_file(  ${PROJECT_SOURCE_DIR}/src/DGtal/io/viewers/OGRE/RequirementsInstall.h.in
   ${INSTALL_INCLUDE_DIR}/DGtal/io/viewers/OGRE/Requirements.h )")
-message(status "Bye")
+
 endif(WITH_VISU3D_OGRE)
 
 
