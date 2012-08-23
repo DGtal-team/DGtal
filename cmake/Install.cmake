f367f59be997c0e235bb7087f224c8f5aef9196e
Anis Benyoub <anisbenyoub@ubuntu.(none)>
2012-08-23 05:35:28 -0700

    Minor fix

--- cmake/Install.cmake
+++ cmake/Install.cmake
@@ -13,8 +13,7 @@ install(DIRECTORY "${PROJECT_BINARY_DIR}/src/DGtal/"
   FILES_MATCHING PATTERN "*.*h")
 INCLUDE(${CMAKE_MODULE_PATH}/DGtalConfigGenerator.cmake)
 
-configure_file(  ${PROJECT_SOURCE_DIR}/src/DGtal/io/viewers/OGRE/Ressources/plugins.cfg.in
-  ${INSTALL_INCLUDE_DIR}/DGtal/io/viewers/OGRE/Ressources/plugins.cfg )
+
 
 if(WITH_VISU3D_OGRE)
   #--- Ogre viewer ressources installation
@@ -23,9 +22,10 @@ if(WITH_VISU3D_OGRE)
     FILES_MATCHING PATTERN "*.*")
 
   install (CODE "set(INSTALL_INCLUDE_DIR ${INSTALL_INCLUDE_DIR})
-configure_file(  ${PROJECT_SOURCE_DIR}/src/DGtal/io/viewers/OGRE/RequirementsInstall.h.in
-  ${INSTALL_INCLUDE_DIR}/DGtal/io/viewers/OGRE/Requirements.h )")
-
+  configure_file(  ${PROJECT_SOURCE_DIR}/src/DGtal/io/viewers/OGRE/RequirementsInstall.h.in
+    ${INSTALL_INCLUDE_DIR}/DGtal/io/viewers/OGRE/Requirements.h )")
+  configure_file(  ${PROJECT_SOURCE_DIR}/src/DGtal/io/viewers/OGRE/Ressources/plugins.cfg.in
+    ${INSTALL_INCLUDE_DIR}/DGtal/io/viewers/OGRE/Ressources/plugins.cfg )
 endif(WITH_VISU3D_OGRE)
 
 
