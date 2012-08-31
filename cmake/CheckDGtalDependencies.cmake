da868f8c24cb7b72308b2df1f86224f8767b37af
David Coeurjolly <david.coeurjolly@liris.cnrs.fr>
2012-08-31 14:31:47 +0200

    Cmake / boost upgrade

--- cmake/CheckDGtalDependencies.cmake
+++ cmake/CheckDGtalDependencies.cmake
@@ -9,13 +9,14 @@ set(Boost_USE_STATIC_LIBS   ON)
 set(Boost_USE_MULTITHREADED ON)
 set(Boost_USE_STATIC_RUNTIME OFF)
 set(Boost_FOUND FALSE)
-FIND_PACKAGE(Boost 1.42.0 REQUIRED COMPONENTS program_options)
+FIND_PACKAGE(Boost 1.42.0 REQUIRED COMPONENTS program_options system)
 if ( Boost_FOUND )
-  message(STATUS "Boost and boost_program_options found.")
+  message(STATUS "Boost and boost components found.")
   include_directories( ${Boost_INCLUDE_DIRS} )
   SET(DGtalLibDependencies ${DGtalLibDependencies} 
      ${Boost_LIBRAIRIES}  
-     ${Boost_PROGRAM_OPTIONS_LIBRARY})
+     ${Boost_PROGRAM_OPTIONS_LIBRARY}
+     ${Boost_SYSTEM_LIBRARY})
    SET(DGtalLibInc ${Boost_INCLUDE_DIRS})
 endif( Boost_FOUND )
 
