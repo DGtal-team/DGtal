ec9abbd91c36abd4377db2bf2c212ca9c0807796
Kerautret <kerautre@loria.fr>
2012-09-01 14:16:33 +0200

    Cherrypicking from Bertrand

--- cmake/Install.cmake
+++ cmake/Install.cmake
@@ -24,8 +24,11 @@ if(WITH_VISU3D_OGRE)
   install (CODE "set(INSTALL_INCLUDE_DIR ${INSTALL_INCLUDE_DIR})
   configure_file(  ${PROJECT_SOURCE_DIR}/src/DGtal/io/viewers/OGRE/RequirementsInstall.h.in
     ${INSTALL_INCLUDE_DIR}/DGtal/io/viewers/OGRE/Requirements.h )")
-install(CODE  "configure_file(  ${PROJECT_SOURCE_DIR}/src/DGtal/io/viewers/OGRE/Ressources/plugins.cfg.in
+  install(CODE  "configure_file(  ${PROJECT_SOURCE_DIR}/src/DGtal/io/viewers/OGRE/Ressources/plugins.cfg.in
     ${INSTALL_INCLUDE_DIR}/DGtal/io/viewers/OGRE/Ressources/plugins.cfg )")
+  install(CODE  "configure_file(  ${PROJECT_SOURCE_DIR}/src/DGtal/io/viewers/OGRE/Ressources/info.plist.in
+    ${INSTALL_INCLUDE_DIR}/DGtal/io/viewers/OGRE/Ressources/info.plist.in )")
+  
 endif(WITH_VISU3D_OGRE)
 
 
