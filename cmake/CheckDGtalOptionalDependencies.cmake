9ab3b9fbd7fb49203c1eea4dbe014f97470ccb56
David Coeurjolly <david.coeurjolly@liris.cnrs.fr>
2012-09-24 13:02:01 +0200

    ObjC in cmake again

--- cmake/CheckDGtalOptionalDependencies.cmake
+++ cmake/CheckDGtalOptionalDependencies.cmake
@@ -331,7 +331,7 @@ ENDIF( WITH_COIN3D-SOQT OR WITH_QGLVIEWER)
 # -----------------------------------------------------------------------------
 set(OGRE_FOUND_DGTAL 0)
 IF(WITH_OGRE)
-  INCLUDE("${CMAKe_MODULE_PATH}/CheckOBJC.cmake")
+  INCLUDE("${CMAKE_MODULE_PATH}/CheckOBJC.cmake")
   IF( NOT OBJC_SUPPORT)
     MESSAGE(ERROR "Your compiler does not support ObjectC++ language. ObjectiveC++ is mandatory for OGRE Viewer. Change yout compiler of disable WITH_OGRE.")
   ENDIF()
