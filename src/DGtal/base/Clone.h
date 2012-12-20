b8f50e4cbefd8f352d49674cead08a569e5b7695
Jacques-Olivier Lachaud <jacques-olivier.lachaud@univ-savoie.fr>
2012-12-20 16:46:03 +0100

    Clone now manages CountedPtr and CowPtr automatically. Update doc

--- src/DGtal/base/Clone.h
+++ src/DGtal/base/Clone.h
@@ -42,6 +42,8 @@
 // Inclusions
 #include <iostream>
 #include "DGtal/base/Common.h"
+#include "DGtal/base/CountedPtr.h"
+#include "DGtal/base/CowPtr.h"
 //////////////////////////////////////////////////////////////////////////////
 
 namespace DGtal
@@ -74,8 +76,8 @@ namespace DGtal
      @see ConstAlias
 
      It can be used as follows. Consider this simple example where
-     class A is a big object. Then we define two classes B1 and B2
-     that uses some instance of A. 
+     class \e A is a big object. Then we define three classes \e B1,
+     \e B2 and \e B3, that uses some instance of \e A.
      
      @code
      const int N = 10000;
@@ -83,22 +85,34 @@ namespace DGtal
        int table[N];
      };
 
-     // Both B1 and B2 uses A, but we do not know if A will be copied
-     // or just const-referenced by only looking at the declaration of
+     // Each B1, B2 or B3 uses A, but we do not know if A will be copied
+     // or just referenced by only looking at the declaration of
      // the method. Generally the ambiguity is removed by adding
      // comments or, for the experienced developper, by looking at
      // other parts of the code.
+
+     // Only aliasing, but for a long lifetime.
      struct B1 {
-       B1( const A & a ) // ambiguous, cost is O(1) here
+       B1( const A & a ) // ambiguous, cost is O(1) here and lifetime of a should exceed constructor.
        : myA( a ) {}
      ...
        const A & myA;
      };
+     // Copying as data member (stack or heap depending on B2 instance)
      struct B2 {
        B2( const A & a ) // ambiguous, cost is O(N) here
        : myA( a ) {}
      ...
        A myA;
+     };
+     // Copying on heap and data member pointing on it.
+     struct B3 {
+       B3( const A & a ) // ambiguous, cost is O(N) here
+       { myA = new A( a ); }
+       ~B3() 
+       { if ( myA != 0 ) delete myA; }
+     ...
+       A* myA;
      };
      @endcode
 
@@ -110,23 +124,53 @@ namespace DGtal
      reasons. The classes above may be rewritten as follows.
      
      @code
+     // Aliasing for a long lifetime is visible.
      struct B1 {
        B1( ConstAlias<A> a ) // not ambiguous, cost is O(1) here and lifetime of a should be long enough
        : myA( a ) {}
      ...
-       const A & myA;
+       const A & myA; 
+       // or Const A* myA;
      };
+     // Cloning as data member is visible.
      struct B2 {
        B2( Clone<A> a ) // not ambiguous, cost is O(N) here and lifetime of a is whatever.
        : myA( a ) {}
      ...
        A myA;
      };
+     // Cloning on the heap requires call to allocate(), so that the user remembers calling \c delete.
+     struct B3_v1 {
+       B3_v1( Clone<A> a ) // not ambiguous, cost is O(N) here and lifetime of a is whatever.
+       : myA( a.allocate() ) {}
+       ~B3_v1() { if ( myA != 0 ) delete myA; }
+     ...
+       A* myA;
+     };
+     // Cloning on the heap with CountedPtr mechanism is straightforward.
+     struct B3_v2 {
+       B3_v2( Clone<A> a ) // not ambiguous, cost is O(N) here and lifetime of a is whatever.
+       : myA( a ) {}
+       ~B3_v2() {} // CountedPtr takes care of delete.
+     ...
+       CountedPtr<A> myA;
+     };
+     // Cloning on the heap with CowPtr mechanism is straightforward.
+     struct B3_v3 {
+       B3_v3( Clone<A> a ) // not ambiguous, cost is O(N) here and lifetime of a is whatever.
+       : myA( a ) {}
+       ~B3_v3() {} // CountedPtr takes care of delete.
+     ...
+       CowPtr<A> myA;
+     };
      ...
      A a1;
      B1 b( a1 );    // do not duplicate a1
      B2 bb( a1 );   // duplicate a1
      B2 bbb( &a1 ); // also duplicate a1 !
+     B3_v1 c1( a1 ); // duplicate a1 on the heap
+     B3_v2 c1( a1 ); // duplicate a1 on the heap
+     B3_v3 c1( a1 ); // duplicate a1 on the heap
      @endcode
 
      A last question could be why are we not just passing the instance
@@ -137,15 +181,17 @@ namespace DGtal
      the compiler will always do it.
 
      @code
-     struct B3 {
-       B3( A a ) // not ambiguous, but cost is O(2N) here. 
+     struct B4 {
+       B4( A a ) // not ambiguous, but cost is O(2N) here. 
        : myA( a ) {}
      ...
        A myA;
      };
      A a1;
-     B3 b3( a1 ) // The object \a a1 is copied once on the heap as the parameter \a a, and once as the member \a b3.myA.
+     B4 b4( a1 ) // The object \a a1 is copied once on the heap as the parameter \a a, and once as the member \a b3.myA.
      @endcode
+
+     @note Clone have no copy constructor. 
 
      @note The user should not used Clone<T> for data members (in
      fact, he cannot), only as a type for parameters.
@@ -206,6 +252,19 @@ namespace DGtal
         @return a pointer on the instance of T allocated on the process heap.
     */
     T* allocate() const;
+
+    /**
+       Cast operator to a CountedPtr<T> instance. This is only at this moment that
+       the object is duplicated (and only once).  This allows things like: CountedPtr<A> a2 = a1;
+       where a1 is of type Clone<A>. It also allows CowPtr<A> a2 = a1;
+    */
+    operator CountedPtr<T>() const;
+    // /**
+    //    Cast operator to a CowPtr<T> instance. This is only at this moment that
+    //    the object is duplicated (and only once).  This allows things like: CowPtr<A> a2 = a1;
+    //    where a1 is of type Clone<A>.
+    // */
+    // operator CowPtr<T>() const;
 
     // ------------------------- Protected Datas ------------------------------
   private:
