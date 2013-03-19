4d314216e5d82fcf286ad436fb37162ded7df277
Jacques-Olivier Lachaud <jacques-olivier.lachaud@univ-savoie.fr>
2013-03-19 17:13:50 +0100

    Move volDistanceTraversal to graph package. Update doc.

--- examples/graph/volDistanceTraversal.cpp
+++ examples/graph/volDistanceTraversal.cpp
@@ -6,7 +6,7 @@
  *
  * @date 2012/02/06
  *
- * An example file named qglViewer.
+ * An example file named volDistanceTraversal.
  *
  * This file is part of the DGtal library.
  */
@@ -20,7 +20,7 @@
 #include "DGtal/base/BasicFunctors.h"
 #include "DGtal/kernel/CanonicSCellEmbedder.h"
 #include "DGtal/helpers/StdDefs.h"
-#include "DGtal/graph/DistanceVisitor.h"
+#include "DGtal/graph/DistanceBreadthFirstVisitor.h"
 #include "DGtal/topology/DigitalSurface.h"
 #include "DGtal/topology/LightImplicitDigitalSurface.h"
 #include "DGtal/geometry/volumes/distance/ExactPredicateLpSeparableMetric.h"
@@ -42,9 +42,10 @@ using namespace Z3i;
 
 void usage( int, char** argv )
 {
-  std::cerr << "Usage: " << argv[ 0 ] << " <fileName.vol> <minT> <maxT>" << std::endl;
-  std::cerr << "\t - displays the boundary of the shape stored in vol file <fileName.vol>." << std::endl;
-  std::cerr << "\t - voxel v belongs to the shape iff its value I(v) follows minT <= I(v) <= maxT." << std::endl;
+  std::cerr << "Usage: " << argv[ 0 ] << " <fileName.vol> <minT> <maxT> <idxP>" << std::endl;
+  std::cerr << "\t - displays the Euclidean distance to the specified surfel on the boundary of the shape stored in vol file <fileName.vol>." << std::endl;
+  std::cerr << "\t - the shape is defined implicitly: voxel v belongs to the shape iff its value I(v) follows minT < I(v) <= maxT." << std::endl;
+  std::cerr << "\t - starts from the <idxP>-th surfel or first one if invalid." << std::endl;
 }
 
 int main( int argc, char** argv )
@@ -57,6 +58,7 @@ int main( int argc, char** argv )
   std::string inputFilename = argv[ 1 ];
   unsigned int minThreshold = atoi( argv[ 2 ] );
   unsigned int maxThreshold = atoi( argv[ 3 ] );
+  unsigned int idxP = (argc <= 4) ? 0 : atoi( argv[ 4 ] );
 
   //! [volDistanceTraversal-readVol]
   trace.beginBlock( "Reading vol file into an image." );
@@ -97,6 +99,10 @@ int main( int argc, char** argv )
     new MyDigitalSurfaceContainer( ks, set3d, surfAdj, bel );
   MyDigitalSurface digSurf( ptrSurfContainer ); // acquired
   trace.endBlock();
+  // Find first bel.
+  MyDigitalSurface::ConstIterator it = digSurf.begin();
+  for ( idxP = idxP % digSurf.size(); idxP != 0; --idxP ) ++it;
+  bel = *it;
   //! [volDistanceTraversal-SetUpDigitalSurface]
 
   //! [volDistanceTraversal-ExtractingSurface]
@@ -108,7 +114,7 @@ int main( int argc, char** argv )
 
   typedef std::binder1st< Distance > DistanceToPoint; 
   typedef Composer<SCellEmbedder, DistanceToPoint, Scalar> VertexFunctor;
-  typedef DistanceVisitor< MyDigitalSurface, VertexFunctor, std::set<SCell> > 
+  typedef DistanceBreadthFirstVisitor< MyDigitalSurface, VertexFunctor, std::set<SCell> > 
     MyDistanceVisitor;
   typedef MyDistanceVisitor::Node MyNode;
   typedef MyDistanceVisitor::Scalar MySize;
