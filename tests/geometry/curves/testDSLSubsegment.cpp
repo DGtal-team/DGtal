50a620f8687655abced99c656e097b79cd23fd64
Isabelle Sivignon <isabelle.sivignon@gipsa-lab.grenoble-inp.fr>
2013-05-28 22:54:39 +0200

    add convex hull with har peled algorithm + trials for an algorithm using CH computation only

--- tests/geometry/curves/testDSLSubsegment.cpp
+++ tests/geometry/curves/testDSLSubsegment.cpp
@@ -52,7 +52,7 @@ using namespace DGtal;
 // Functions for testing class DSLSubsegment.
 ///////////////////////////////////////////////////////////////////////////////
 
-//#define CHECK_RES
+#define CHECK_RES
 
 template <typename Integer>
 bool testDSLSubsegment( unsigned int nbtries, Integer modb, Integer modx)
@@ -135,15 +135,15 @@ bool testDSLSubsegment( unsigned int nbtries, Integer modb, Integer modx)
 		  
 		  if(B[0]-A[0] < 2*b) // reject easy cases when the segment contains a period of the DSL
 		    {
-		      std::cout << "(" << a << "," << b << "," << mu << ")\n" ; //(" << alpha << "," << beta << ")" << std::endl;
-		      trace.info() << "Points " << A << " " << B << std::endl;
+		      //std::cout << "(" << a << "," << b << "," << mu << ")\n" ; //(" << alpha << "," << beta << ")" << std::endl;
+		      //trace.info() << "Points " << A << " " << B << std::endl;
 		      nb++;
 		      timeBeginSubseg = clock();
 		      DSLSubseg D(a,b,mu,A,B);
 		      timeEndSubseg = clock();
 		      timeTotalSubseg += ((double)timeEndSubseg-(double)timeBeginSubseg)/(((double)CLOCKS_PER_SEC)/1000);
 		      
-		      std::cout << "res = " << "(" << D.aa << "," << D.bb << "," << D.Nu << ")" << std::endl;
+		      //std::cout << "res = " << "(" << D.aa << "," << D.bb << "," << D.Nu << ")" << std::endl;
 
 		  // // DSLSubsegment algorithm using floating points
 		  // timeBeginSubsegD = clock();
@@ -164,12 +164,12 @@ bool testDSLSubsegment( unsigned int nbtries, Integer modb, Integer modx)
 
 		  // Computation of the reduced parameters of Charrier & Buzer
 		      
-		      timeBeginCH = clock();
-		      DSLSubseg DD(a,b,mu,A,B,1);
-		      assert(D.aa == DD.aa && D.bb == DD.bb && D.Nu == DD.Nu);
-		      timeEndCH = clock();
-		      timeTotalCH += ((double)timeEndCH-(double)timeBeginCH)/(((double)CLOCKS_PER_SEC)/1000);
-		    }
+		      // timeBeginCH = clock();
+		      // DSLSubseg DD(a,b,mu,A,B,1);
+		      // assert(D.aa == DD.aa && D.bb == DD.bb && D.Nu == DD.Nu);
+		      // timeEndCH = clock();
+		      // timeTotalCH += ((double)timeEndCH-(double)timeBeginCH)/(((double)CLOCKS_PER_SEC)/1000);
+		      
 		  
 #ifdef CHECK_RES
 		  // Check if the result is ok comparing with ArithmeticalDSS recognition algorithm
@@ -197,7 +197,7 @@ bool testDSLSubsegment( unsigned int nbtries, Integer modb, Integer modx)
 #endif CHECK_RES
 		  
 		  
-		  
+		    }
 		  
 		}
 	      
@@ -227,7 +227,7 @@ int main( int argc, char** argv )
   typedef DGtal::int64_t Integer;
   
 
-  unsigned int nbtries = ( argc > 1 ) ? atoi( argv[ 1 ] ) :200;
+  unsigned int nbtries = ( argc > 1 ) ? atoi( argv[ 1 ] ) :100;
   
   
   Integer modb = 10000000000;
