7ed9b6b5e6afa7a649041211f43ff5acd2ef632b
troussil <tristan.roussillon@liris.cnrs.fr>
2013-07-02 16:20:54 +0200

    adding iterator services to arithmetical DSSs

--- tests/geometry/curves/testArithmeticalDSS.cpp
+++ tests/geometry/curves/testArithmeticalDSS.cpp
@@ -40,8 +40,9 @@ using namespace DGtal;
 // Functions for testing class ArithmeticalDSS.
 ///////////////////////////////////////////////////////////////////////////////
 /**
- * Example of a test. To be completed.
- *
+ * Test of the main operators
+ * @tparam DSS a model of arithmetical DSS, 
+ * either naive or standard 
  */
 template <typename DSS>
 bool mainTest()
@@ -51,17 +52,17 @@ bool mainTest()
   unsigned int nbok = 0;
   unsigned int nb = 0;
   
-  trace.beginBlock ( "Testing block ..." );
+  trace.beginBlock ( "Main operators..." );
 
   //operateur constructeur, copie, affectation
   trace.info() << "constructor, copy, assignement, equality" << std::endl; 
 
-  DSS dss(0, 1, 0, 1, 
+  DSS dss(0, 1, 0,  
 	  Point(0,0), Point(1,0), 
 	  Point(0,0), Point(1,0),
 	  Point(0,0), Point(1,0) ); 
   DSS dss2 = dss; 
-  DSS dss3(1, 1, 0, 2, 
+  DSS dss3(1, 1, 0,  
 	  Point(0,0), Point(1,1), 
 	  Point(0,0), Point(1,1),
 	  Point(0,0), Point(1,1) );
@@ -69,7 +70,7 @@ bool mainTest()
   dss3 = dss2 = dss; 
 
   //egalite, difference
-  DSS dss5(0, -1, 0, 1, 
+  DSS dss5(0, -1, 0, 
 	  Point(1,0), Point(0,0), 
 	  Point(1,0), Point(0,0),
 	  Point(1,0), Point(0,0) ); 
@@ -82,9 +83,6 @@ bool mainTest()
   nb++;
   trace.info() << "(" << nbok << "/" << nb << ") "
 	       << std::endl;
-
-  //sortie standard
-  trace.info() << dss << std::endl; 
 
   //validite
   trace.info() << "valid dss" << std::endl; 
@@ -131,17 +129,16 @@ bool mainTest()
   trace.info() << "(" << nbok << "/" << nb << ") "
   	       << std::endl;
 
-  DSS dss7(5, 8, 0, 13, 
-	  Point(0,0), Point(8,5), 
-	  Point(0,0), Point(8,5),
-	  Point(4,1), Point(4,1) ); 
+  DSS dss7(Point(0,0), Point(8,5)); 
   
   trace.info() << "remainder, position, tests" << std::endl; 
+  trace.info() << dss7 << std::endl; 
+
   if ( (dss7.isValid()) 
-       && (dss7.r( Point(8,5) ) == 0)
-       &&(dss7.r( Point(16,10) ) == 0)
-       &&(dss7.r( Point(3,2) ) == -1)
-       &&(dss7.r( Point(5,3) ) == 1) )
+       && (dss7.remainder( Point(8,5) ) == 0)
+       &&(dss7.remainder( Point(16,10) ) == 0)
+       &&(dss7.remainder( Point(3,2) ) == -1)
+       &&(dss7.remainder( Point(5,3) ) == 1) )
     nbok++; 
   nb++; 
 
@@ -185,8 +182,89 @@ bool mainTest()
 }
 
 ///////////////////////////////////////////////////////////////////////////////
-// Standard services - public :
+/**
+ * Test of the range services 
+ * @param dss an instance of DSS
+ * @tparam DSS a model of arithmetical DSS, 
+ * either naive or standard 
+ */
+template <typename DSS>
+bool rangeTest(const DSS& dss)
+{
+  typedef typename DSS::Point Point; 
 
+  unsigned int nbok = 0;
+  unsigned int nb = 0;
+  
+  trace.beginBlock ( "Range/Iterator services..." );
+  trace.info() << dss << std::endl; 
+
+  if (dss.isValid())
+    nbok++; 
+  nb++; 
+
+  trace.info() << "(" << nbok << "/" << nb << ") "
+  	       << std::endl;
+
+  {//forward pass  
+    typedef typename DSS::ConstIterator I; 
+    BOOST_CONCEPT_ASSERT(( boost_concepts::ReadableIteratorConcept<I> )); 
+    BOOST_CONCEPT_ASSERT(( boost_concepts::BidirectionalTraversalConcept<I> ));
+    bool res = true; 
+    int c = 0; 
+    for (I it = dss.begin(), itEnd = dss.end(); 
+	 ( (it != itEnd)&&(res)&&(c<100) ); 
+	 ++it, ++c)
+      {
+	trace.info() << *it << " ";  
+	if ( !dss(*it) )
+	  res = false; 
+      }
+    trace.info() << " : " << c << " points " << std::endl; 
+    trace.info() << std::endl; 
+
+    if ( (res)&&(c == (dss.omega()+1))
+	 &&(*dss.begin() == dss.back())
+	 &&(*--dss.end() == dss.front()) ) 
+      nbok++; 
+    nb++;
+
+  trace.info() << "(" << nbok << "/" << nb << ") "
+  	       << std::endl;
+  }
+
+  {//backward pass
+    typedef typename DSS::ConstReverseIterator I; 
+    bool res = true; 
+    int c = 0; 
+    for (I it = dss.rbegin(), itEnd = dss.rend(); 
+	 ( (it != itEnd)&&(res)&&(c<100) ); 
+	 ++it, ++c)
+      {
+	trace.info() << *it << " ";  
+	if ( !dss(*it) )
+	  res = false; 
+      }
+    trace.info() << " : " << c << " points " << std::endl; 
+    trace.info() << std::endl; 
+
+    if ( (res)&&(c == (dss.omega()+1))
+	 &&(*dss.rbegin() == dss.front())
+	 &&(*--dss.rend() == dss.back()) ) 
+      nbok++; 
+    nb++;
+
+  trace.info() << "(" << nbok << "/" << nb << ") "
+  	       << std::endl;
+  }
+
+  trace.endBlock();
+  
+  return nbok == nb;
+}
+
+
+///////////////////////////////////////////////////////////////////////////////
 int main( int argc, char** argv )
 {
   trace.beginBlock ( "Testing class ArithmeticalDSS" );
@@ -195,9 +273,62 @@ int main( int argc, char** argv )
     trace.info() << " " << argv[ i ];
   trace.info() << endl;
 
+  //main operator
   bool res = mainTest<DGtal::ArithmeticalDSS<DGtal::int32_t> >()
-    && mainTest<DGtal::ArithmeticalDSS<DGtal::int32_t, DGtal::BigInteger, 4> >()
-    ;
+    && mainTest<DGtal::ArithmeticalDSS<DGtal::int32_t, DGtal::BigInteger, 4> >(); 
+
+  //range services for 8 adjacency
+  {
+    typedef DGtal::ArithmeticalDSS<DGtal::int32_t> DSS8; 
+    typedef DSS8::Point Point; 
+
+    DSS8 dss0(Point(0,0), Point(8,5)); 
+    DSS8 dss1(Point(0,0), Point(5,8)); 
+    DSS8 dss2(Point(0,0), Point(-5,8)); 
+    DSS8 dss3(Point(0,0), Point(-8,5));
+    DSS8 dss4(Point(0,0), Point(-8,-5)); 
+    DSS8 dss5(Point(0,0), Point(-5,-8)); 
+    DSS8 dss6(Point(0,0), Point(5,-8)); 
+    DSS8 dss7(Point(0,0), Point(8,-5)); 
+
+    res = res 
+      && rangeTest(dss0)
+      && rangeTest(dss1)
+      && rangeTest(dss2)
+      && rangeTest(dss3)
+      && rangeTest(dss4)
+      && rangeTest(dss5)
+      && rangeTest(dss6)
+      && rangeTest(dss7)
+      ;
+  }
+
+  //range services for 4 adjacency
+  {
+    typedef DGtal::ArithmeticalDSS<DGtal::int32_t, DGtal::int32_t, 4> DSS4; 
+    typedef DSS4::Point Point; 
+    
+    DSS4 dss0(Point(0,0), Point(8,5)); 
+    DSS4 dss1(Point(0,0), Point(5,8)); 
+    DSS4 dss2(Point(0,0), Point(-5,8)); 
+    DSS4 dss3(Point(0,0), Point(-8,5));
+    DSS4 dss4(Point(0,0), Point(-8,-5)); 
+    DSS4 dss5(Point(0,0), Point(-5,-8)); 
+    DSS4 dss6(Point(0,0), Point(5,-8)); 
+    DSS4 dss7(Point(0,0), Point(8,-5)); 
+
+    res = res 
+      && rangeTest(dss0)
+      && rangeTest(dss1)
+      && rangeTest(dss2)
+      && rangeTest(dss3)
+      && rangeTest(dss4)
+      && rangeTest(dss5)
+      && rangeTest(dss6)
+      && rangeTest(dss7)
+      ;
+  }
+
   trace.emphase() << ( res ? "Passed." : "Error." ) << endl;
   trace.endBlock();
   return res ? 0 : 1;
