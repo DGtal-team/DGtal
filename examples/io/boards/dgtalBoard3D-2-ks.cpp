d7df243fd907b00c6fb85db680fffc8a1cfe7f1c
Aline Martin <aline.martin@insa-lyon.fr>
2013-07-25 17:55:43 +0200

    #export materials and clipping planes
    now the clipping planes are taken into account to display or not the
    figures, except themselves- the quadD3D list -

--- examples/io/boards/dgtalBoard3D-2-ks.cpp
+++ examples/io/boards/dgtalBoard3D-2-ks.cpp
@@ -42,56 +42,62 @@ using namespace Z3i;
 
 int main( )
 {
-  Board3D<> board;
 
-  KSpace K;
-  Point plow(0,0,0);
+
+    KSpace K;
+  Point plow(0,0,0);  
   Point pup(1,1,0);
   Domain domain( plow, pup );
-  K.init( plow, pup, true );
+  K.init( plow, pup, true ); 
+
+  Board3D<Space, KSpace> board(K);
 
   board << SetMode3D( domain.className(), "Paving" );
-  //board << domain;
+  board << domain;
 
-  // drawing cells of dimension 3
+  // drawing cells of dimension 3  
 
   SCell v2 = K.sSpel( Point( 1, 0, 0 ), KSpace::POS ); // +v
   SCell v3 = K.sSpel( Point( 0, 1, 0 ), KSpace::POS ); // +v
   SCell v4 = K.sSpel( Point( 1, 1, 0 ), KSpace::NEG ); // +v
+  //! [SetKSIllustrationMode3D]
   SCell v = K.sSpel( Point( 0, 0, 0 ), KSpace::POS ); // +v
   board << SetMode3D( v.className(), "Illustration" );
-
-  // board << v << v2 << v3;
-
-
+  //! [SetKSIllustrationMode3D]
+  
+  board << v << v2 << v3;
+  
+  
   // Surfel of Voxel (0,0)
-  SCell sx = K.sIncident( v, 0, true ); // surfel further along x
+  //! [KSIllustrationModeTransformed]
+  SCell sx = K.sIncident( v, 0, true ); // surfel further along x  
   DGtal::TransformedSurfelPrism tsx (sx, v);
-
-  SCell sy = K.sIncident( v, 1, true ); // surfel further along y
-  SCell sz = K.sIncident( v, 2, true ); // surfel further along z
-  SCell sxn = K.sIncident( v, 0, false ); // surfel further along x
-  SCell syn = K.sIncident( v, 1, false ); // surfel further along y
-  SCell szn = K.sIncident( v, 2, false ); // surfel further along z
-
+  //! [KSIllustrationModeTransformed]
+  
+  SCell sy = K.sIncident( v, 1, true ); // surfel further along y  
+  SCell sz = K.sIncident( v, 2, true ); // surfel further along z  
+  SCell sxn = K.sIncident( v, 0, false ); // surfel further along x  
+  SCell syn = K.sIncident( v, 1, false ); // surfel further along y  
+  SCell szn = K.sIncident( v, 2, false ); // surfel further along z  
+  
   // Resizing and shifting the surfel towords its associated voxel (v).
-
+  
   DGtal::TransformedSurfelPrism tsy (sy, v);
   DGtal::TransformedSurfelPrism tsz (sz, v);
   DGtal::TransformedSurfelPrism tsxn (sxn, v);
   DGtal::TransformedSurfelPrism tsyn (syn, v);
   DGtal::TransformedSurfelPrism tszn (szn, v);
-
+  
   board << tsx << tsy << tsz << tsxn << tsyn << tszn;
 
-
+  
   // Surfel of Voxel (1,0)
-  SCell sx2 = K.sIncident( v2, 0, true ); // surfel further along x
-  SCell sy2 = K.sIncident( v2, 1, true ); // surfel further along y
-  SCell sz2 = K.sIncident( v2, 2, true ); // surfel further along z
+  SCell sx2 = K.sIncident( v2, 0, true ); // surfel further along x  
+  SCell sy2 = K.sIncident( v2, 1, true ); // surfel further along y  
+  SCell sz2 = K.sIncident( v2, 2, true ); // surfel further along z  
   SCell sxn2 = K.sIncident( v2, 0, false ); // surfel further along x
-  SCell syn2 = K.sIncident( v2, 1, false ); // surfel further along y
-  SCell szn2 = K.sIncident( v2, 2, false ); // surfel further along z
+  SCell syn2 = K.sIncident( v2, 1, false ); // surfel further along y  
+  SCell szn2 = K.sIncident( v2, 2, false ); // surfel further along z  
 
   // Resizing and shifting the surfel towords its associated voxel (v2).
   DGtal::TransformedSurfelPrism tsx2 (sx2, v2);
@@ -100,18 +106,18 @@ int main( )
   DGtal::TransformedSurfelPrism tsxn2 (sxn2, v2);
   DGtal::TransformedSurfelPrism tsyn2 (syn2, v2);
   DGtal::TransformedSurfelPrism tszn2 (szn2, v2);
-
+  
   board << tsx2 << tsy2 << tsz2 << tsxn2 << tsyn2 << tszn2;
 
-
+  
   // Surfel of Voxel (0,1)
-  SCell sx3 = K.sIncident( v3, 0, true ); // surfel further along x
-  SCell sy3 = K.sIncident( v3, 1, true ); // surfel further along y
-  SCell sz3 = K.sIncident( v3, 2, true ); // surfel further along z
-  SCell sxn3 = K.sIncident( v3, 0, false ); // surfel further along x
-  SCell syn3 = K.sIncident( v3, 1, false ); // surfel further along y
-  SCell szn3 = K.sIncident( v3, 2, false ); // surfel further along z
-
+  SCell sx3 = K.sIncident( v3, 0, true ); // surfel further along x  
+  SCell sy3 = K.sIncident( v3, 1, true ); // surfel further along y  
+  SCell sz3 = K.sIncident( v3, 2, true ); // surfel further along z  
+  SCell sxn3 = K.sIncident( v3, 0, false ); // surfel further along x  
+  SCell syn3 = K.sIncident( v3, 1, false ); // surfel further along y  
+  SCell szn3 = K.sIncident( v3, 2, false ); // surfel further along z  
+ 
   // Shifting the surfel to its associated voxel (v3).
   DGtal::TransformedSurfelPrism tsx3 (sx3, v3);
   DGtal::TransformedSurfelPrism tsy3 (sy3, v3);
@@ -119,9 +125,11 @@ int main( )
   DGtal::TransformedSurfelPrism tsxn3 (sxn3, v3);
   DGtal::TransformedSurfelPrism tsyn3 (syn3, v3);
   DGtal::TransformedSurfelPrism tszn3 (szn3, v3);
-
-
+  
+  
   board << tsx3 << tsy3 << tsz3 << tsxn3 << tsyn3 << tszn3;
+
+  std::cout << "save obj" << std::endl;
   board.saveOBJ("board3D-2-ks.obj");
 
 
