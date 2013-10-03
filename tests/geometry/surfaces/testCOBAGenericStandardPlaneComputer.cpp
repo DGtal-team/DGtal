3fea92e8825f4ac4c4c947c2d078fb6be459cb8e
Jacques-Olivier Lachaud <jacques-olivier.lachaud@univ-savoie.fr>
2013-10-03 09:36:55 +0200

    Fully tested standard plane recognition with COBA. Add example: viewer of standard plane.

--- tests/geometry/surfaces/testCOBAGenericStandardPlaneComputer.cpp
+++ tests/geometry/surfaces/testCOBAGenericStandardPlaneComputer.cpp
@@ -30,6 +30,7 @@
 ///////////////////////////////////////////////////////////////////////////////
 #include <cstdlib>
 #include <iostream>
+#include <algorithm>
 #include "DGtal/base/Common.h"
 #include "DGtal/helpers/StdDefs.h"
 #include "DGtal/kernel/CPointPredicate.h"
@@ -46,6 +47,79 @@ using namespace DGtal;
 
 ///////////////////////////////////////////////////////////////////////////////
 // Standard services - public :
+template <typename Integer>
+Integer getRandomInteger( const Integer & first, const Integer & after_last )
+{
+  Integer r = (Integer) random();
+  return ( r % (after_last - first) ) + first;
+}
+
+template <typename Domain>
+std::vector<typename Domain::Point> pointsInStandardPlane
+( const Domain & domain,
+  typename Domain::Integer a, 
+  typename Domain::Integer b,
+  typename Domain::Integer c,
+  typename Domain::Integer mu )
+{
+  typedef typename Domain::Integer Integer;
+  typedef typename Domain::Point Point;
+  typedef typename Domain::ConstIterator ConstIterator;
+  std::vector<Point> pts;
+  Integer mup = mu + abs(a) + abs(b) + abs(c);
+  for ( ConstIterator it = domain.begin(), itE = domain.end();
+        it != itE; ++it )
+    {
+      Point p = *it;
+      Integer r = a * p[ 0 ] + b * p[ 1 ] + c * p[ 2 ];
+      if ( ( mu <= r ) && ( r < mup ) )
+        pts.push_back( p );
+    }
+  return pts;
+}
+
+template <typename PlaneComputer >
+bool checkCOBAGenericStandardPlaneComputer
+( PlaneComputer & computer, unsigned int nbplanes, int diameter )
+{
+  typedef typename PlaneComputer::Space Space;
+  typedef HyperRectDomain<Space> Domain;
+  typedef typename Space::Integer Integer;
+  typedef typename Space::Point Point;
+  unsigned int nb = 0;
+  unsigned int nbok = 0;
+  Domain domain( Point( -diameter, -diameter, -diameter ),
+                 Point(  diameter,  diameter,  diameter ) );
+  Integer a, b, c, mu;
+  for ( unsigned int p = 0; p < nbplanes; ++p )
+    {
+      do {
+        a = getRandomInteger( -diameter, diameter+1 );
+        b = getRandomInteger( -diameter, diameter+1 );
+        c = getRandomInteger( -diameter, diameter+1 );
+      }
+      while ( ( a == 0 ) && ( b == 0 ) && ( c == 0 ) );
+      mu = getRandomInteger( -diameter, diameter );
+      std::vector<Point> pts = pointsInStandardPlane( domain, a, b, c, mu );
+      computer.init( 2*diameter, 1, 1 );
+      ++nb, nbok += computer.extend( pts.begin(), pts.end() ) ? 1 : 0;
+      trace.info() << "Primitive=" << computer.primitive() << std::endl;
+      trace.info() << "(" << nbok << "/" << nb << ") extend " 
+                   << pts.size() << " points of plane " 
+                   << mu << " <= " << a << "*x+" << b << "*y+" << c << "*z+"
+                   << " < " << (mu+a+b+c) << std::endl;
+      computer.init( 2*diameter, 1, 1 );
+      std::random_shuffle( pts.begin(), pts.end() );
+      ++nb, nbok += computer.extend( pts.begin(), pts.end() ) ? 1 : 0;
+      trace.info() << "Primitive=" << computer.primitive() << std::endl;
+      trace.info() << "(" << nbok << "/" << nb << ") extend " 
+                   << pts.size() << " shuffled points of plane " 
+                   << mu << " <= " << a << "*x+" << b << "*y+" << c << "*z+"
+                   << " < " << (mu+a+b+c) << std::endl;
+    }
+  return nb == nbok;
+}
+
 
 int main( int /*argc*/, char** /*argv*/ )
 {
@@ -80,5 +154,7 @@ int main( int /*argc*/, char** /*argv*/ )
   ok = plane.extend( Point(1,1,1) );
   trace.info() << "Point(1,1,1) is " << ( ok ? "ok" : "ko" ) << std::endl;
   trace.info() << plane << std::endl;
+  checkCOBAGenericStandardPlaneComputer( plane, 5, 20 );
+
   return 0;
 }
