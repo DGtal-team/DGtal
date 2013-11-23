258be4949f5757317eaa5ce2fc3a7924a3bfaae1
troussil <tristan.roussillon@liris.cnrs.fr>
2013-11-23 08:02:05 +0100

    adding tools to robustly compute the orientation of three given points

---
+++ src/DGtal/geometry/tools/determinant/COrientationFunctor.h
@@ -0,0 +1,123 @@
+/**
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU Lesser General Public License as
+ *  published by the Free Software Foundation, either version 3 of the
+ *  License, or  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ **/
+
+#pragma once
+
+/**
+ * @file COrientationFunctor.h
+ * @author Tristan Roussillon (\c tristan.roussillon@liris.cnrs.fr )
+ * Laboratoire d'InfoRmatique en Image et Systèmes d'information - LIRIS (CNRS, UMR 5205), CNRS, France
+ *
+ * @date 2013/11/22
+ *
+ * Header file for concept COrientationFunctor.cpp
+ *
+ * This file is part of the DGtal library.
+ */
+
+#if defined(COrientationFunctor_RECURSES)
+#error Recursive header files inclusion detected in COrientationFunctor.h
+#else // defined(COrientationFunctor_RECURSES)
+/** Prevents recursive inclusion of headers. */
+#define COrientationFunctor_RECURSES
+
+#if !defined COrientationFunctor_h
+/** Prevents repeated inclusion of headers. */
+#define COrientationFunctor_h
+
+//////////////////////////////////////////////////////////////////////////////
+// Inclusions
+#include <iostream>
+#include "DGtal/base/Common.h"
+
+#include "DGtal/kernel/CPointFunctor.h"
+//////////////////////////////////////////////////////////////////////////////
+
+namespace DGtal
+{
+
+  /////////////////////////////////////////////////////////////////////////////
+  // class COrientationFunctor
+  /**
+     Description of \b concept '\b COrientationFunctor' <p>
+     @ingroup Concepts
+     @brief Aim: This concept gathers models used to perform
+     an orientation test given three points. These three points
+     are provided in two steps so that this concept is a refinement
+     of CPointFunctor: 
+      - First, we set the first two points by method 'init'. 
+      - Then, we look at the position of the third point with respect
+      to the first two ones: 'operator()' takes an input point and 
+      returns a signed value.  
+
+     The returned value is guaranteed to be: 
+     - zero if the three points belong to the same line
+     - strictly positive if the three points are counter-clockwise oriented
+     - striclty negative if the three points are clockwise oriented
+     
+     ### Refinement of CPointFunctor
+
+     ### Associated types
+     As a refinement of CPointFunctor, it has the following nested types: 
+     - Point type of input points
+     - Value type of the result, at least a model of CSignedNumber
+
+     ### Notation
+     - \e X : A type that is a model of COrientationFunctor
+     - \e x : object of type X
+     - \e a , \e b : objects of type Point
+
+     ### Valid expressions and semantics
+
+     | Name           | Expression  | Type requirements  | Return type | Precondition | Semantics         | Post condition | Complexity |
+     |----------------+-------------+--------------------+-------------+--------------+-------------------+----------------+------------|
+     | initialization | x.init(a,b) | a and b are points |             |              | memorizes a and b |                | constant   |
+
+     ### Models
+  
+     OrientationFunctor2dBy2x2DetComputer
+
+     @tparam T the type that should be a model of COrientationFunctor.
+  */
+  template <typename T>
+  struct COrientationFunctor : CPointFunctor<T>
+  {
+    // ----------------------- Concept checks ------------------------------
+  public:
+
+    BOOST_CONCEPT_USAGE( COrientationFunctor )
+    {
+      myX.init( myA, myB );
+    }
+    // ------------------------- Private Datas --------------------------------
+  private:
+    T myX; 
+    typename T::Point myA, myB;
+
+    // ------------------------- Internals ------------------------------------
+  private:
+
+  }; // end of concept COrientationFunctor
+
+} // namespace DGtal
+
+//                                                                           //
+///////////////////////////////////////////////////////////////////////////////
+
+#endif // !defined COrientationFunctor_h
+
+#undef COrientationFunctor_RECURSES
+#endif // else defined(COrientationFunctor_RECURSES)
---
+++ tests/geometry/tools/determinant/testOrientationFunctors2d.cpp
@@ -0,0 +1,117 @@
+/**
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU Lesser General Public License as
+ *  published by the Free Software Foundation, either version 3 of the
+ *  License, or  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ **/
+
+/**
+ * @file testOrientationFunctors2d.cpp
+ * @ingroup Tests
+ * @author Tristan Roussillon (\c tristan.roussillon@liris.cnrs.fr )
+ * Laboratoire d'InfoRmatique en Image et Systèmes d'information - LIRIS (CNRS, UMR 5205), CNRS, France
+ *
+ * @date 2013/11/22
+ *
+ * Functions for testing models of COrientationFunctor.
+ *
+ * This file is part of the DGtal library.
+ */
+
+///////////////////////////////////////////////////////////////////////////////
+#include <iostream>
+#include "DGtal/base/Common.h"
+#include "DGtal/kernel/PointVector.h"
+
+#include "DGtal/geometry/tools/determinant/Simple2x2DetComputer.h"
+
+#include "DGtal/geometry/tools/determinant/COrientationFunctor.h"
+#include "DGtal/geometry/tools/determinant/OrientationFunctor2dBy2x2DetComputer.h"
+#include "DGtal/geometry/tools/determinant/OrientationFunctor2dBySimpleMatrix.h"
+///////////////////////////////////////////////////////////////////////////////
+
+using namespace std;
+using namespace DGtal;
+
+///////////////////////////////////////////////////////////////////////////////
+// Functions for testing class OrientationFunctors.
+///////////////////////////////////////////////////////////////////////////////
+/**
+ * Example of a test. To be completed.
+ * @param f any orientation functor
+ * @tparam OrientationFunctor a model of COrientationFunctor
+ */
+template<typename OrientationFunctor>
+bool testOrientationFunctors(OrientationFunctor f)
+{
+  BOOST_CONCEPT_ASSERT(( COrientationFunctor<OrientationFunctor> )); 
+
+  unsigned int nbok = 0;
+  unsigned int nb = 0;
+  
+  trace.beginBlock ( "Testing block ..." );
+  trace.info() << f << endl; 
+
+  typedef typename OrientationFunctor::Value Value; 
+  typedef typename OrientationFunctor::Point Point; 
+  Point a(0,0); 
+  Point b(5,2);
+ 
+  //first quadrant
+  f.init(a, b); 
+  if (f( Point(2,1) ) == NumberTraits<Value>::ONE)
+    nbok++;   //a, b, (2,1) are CCW oriented
+  nb++; 
+  trace.info() << "(" << nbok << "/" << nb << ") " << endl;
+
+  if (f( Point(3,1) ) == -NumberTraits<Value>::ONE)
+    nbok++;   //a, b, (3,1) are CW oriented
+  nb++; 
+  trace.info() << "(" << nbok << "/" << nb << ") " << endl;
+
+  if (f( Point(10,4) ) == NumberTraits<Value>::ZERO)
+    nbok++;   //a, b, (10,4) belong to the same line
+  nb++; 
+  trace.info() << "(" << nbok << "/" << nb << ") " << endl;
+
+  trace.endBlock();
+  
+  return nbok == nb;
+}
+
+///////////////////////////////////////////////////////////////////////////////
+// Standard services - public :
+int main( int argc, char** argv )
+{
+  trace.beginBlock ( "Testing class OrientationFunctors" );
+  trace.info() << "Args:";
+  for ( int i = 0; i < argc; ++i )
+    trace.info() << " " << argv[ i ];
+  trace.info() << endl;
+
+  typedef PointVector<2, DGtal::int16_t> Point; 
+
+  bool res = true; 
+
+  typedef Simple2x2DetComputer<DGtal::int32_t, DGtal::int64_t> DetComputer; 
+  typedef OrientationFunctor2dBy2x2DetComputer<Point, DetComputer> Functor1; 
+  res = res && testOrientationFunctors( Functor1() );
+ 
+  typedef OrientationFunctor2dBySimpleMatrix<Point, DGtal::int32_t> Functor2; 
+  res = res && testOrientationFunctors( Functor2() );
+
+  trace.emphase() << ( res ? "Passed." : "Error." ) << endl;
+  trace.endBlock();
+  return res ? 0 : 1;
+}
+//                                                                           //
+///////////////////////////////////////////////////////////////////////////////
