6df78e71f6e0299a7f6fc9ccde07469adf1e1fbb
Jacques-Olivier Lachaud <jacques-olivier.lachaud@univ-savoie.fr>
2014-02-14 14:44:03 +0100

    Add embed method in TrueDigitalSurfaceLocalEstimator.

--- src/DGtal/geometry/surfaces/estimation/TrueDigitalSurfaceLocalEstimator.h
+++ src/DGtal/geometry/surfaces/estimation/TrueDigitalSurfaceLocalEstimator.h
@@ -170,6 +170,12 @@ namespace DGtal
                          SurfelConstIterator ite,
                          OutputIterator result ) const;
 
+    /**
+       @param any surfel.
+       @return the associated point on the shape (the one that is closest to its centroid).
+    */
+    RealPoint embed( Surfel surfel ) const;
+
     // ----------------------- Interface --------------------------------------
   public:
 
