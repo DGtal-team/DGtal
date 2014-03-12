8d5bd1f39c713bba138a780dba8475a1a09fa66b
Pierre Gueth <pierre.gueth@gmail.com>
2014-03-12 15:46:29 +0100

    solver in CLinearAlgebra concept

--- src/DGtal/dec/suitesparse/SuiteSparseLinearAlgebra.h
+++ src/DGtal/dec/suitesparse/SuiteSparseLinearAlgebra.h
@@ -5,12 +5,37 @@
 #include "DenseMatrix.h"
 #include "SparseMatrix.h"
 
+struct SuiteSparseSolver
+{
+    public:
+        typedef DDG::DenseMatrix<DDG::Real> Vector;
+        typedef DDG::SparseMatrix<DDG::Real> Matrix;
+
+        SuiteSparseSolver& compute(const Matrix& _matrix)
+        {
+            matrix = _matrix;
+            return *this;
+        }
+
+        Vector solve(const Vector& input) const
+        {
+            Matrix matrix_copy(matrix);
+            Vector input_copy(input);
+            Vector solution;
+            DDG::solve<DDG::Real>(matrix_copy, input_copy, solution);
+            return solution;
+        }
+    protected:
+        Matrix matrix;
+};
+
 struct SuiteSparseLinearAlgebra
 {
     typedef unsigned int Index;
     typedef DDG::Real Scalar;
     typedef DDG::DenseMatrix<DDG::Real> Vector;
     typedef DDG::SparseMatrix<DDG::Real> Matrix;
+    typedef SuiteSparseSolver Solver;
 };
 
 #endif
