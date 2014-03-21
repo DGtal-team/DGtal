a001c348b15024672b4a90ee45474429e214448a
Pierre Gueth <pierre.gueth@gmail.com>
2014-03-21 13:02:57 +0100

    moved eigen backend

--- src/DGtal/math/linalg/EigenLinearAlgebra.h
+++ src/DGtal/math/linalg/EigenLinearAlgebra.h
@@ -1,8 +1,8 @@
 #if !defined(__DEC_EIGEN_LINEAR_ALGEBRA__)
 #define __DEC_EIGEN_LINEAR_ALGEBRA__
 
-#define EIGEN_DENSEBASE_PLUGIN "DGtal/math/linalg/eigen/DenseBaseAddons.h"
-#define EIGEN_SPARSEMATRIX_PLUGIN "DGtal/math/linalg/eigen/SparseMatrixAddons.h"
+#define EIGEN_DENSEBASE_PLUGIN "DGtal/math/linalg/EigenDenseBaseAddons.h"
+#define EIGEN_SPARSEMATRIX_PLUGIN "DGtal/math/linalg/EigenSparseMatrixAddons.h"
 
 #include <Eigen/Dense>
 #include <Eigen/Sparse>
