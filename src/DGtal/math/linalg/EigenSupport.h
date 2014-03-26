93fb29e96bfb557752501667c3c9e6c0c460b6c0
Pierre Gueth <pierre.gueth@gmail.com>
2014-03-26 12:25:14 +0100

    new test for eigen solvers concepts

--- src/DGtal/math/linalg/EigenSupport.h
+++ src/DGtal/math/linalg/EigenSupport.h
@@ -15,12 +15,22 @@ struct EigenDenseLinearAlgebraBackend
 {
     typedef Eigen::VectorXd Vector;
     typedef Eigen::MatrixXd Matrix;
+
+    typedef Eigen::ConjugateGradient<Matrix> SolverConjugateGradient;
+    typedef Eigen::BiCGSTAB<Matrix> SolverBiCGSTAB;
 };
 
 struct EigenSparseLinearAlgebraBackend
 {
     typedef Eigen::VectorXd Vector;
     typedef Eigen::SparseMatrix<Vector::Scalar, Eigen::ColMajor> Matrix;
+
+    typedef Eigen::SimplicialLLT<Matrix> SolverSimplicialLLT;
+    typedef Eigen::SimplicialLDLT<Matrix> SolverSimplicialLDLT;
+    typedef Eigen::ConjugateGradient<Matrix> SolverConjugateGradient;
+    typedef Eigen::BiCGSTAB<Matrix> SolverBiCGSTAB;
+    typedef Eigen::SparseLU<Matrix> SolverSparseLU;
+    typedef Eigen::SparseQR<Matrix, Eigen::AMDOrdering<Matrix::Index> > SolverSparseQR;
 };
 
 std::ostream& operator<<(std::ostream& os, const Eigen::ComputationInfo& info)
