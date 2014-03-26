f7026f40a99dd8ffeebe8947dc25b424291ccfc8
Pierre Gueth <pierre.gueth@gmail.com>
2014-03-26 14:48:01 +0100

    formated KForm

--- src/DGtal/dec/KForm.h
+++ src/DGtal/dec/KForm.h
@@ -1,58 +1,201 @@
-#if !defined(__DEC_KFORM__)
-#define __DEC_KFORM__
+/**
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU Lesser General Public License as
+ *  published by the Free Software Foundation, either version 3 of the
+ *  License, or  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ **/
 
-#include "Duality.h"
+#pragma once
 
-#include <DGtal/base/Common.h>
+/**
+ * @file KForm.h
+ * @author Pierre Gueth (\c pierre.gueth@liris.cnrs.fr )
+ * Laboratoire d'InfoRmatique en Image et Systemes d'information - LIRIS (CNRS, UMR 5205), CNRS, France
+ *
+ * @date 2014/03/26
+ *
+ * Header file for module KForm.cpp
+ *
+ * This file is part of the DGtal library.
+ */
 
-template <typename C, Order order, Duality duality>
-struct KForm
+#if defined(KForm_RECURSES)
+#error Recursive header files inclusion detected in KForm.h
+#else // defined(KForm_RECURSES)
+/** Prevents recursive inclusion of headers. */
+#define KForm_RECURSES
+
+#if !defined KForm_h
+/** Prevents repeated inclusion of headers. */
+#define KForm_h
+
+//////////////////////////////////////////////////////////////////////////////
+// Inclusions
+#include <iostream>
+#include "DGtal/base/Common.h"
+//////////////////////////////////////////////////////////////////////////////
+
+namespace DGtal
 {
-    typedef C Calculus;
-    typedef typename Calculus::Vector Container;
 
-    typedef typename Calculus::Scalar Scalar;
+  /////////////////////////////////////////////////////////////////////////////
+  // template class KForm
+  /**
+   * Description of template class 'KForm' <p>
+   * \brief Aim:
+   * KForm represents kforms in the dec package.
+   *
+   * @tparam Calculus should be DiscreteExteriorCalculus.
+   * @tparam Order is the order of the kform.
+   * @tparam Duality is the duality of the kform.
+   */
+  template <typename C, Order order, Duality duality>
+  class KForm
+  {
+    // ----------------------- Standard services ------------------------------
+  public:
+    typedef C Calculus;
 
     BOOST_STATIC_ASSERT(( order >= 0 ));
     BOOST_STATIC_ASSERT(( order <= Calculus::dimension ));
 
-    const Calculus& calculus;
+    typedef typename Calculus::Vector Container;
+    typedef typename Calculus::Scalar Scalar;
+
+    /**
+     * Constructor.
+     * @param calculus the discrete exterior calculus to use.
+     */
+    KForm(const Calculus& calculus);
+
+    /**
+     * Constructor.
+     * @param calculus the discrete exterior calculus to use.
+     * @param container the container to copy.
+     */
+    KForm(const Calculus& calculus, const Container& container);
+
+    /**
+     * Assignment.
+     * @param other the object to copy.
+     * @return a reference on 'this'.
+     */
+    KForm& operator=(const KForm& other);
+
+    // ----------------------- Interface --------------------------------------
+  public:
+
+    /**
+     * Container used to actually hold the kform data.
+     */
     Container container;
 
-    KForm(const Calculus& _calculus);
-    KForm(const Calculus& _calculus, const Container& _container);
-    KForm& operator=(const KForm& _form);
+    /**
+     * Const reference to calculus
+     */
+    const Calculus& calculus;
 
-    template <typename SCellMap>
-    void applyToAccum(SCellMap& scell_map) const;
+    /**
+     * Writes/Displays the object on an output stream.
+     * @param out the output stream where the object is written.
+     */
+    void selfDisplay(std::ostream& out) const;
 
-    typename Calculus::SCell
-    getSCell(const typename Calculus::Index& index) const;
+    /**
+     * Writes kform values to scell accumulator.
+     * @param scell_map scell accumulator where the object is written.
+     */
+    template <typename Accum>
+    void applyToAccum(Accum& scell_map) const;
 
-    void
-    clear();
-};
+    /**
+     * Clear current kform.
+     */
+    void clear();
 
-template <typename Calculus, Order order, Duality duality>
-std::ostream&
-operator<<(std::ostream& os, const KForm<Calculus, order, duality>& form);
+    /**
+     * Get kcell from index.
+     * @param index
+     */
+    typename Calculus::SCell getSCell(const typename Calculus::Index& index) const;
 
-// k-forms addition
-template <typename Calculus, Order order, Duality duality>
-KForm<Calculus, order, duality>
-operator+(const KForm<Calculus, order, duality>& form_a, const KForm<Calculus, order, duality>& form_b);
+    /**
+     * Checks the validity/consistency of the object.
+     * @return 'true' if the object is valid, 'false' otherwise.
+     */
+    bool isValid() const;
 
-// k-forms subtraction
-template <typename Calculus, Order order, Duality duality>
-KForm<Calculus, order, duality>
-operator-(const KForm<Calculus, order, duality>& form_a, const KForm<Calculus, order, duality>& form_b);
+    // ------------------------- Private Datas --------------------------------
+  private:
 
-// k-forms scalar multiplication
-template <typename Calculus, Order order, Duality duality>
-KForm<Calculus, order, duality>
-operator*(const typename Calculus::Scalar& scalar, const KForm<Calculus, order, duality>& form);
+    // ------------------------- Hidden services ------------------------------
+  protected:
 
-#include "KForm.ih"
+    /**
+     * Constructor.
+     * Forbidden by default (protected to avoid g++ warnings).
+     */
+    KForm();
 
-#endif
+    // ------------------------- Internals ------------------------------------
+  private:
 
+  }; // end of class KForm
+
+  /**
+   * Overloads 'operator<<' for displaying objects of class 'KForm'.
+   * @param out the output stream where the object is written.
+   * @param object the object of class 'KForm' to write.
+   * @return the output stream after the writing.
+   */
+  template <typename Calculus, Order order, Duality duality>
+  std::ostream&
+  operator<<(std::ostream& out, const KForm<Calculus, order, duality>& object);
+
+  /**
+   * Overloads 'operator+' for adding objects of class 'KForm'.
+   * @return form_a + form_b.
+   */
+  template <typename Calculus, Order order, Duality duality>
+  KForm<Calculus, order, duality>
+  operator+(const KForm<Calculus, order, duality>& form_a, const KForm<Calculus, order, duality>& form_b);
+
+  /**
+   * Overloads 'operator-' for substracting objects of class 'KForm'.
+   * @return form_a - form_b.
+   */
+  template <typename Calculus, Order order, Duality duality>
+  KForm<Calculus, order, duality>
+  operator-(const KForm<Calculus, order, duality>& form_a, const KForm<Calculus, order, duality>& form_b);
+
+  /**
+   * Overloads 'operator*' for scalar multiplication of objects of class 'KForm'.
+   * @return scalar * form.
+   */
+  template <typename Calculus, Order order, Duality duality>
+  KForm<Calculus, order, duality>
+  operator*(const typename Calculus::Scalar& scalar, const KForm<Calculus, order, duality>& form);
+
+} // namespace DGtal
+
+
+///////////////////////////////////////////////////////////////////////////////
+// Includes inline functions.
+#include "DGtal/dec/KForm.ih"
+
+//                                                                           //
+///////////////////////////////////////////////////////////////////////////////
+
+#endif // !defined KForm_h
+
+#undef KForm_RECURSES
+#endif // else defined(KForm_RECURSES)
