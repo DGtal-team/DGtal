ce7b0b00fbd06f6c67ec39409e7b0f1e32fec758
Pierre Gueth <pierre.gueth@gmail.com>
2014-03-27 14:16:52 +0100

    formatted AllSCellMap

--- src/DGtal/dec/AllSCellMap.h
+++ src/DGtal/dec/AllSCellMap.h
@@ -1,51 +1,184 @@
-#if !defined(__DEC_ALLSCELLMAP_H__)
-#define __DEC_ALLSCELLMAP_H__
+/**
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU Lesser General Public License as
+ *  published by the Free Software Foundation, either version 3 of the
+ *  License, or  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ **/
 
+#pragma once
+
+/**
+ * @file AllSCellMap.h
+ * @author Pierre Gueth (\c pierre.gueth@liris.cnrs.fr )
+ * Laboratoire d'InfoRmatique en Image et Systemes d'information - LIRIS (CNRS, UMR 5205), CNRS, France
+ *
+ * @date 2014/03/27
+ *
+ * Header file for module AllSCellMap.cpp
+ *
+ * This file is part of the DGtal library.
+ */
+
+#if defined(AllSCellMap_RECURSES)
+#error Recursive header files inclusion detected in AllSCellMap.h
+#else // defined(AllSCellMap_RECURSES)
+/** Prevents recursive inclusion of headers. */
+#define AllSCellMap_RECURSES
+
+#if !defined AllSCellMap_h
+/** Prevents repeated inclusion of headers. */
+#define AllSCellMap_h
+
+//////////////////////////////////////////////////////////////////////////////
+// Inclusions
+#include <iostream>
 #include <map>
 #include <string>
-
+#include "DGtal/base/Common.h"
 #if defined(WITH_ITK)
 #include <DGtal/images/ImageSelector.h>
 #include <DGtal/io/writers/ITKWriter.h>
 #endif
-
 #include <DGtal/io/Color.h>
 #include <DGtal/io/boards/Board2D.h>
 #include <DGtal/io/viewers/Viewer3D.h>
+//////////////////////////////////////////////////////////////////////////////
 
 namespace DGtal
 {
 
-template <typename Calculus, typename V>
-struct AllSCellMap : public std::map<typename Calculus::SCell, V>
-{
-		typedef typename Calculus::SCell SCell;
-		typedef SCell Key;
-		typedef V Value;
+  /////////////////////////////////////////////////////////////////////////////
+  // template class AllSCellMap
+  /**
+   * Description of template class 'AllSCellMap' <p>
+   * \brief Aim:
+   * This containter holds data associated with DiscreteExteriorCalculus cells.
+   *
+   * @tparam Calculus should be DiscreteExteriorCalculus.
+   * @tparam Value is type of value stored per cells.
+   */
+  template <typename C, typename V>
+  class AllSCellMap : public std::map<typename C::SCell, V>
+  {
+    // ----------------------- Standard services ------------------------------
+  public:
 
-		typedef typename std::map<Key, Value> Container;
-		typedef typename Container::const_iterator ConstIterator;
-		typedef typename Container::iterator Iterator;
+    typedef C Calculus;
+    typedef V Value;
 
-		AllSCellMap(const Calculus& _calculus);
-		AllSCellMap& operator=(const AllSCellMap& _scell_map);
+    typedef typename Calculus::SCell SCell;
+    typedef SCell Key;
 
+    typedef typename std::map<Key, Value> Container;
+    typedef typename Container::const_iterator ConstIterator;
+    typedef typename Container::iterator Iterator;
+
+    /**
+     * Constructor.
+     * @param calculus the discrete exterior calculus to use.
+     */
+    AllSCellMap(const Calculus& calculus);
+
+    /**
+     * Assignment.
+     * @param other the object to copy.
+     * @return a reference on 'this'.
+     */
+    AllSCellMap& operator=(const AllSCellMap& _scell_map);
+
+    // ----------------------- Interface --------------------------------------
+  public:
+
+    /**
+     * Const reference to calculus
+     */
+    const Calculus& calculus;
+
+    /**
+     * Writes/Displays the object on an output stream.
+     * @param out the output stream where the object is written.
+     */
+    void selfDisplay ( std::ostream & out ) const;
+
+    /**
+     * Writes the object on an ITK image
+     * @param filename the name of the file written.
+     * @param value_outside image value for non set order of cells
+     * @params value_inside_default image value for cells of correct order without values.
+     */
 #if defined(WITH_ITK)
-		void writeITKImage(const std::string& filename, const Value& value_outside = -1, const Value& value_inside_default = 0) const;
+    void writeITKImage(const std::string& filename, const Value& value_outside = -1, const Value& value_inside_default = 0) const;
 #endif
 
-		template <typename Viewer, typename ColorMap>
-		void display3D(Viewer& viewer, const ColorMap& color_map) const;
+    /**
+     * Displays the object on an 3D viewer.
+     * @param viewer
+     * @param color_map
+     */
+    template <typename Viewer, typename ColorMap>
+    void display3D(Viewer& viewer, const ColorMap& color_map) const;
 
-		template <typename Board, typename ColorMap>
-		void display2D(Board& board, const ColorMap& color_map) const;
+    /**
+     * Displays the object on an 2D board.
+     * @param board
+     * @param color_map
+     */
+    template <typename Board, typename ColorMap>
+    void display2D(Board& board, const ColorMap& color_map) const;
 
-		const Calculus& calculus;
-};
+    /**
+     * Checks the validity/consistency of the object.
+     * @return 'true' if the object is valid, 'false' otherwise.
+     */
+    bool isValid() const;
 
-}
+    // ------------------------- Private Datas --------------------------------
+  private:
 
-#include "AllSCellMap.ih"
+    // ------------------------- Hidden services ------------------------------
+  protected:
 
-#endif
+    /**
+     * Constructor.
+     * Forbidden by default (protected to avoid g++ warnings).
+     */
+    AllSCellMap();
 
+    // ------------------------- Internals ------------------------------------
+  private:
+
+  }; // end of class AllSCellMap
+
+  /**
+   * Overloads 'operator<<' for displaying objects of class 'AllSCellMap'.
+   * @param out the output stream where the object is written.
+   * @param object the object of class 'AllSCellMap' to write.
+   * @return the output stream after the writing.
+   */
+  template <typename Calculus, typename Value>
+  std::ostream&
+  operator<<(std::ostream& out, const AllSCellMap<Calculus, Value>& object);
+
+} // namespace DGtal
+
+
+///////////////////////////////////////////////////////////////////////////////
+// Includes inline functions.
+#include "DGtal/dec/AllSCellMap.ih"
+
+//                                                                           //
+///////////////////////////////////////////////////////////////////////////////
+
+#endif // !defined AllSCellMap_h
+
+#undef AllSCellMap_RECURSES
+#endif // else defined(AllSCellMap_RECURSES)
