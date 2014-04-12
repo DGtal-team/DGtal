de5c9ae7f51ecc4f53d2a7c939baa83e6577f296
David Coeurjolly <david.coeurjolly@liris.cnrs.fr>
2014-04-12 13:48:24 +0200

    ok

---
+++ tests/io/viewers/testViewerQuad.cpp
@@ -0,0 +1,87 @@
+/**
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU Lesser General Public License as
+ *  published by the Free Software Foundation, either version 3 of the
+ *  License, or  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ **/
+
+/**
+ * @file test3dViewer.cpp
+ * @ingroup Tests
+ * @author Bertrand Kerautret (\c kerautre@loria.fr )
+ * LORIA (CNRS, UMR 7503), University of Nancy, France
+ *
+ * @date 2011/01/03
+ *
+ * Functions for testing class Viewer3D.
+ *
+ * This file is part of the DGtal library.
+ */
+
+///////////////////////////////////////////////////////////////////////////////
+#include <iostream>
+#include <QtGui/qapplication.h>
+#include "DGtal/base/Common.h"
+#include "DGtal/io/viewers/Viewer3D.h"
+#include "DGtal/io/DrawWithDisplay3DModifier.h"
+#include "DGtal/io/Color.h"
+#include "DGtal/helpers/StdDefs.h"
+#include "DGtal/shapes/Shapes.h"
+///////////////////////////////////////////////////////////////////////////////
+
+using namespace std;
+using namespace DGtal;
+using namespace Z3i;
+
+///////////////////////////////////////////////////////////////////////////////
+// Functions for testing class Viewer3D.
+///////////////////////////////////////////////////////////////////////////////
+
+///////////////////////////////////////////////////////////////////////////////
+// Standard services - public :
+
+int main( int argc, char** argv )
+{
+
+ QApplication application(argc,argv);
+ Viewer3D<> viewer;
+ viewer.setWindowTitle("simpleViewer");
+ viewer.show();
+
+
+ trace.beginBlock ( "Testing class for  Viewer3D" );
+
+  Point p1( 0, 0, 0 );
+  Point p2( 0, 1 , 0);
+  Point p3( 1, 1, 0);
+  Point p4(1, 0, 0 );
+  Point p5( 2, 0 , 0);
+  Point p6( 2, 1, 0);
+  RealVector n(1,1,1);
+  RealVector n2(0,1,1);
+
+  Viewer3D<> viewer;
+  viewer.addQuadWithNormal(p1,p2,p3,p4, n.getNormalized());
+  viewer.addQuadWithNormal(p4,p5,p6,p3, n2.getNormalized());
+
+  viewer  << Display3D<Space, KSpace>::updateDisplay;
+
+
+ bool res = application.exec();
+ trace.emphase() << ( res ? "Passed." : "Error." ) << endl;
+ trace.endBlock();
+ return res ? 0 : 1;
+
+
+}
+//                                                                           //
+///////////////////////////////////////////////////////////////////////////////
