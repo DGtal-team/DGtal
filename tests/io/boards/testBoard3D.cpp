fda7c47093da4ae556687a4b978e9abb71aaa332
David Coeurjolly <david.coeurjolly@liris.cnrs.fr>
2014-04-15 08:33:30 +0200

    testBoard3D ok

--- tests/io/boards/testBoard3D.cpp
+++ tests/io/boards/testBoard3D.cpp
@@ -110,13 +110,14 @@ bool testQuadNorm()
   RealVector n(1,1,1);
   RealVector n2(0,1,1);
 
-  Board3D<> board;
-  board.addQuadWithNormal(p1,p2,p3,p4, n.getNormalized());
-  board.addQuadWithNormal(p4,p5,p6,p3, n2.getNormalized());
-
   KSpace k;
 
   k.init(Point(2,2,2), Point(4,4,4), true);
+
+  Board3D<Space,KSpace> board(k);
+  board.addQuadWithNormal(p1,p2,p3,p4, n.getNormalized());
+  board.addQuadWithNormal(p4,p5,p6,p3, n2.getNormalized());
+
   Cell surfel = k.uCell( Point( 2,3,3) );
 
   Display3DFactory<Space,KSpace>::drawSurfelWithNormal( board, surfel, n2.getNormalized());
