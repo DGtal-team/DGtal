e7f710816149c831a710706ae15cce382909b6cd
Kacper Pluta <kacper.pluta@gmail.com>
2014-06-28 13:16:18 +0200

    First version of 2D transformations

--- tests/images/testRigidTransformation2D.cpp
+++ tests/images/testRigidTransformation2D.cpp
@@ -29,15 +29,21 @@
 
 ///////////////////////////////////////////////////////////////////////////////
 #include <iostream>
+#include <cmath>
+#include <DGtal/images/ImageSelector.h>
 #include <DGtal/images/ImageContainerBySTLVector.h>
+#include "DGtal/images/ConstImageAdapter.h"
 #include "DGtal/base/Common.h"
+#include "DGtal/io/boards/Board2D.h"
 #include "ConfigTest.h"
 #include "DGtal/helpers/StdDefs.h"
 #include "DGtal/images/RigidTransformation2D.h"
+#include "DGtal/io/colormaps/GrayscaleColorMap.h"
 ///////////////////////////////////////////////////////////////////////////////
 
 using namespace std;
 using namespace DGtal;
+using namespace Z2i;
 
 ///////////////////////////////////////////////////////////////////////////////
 // Functions for testing class testRigidTransformation2D.
@@ -46,34 +52,79 @@ using namespace DGtal;
  * Example of a test. To be completed.
  *
  */
-bool testtestRigidTransformation2D()
+
+class testRigidTransformation2D
 {
-  unsigned int nbok = 0;
-  unsigned int nb = 0;
-  
-  trace.beginBlock ( "Testing block ..." );
-  nbok += true ? 1 : 0; 
-  nb++;
-  trace.info() << "(" << nbok << "/" << nb << ") "
-	       << "true == true" << std::endl;
-  trace.endBlock();
-  typedef ImageContainerBySTLVector<Z2i::Domain, int> Image;
-  RigidTransformation2D <Image> rig;
-  return nbok == nb;
-}
+  typedef ImageSelector<Domain, int >::Type Image;
+  typedef ForwardRigidTransformation2D < Point, RealVector > ForwardTrans;
+  typedef BackwardRigidTransformation2D < Point, RealVector > BackwardTrans;
+  typedef ConstImageAdapter<Image, Domain, ForwardTrans, Image::Value, DefaultFunctor > MyImageForwardAdapter;
+  typedef ConstImageAdapter<Image, Domain, BackwardTrans, Image::Value, DefaultFunctor > MyImageBackwardAdapter;
+private:
+  Image binary;
+  ForwardTrans forwardTrans;
+  BackwardTrans backwardTrans;
+  typedef GrayscaleColorMap<unsigned char> Gray;
+  DefaultFunctor idD;
+  Board2D aBoard;
+  DomainRigidTransformation2D < Domain, ForwardTrans > domainTrans;
+public:
+  // Setup part
+  testRigidTransformation2D() : 
+  binary ( Domain ( Point ( 0,0 ), Point ( 10, 10 ) ) ),
+  forwardTrans ( Point ( 5, 5 ), M_PI_2/2, RealVector() ),
+    backwardTrans ( Point ( 5, 5 ), M_PI_2/2, RealVector() ),
+    domainTrans ( forwardTrans )
+    {
+      binary.setValue ( Point ( 3,3 ), 255 );
+      binary.setValue ( Point ( 3,4 ), 255 );
+      binary.setValue ( Point ( 4,3 ), 255 );
+      binary.setValue ( Point ( 4,4 ), 255 );
+    }
+    
+    bool forwardTransformationBinary ()
+    {
+      Display2DFactory::drawImage<Gray>(aBoard, binary, (unsigned char)0, (unsigned char)255);
+      aBoard.saveSVG( "forward_before.svg" );
+      
+      Image transformed ( domainTrans ( binary.domain() ) );
+      for ( Domain::ConstIterator it = binary.domain().begin(); it != binary.domain().end(); ++it )
+      {
+	if ( binary(*it) == 255 )
+	{
+	  transformed.setValue ( forwardTrans ( *it ), 255 );
+	}
+	else
+	{
+	  transformed.setValue ( forwardTrans ( *it ), 0 );
+	}
+      }
+      Display2DFactory::drawImage<Gray>(aBoard, transformed, (unsigned char)0, (unsigned char)255);
+      aBoard.saveSVG( "forward_after.svg" );
+      return true;
+    }
+    
+    bool backwardTransformationBinary ()
+    {
+      MyImageBackwardAdapter adapter ( binary, domainTrans ( binary.domain() ), backwardTrans, idD );
+      Display2DFactory::drawImage<Gray>(aBoard, binary, (unsigned char)0, (unsigned char)255);
+      aBoard.saveSVG( "backward_before.svg" );
+      Display2DFactory::drawImage<Gray>(aBoard, adapter, (unsigned char)0, (unsigned char)255);
+      aBoard.saveSVG( "backward_after.svg" );
+      return true;
+    }
+};
 
 ///////////////////////////////////////////////////////////////////////////////
 // Standard services - public :
 
-int main( int argc, char** argv )
+int main( int, char** )
 {
-  trace.beginBlock ( "Testing class testRigidTransformation2D" );
-  trace.info() << "Args:";
-  for ( int i = 0; i < argc; ++i )
-    trace.info() << " " << argv[ i ];
-  trace.info() << endl;
-
-  bool res = testtestRigidTransformation2D(); // && ... other tests
+  trace.beginBlock ( "Testing RigidTransformation2D" );
+  bool res = true;
+  testRigidTransformation2D testRigid;
+  res &= testRigid.forwardTransformationBinary();
+  res &= testRigid.backwardTransformationBinary();
   trace.emphase() << ( res ? "Passed." : "Error." ) << endl;
   trace.endBlock();
   return res ? 0 : 1;
