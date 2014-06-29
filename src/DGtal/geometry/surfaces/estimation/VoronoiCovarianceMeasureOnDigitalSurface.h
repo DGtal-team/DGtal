ed6ff97078eacee3ff2afdd095c0be63c4bb814a
David Coeurjolly <david.coeurjolly@liris.cnrs.fr>
2014-06-29 18:36:11 +0200

    propagating changes

--- src/DGtal/geometry/surfaces/estimation/VoronoiCovarianceMeasureOnDigitalSurface.h
+++ src/DGtal/geometry/surfaces/estimation/VoronoiCovarianceMeasureOnDigitalSurface.h
@@ -85,7 +85,7 @@ namespace DGtal
   class VoronoiCovarianceMeasureOnDigitalSurface
   {
     BOOST_CONCEPT_ASSERT(( CDigitalSurfaceContainer< TDigitalSurfaceContainer > ));
-    BOOST_CONCEPT_ASSERT(( CSeparableMetric<TSeparableMetric> ));
+    BOOST_CONCEPT_ASSERT(( concepts::CSeparableMetric<TSeparableMetric> ));
     // ----------------------- public types ------------------------------
   public:
     typedef TDigitalSurfaceContainer DigitalSurfaceContainer; ///< the chosen container
