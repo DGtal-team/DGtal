69491564de2101aec6212df226d6beded0a5dc31
David Coeurjolly <david.coeurjolly@liris.cnrs.fr>
2014-07-01 08:26:27 +0200

    coverage

--- cmake/CheckCoverage.cmake
+++ cmake/CheckCoverage.cmake
@@ -2,31 +2,34 @@
 # Coverage brute-froce discovery
 # -----------------------------------------------------------------------------
 ADD_CUSTOM_TARGET(lcov)
-ADD_CUSTOM_COMMAND(TARGET lcov
+OPTION(WITH_COVERAGE "Enable lcov code coverage." ON)
+IF (WITH_COVERAGE)
+  ADD_CUSTOM_COMMAND(TARGET lcov
     COMMAND mkdir -p coverage
     WORKING_DIRECTORY ${CMAKE_BINARY_DIR}
     )
-ADD_CUSTOM_COMMAND(TARGET lcov
+  ADD_CUSTOM_COMMAND(TARGET lcov
     COMMAND lcov --directory . --zerocounters
     WORKING_DIRECTORY ${CMAKE_BINARY_DIR}
     )
-ADD_CUSTOM_COMMAND(TARGET lcov
+  ADD_CUSTOM_COMMAND(TARGET lcov
     COMMAND make test
     WORKING_DIRECTORY ${CMAKE_BINARY_DIR}
     )
-ADD_CUSTOM_COMMAND(TARGET lcov
+  ADD_CUSTOM_COMMAND(TARGET lcov
     COMMAND lcov --directory . --capture --output-file ./coverage/stap_all.info --no-checksum
     WORKING_DIRECTORY ${CMAKE_BINARY_DIR}
     )
-ADD_CUSTOM_COMMAND(TARGET lcov
+  ADD_CUSTOM_COMMAND(TARGET lcov
     COMMAND lcov --directory . -r ./coverage/stap_all.info ${REMOVE_PATTERN} --output-file ./coverage/stap.info
     WORKING_DIRECTORY ${CMAKE_BINARY_DIR}
     )
-ADD_CUSTOM_COMMAND(TARGET lcov
+  ADD_CUSTOM_COMMAND(TARGET lcov
     COMMAND genhtml -o ./coverage --title "Code Coverage" --legend --show-details --demangle-cpp ./coverage/stap.info
     WORKING_DIRECTORY ${CMAKE_BINARY_DIR}
     )
-ADD_CUSTOM_COMMAND(TARGET lcov
+  ADD_CUSTOM_COMMAND(TARGET lcov
     COMMAND echo "Open ${CMAKE_BINARY_DIR}/coverage/index.html to view the coverage analysis results."
     WORKING_DIRECTORY ${CMAKE_BINARY_DIR}
     )
+ENDIF()
