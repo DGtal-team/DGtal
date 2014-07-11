9ca617fbd82a1b3cff6f72db5d2a229f254e0b13
Pierre Gueth <pierre.gueth@gmail.com>
2014-07-11 12:54:09 +0200

    fix namespace in dec examples

--- examples/dec/common.h
+++ examples/dec/common.h
@@ -3,22 +3,21 @@
 
 #include "DGtal/base/Common.h"
 #include "DGtal/helpers/StdDefs.h"
-using namespace DGtal;
 
-inline Z2i::DigitalSet generateRingSet(const Z2i::Domain& domain)
+inline DGtal::Z2i::DigitalSet generateRingSet(const DGtal::Z2i::Domain& domain)
 {
-    Z2i::DigitalSet set(domain);
-    Z2i::RealPoint center = domain.lowerBound() + domain.upperBound();
+    DGtal::Z2i::DigitalSet set(domain);
+    DGtal::Z2i::RealPoint center = domain.lowerBound() + domain.upperBound();
     center /= 2.;
-    Z2i::RealPoint delta = domain.upperBound() - domain.lowerBound();
+    DGtal::Z2i::RealPoint delta = domain.upperBound() - domain.lowerBound();
     double radius = delta[0]>delta[1] ? delta[1] : delta[0];
     radius += 1.;
     radius /= 2.;
 
-    for (Z2i::Domain::ConstIterator di=domain.begin(), die=domain.end(); di!=die; di++)
+    for (DGtal::Z2i::Domain::ConstIterator di=domain.begin(), die=domain.end(); di!=die; di++)
     {
-        const Z2i::Point point = *di;
-        const Z2i::RealPoint point_real = Z2i::RealPoint(point) - center;
+        const DGtal::Z2i::Point point = *di;
+        const DGtal::Z2i::RealPoint point_real = DGtal::Z2i::RealPoint(point) - center;
         if (point_real.norm() < 1.*radius/6.) continue;
         if (point_real.norm() > 5.*radius/6.) continue;
         set.insert(point);
@@ -27,31 +26,31 @@ inline Z2i::DigitalSet generateRingSet(const Z2i::Domain& domain)
     return set;
 }
 
-inline Z2i::DigitalSet generateDoubleRingSet(const Z2i::Domain& domain)
+inline DGtal::Z2i::DigitalSet generateDoubleRingSet(const DGtal::Z2i::Domain& domain)
 {
-    Z2i::DigitalSet set(domain);
-    Z2i::RealPoint center = domain.lowerBound() + domain.upperBound();
+    DGtal::Z2i::DigitalSet set(domain);
+    DGtal::Z2i::RealPoint center = domain.lowerBound() + domain.upperBound();
     center /= 2.;
-    Z2i::RealPoint delta = domain.upperBound() - domain.lowerBound();
+    DGtal::Z2i::RealPoint delta = domain.upperBound() - domain.lowerBound();
     double radius = delta[0]>delta[1] ? delta[1] : delta[0];
     radius += 1.;
     radius /= 2.;
 
-    center -= Z2i::RealPoint(radius/2.+radius/5.,0);
-    for (Z2i::Domain::ConstIterator di=domain.begin(), die=domain.end(); di!=die; di++)
+    center -= DGtal::Z2i::RealPoint(radius/2.+radius/5.,0);
+    for (DGtal::Z2i::Domain::ConstIterator di=domain.begin(), die=domain.end(); di!=die; di++)
     {
-        const Z2i::Point point = *di;
-        const Z2i::RealPoint point_real = Z2i::RealPoint(point) - center;
+        const DGtal::Z2i::Point point = *di;
+        const DGtal::Z2i::RealPoint point_real = DGtal::Z2i::RealPoint(point) - center;
         if (point_real.norm() < 1.*radius/6.) continue;
         if (point_real.norm() > 4.5*radius/6.) continue;
         set.insert(point);
     }
 
-    center += Z2i::RealPoint(2*(radius/2.+radius/5.),0);
-    for (Z2i::Domain::ConstIterator di=domain.begin(), die=domain.end(); di!=die; di++)
+    center += DGtal::Z2i::RealPoint(2*(radius/2.+radius/5.),0);
+    for (DGtal::Z2i::Domain::ConstIterator di=domain.begin(), die=domain.end(); di!=die; di++)
     {
-        const Z2i::Point point = *di;
-        const Z2i::RealPoint point_real = Z2i::RealPoint(point) - center;
+        const DGtal::Z2i::Point point = *di;
+        const DGtal::Z2i::RealPoint point_real = DGtal::Z2i::RealPoint(point) - center;
         if (point_real.norm() < 1.*radius/6.) continue;
         if (point_real.norm() > 4.5*radius/6.) continue;
         set.insert(point);
