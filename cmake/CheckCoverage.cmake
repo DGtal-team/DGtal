66b7ccfb7a7c44a89ac342345cf1c02a5264b7a1
David Coeurjolly <david.coeurjolly@liris.cnrs.fr>
2014-08-22 22:08:15 +0200

    exclude /usr in coverage

--- cmake/CheckCoverage.cmake
+++ cmake/CheckCoverage.cmake
@@ -7,16 +7,21 @@ IF (WITH_COVERAGE)
   MESSAGE(STATUS "Code coverage enabled")
   message(STATUS "-------------------------------------------------------------------------------")
   SET(REMOVE_PATTERN "*/src/Board/*")
+  SET(REMOVE_PATTERN2 "/usr/*")
   ADD_CUSTOM_COMMAND(TARGET lcov
     COMMAND mkdir -p coverage
     WORKING_DIRECTORY ${CMAKE_BINARY_DIR}
     )
   ADD_CUSTOM_COMMAND(TARGET lcov
-    COMMAND lcov --directory . --capture --output-file ./coverage/stap_all.info --no-checksum --no-external
+    COMMAND lcov --directory . --capture --output-file ./coverage/stap_all.info --no-checksum 
     WORKING_DIRECTORY ${CMAKE_BINARY_DIR}
     )
   ADD_CUSTOM_COMMAND(TARGET lcov
-    COMMAND lcov --directory . -r ./coverage/stap_all.info ${REMOVE_PATTERN} --output-file ./coverage/stap.info
+    COMMAND lcov --directory . -r ./coverage/stap_all.info ${REMOVE_PATTERN} --output-file ./coverage/stap2.info
+    WORKING_DIRECTORY ${CMAKE_BINARY_DIR}
+    )
+  ADD_CUSTOM_COMMAND(TARGET lcov
+    COMMAND lcov --directory . -r ./coverage/stap2.info ${REMOVE_PATTERN2} --output-file ./coverage/stap.info
     WORKING_DIRECTORY ${CMAKE_BINARY_DIR}
     )
   ADD_CUSTOM_COMMAND(TARGET lcov
