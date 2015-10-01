114722ed696dddcce66cb17f035fb50f103721f7
Kacper Pluta <kacper.pluta@esiee.fr>
2015-10-01 16:29:29 +0200

    Missing documentation.

--- src/DGtal/geometry/curves/estimation/CLMSTTangentFromDSS.h
+++ src/DGtal/geometry/curves/estimation/CLMSTTangentFromDSS.h
@@ -42,48 +42,85 @@
 #include "DGtal/base/Common.h"
 #include "boost/concept_check.hpp"
 #include "DGtal/base/ConceptUtils.h"
+#include "DGtal/base/CBidirectionalRange.h"
+#include "DGtal/geometry/curves/CForwardSegmentComputer.h"
 //////////////////////////////////////////////////////////////////////////////
 
 namespace DGtal
 {
-namespace concepts
-{
-  
-  /////////////////////////////////////////////////////////////////////////////
-  // class CLMSTTangentFromDSS
-  /**
-   * Description of \b concept '\b CLMSTTangentFromDSS' <p>
-   * @ingroup Concepts
-   * @brief Aim: Defines the concept describing a functor which calculate a direction
-   * of the 2D DSS and an eccentricity of a given point in this DSS.
-   * 
-   * @tparam T the type that should be a model of CLMSTTangentFromDSS.
-   */
-  template <typename T>
-  struct CLMSTTangentFromDSS : boost::DefaultConstructible<T>, boost::CopyConstructible<T>, boost::Assignable<T>
+  namespace concepts
   {
-    // ----------------------- Types ------------------------------
-    typedef typename T::Value Value;
-    typedef typename T::RealVector RealVector;
-    typedef typename T::TDSS TDSS;
-    // ----------------------- Concept checks ------------------------------
-  public:
-    BOOST_CONCEPT_USAGE(CLMSTTangentFromDSS)
+    
+    /////////////////////////////////////////////////////////////////////////////
+    // class CLMSTTangentFromDSS
+    /**
+     * Description of \b concept '\b CLMSTTangentFromDSS' <p>
+     * @ingroup Concepts
+     * @brief Aim: Defines the concept describing a functor which calculates a direction
+     *        of the 2D DSS and an eccentricity \cite LachaudIVC2007 of a given point in this DSS.
+     * 
+     * ### Refinement of boost::DefaultConstructible<T>, boost::CopyConstructible<T>, boost::Assignable<T>
+     * 
+     * ### Associated types :
+     *    - TDSS a 3d digital straight segment recognition algorithm
+     *    - LambdaFunction @see FunctorsLambdaMST.h
+     *    - Value
+     *    struct Value { RealVector first; double second; Value & operator += ( const Value &); }
+     * 
+     * ### Notation
+     * - \e T : A type that is a model of CLMSTTangentFromDSS
+     * - \e x object of type X
+     * 
+     * ### Definitions
+     * 
+     * ### Valid expressions and semantics
+     * 
+     * | Name  | Expression | Type requirements | Return type   | Precondition | Semantics | Post condition | Complexity |
+     * |-------|------------|-------------------|---------------|--------------|-----------|----------------|------------|
+     * | operator()|x()     |(const TDSS &, const int &, const int &)|T::Value|              | Calculates DSS direction and eccentricity of given points  in the DSS. | |model dependant|
+     * 
+     * ### Invariants
+     * 
+     * ### Models
+     * 
+     * - TangentFromDSS2DFunctor, TangentFromDSS3DFunctor
+     * 
+     * ### Notes
+     * 
+     * @tparam T the type that should be a model of CLMSTTangentFromDSS.
+     */
+    template <typename T>
+    struct CLMSTTangentFromDSS : boost::DefaultConstructible<T>, boost::CopyConstructible<T>, boost::Assignable<T>
     {
-      concepts::ConceptUtils::sameType( vec, v.first );
-      concepts::ConceptUtils::sameType( d, v.second );
-      concepts::ConceptUtils::sameType( v, t.operator()( dss, 0, 10 ) );
-    }
-    // ------------------------- Private Datas --------------------------------
-  private:
-    RealVector vec;
-    Value v;
-    T t;
-    double d;
-    TDSS dss;
-  }; // end of concept CLMSTTangentFromDSS
-
-} // namespace concepts
+      // ----------------------- Types ------------------------------
+      typedef typename T::Value Value;
+      typedef typename T::RealVector RealVector;
+      typedef typename T::TDSS TDSS;
+      // ----------------------- Concept checks ------------------------------
+      BOOST_CONCEPT_ASSERT(( CForwardSegmentComputer < TDSS > ));
+      BOOST_CONCEPT_ASSERT(( CBidirectionalRange < RealVector > ));
+    public:
+      BOOST_CONCEPT_USAGE(CLMSTTangentFromDSS)
+      {
+	concepts::ConceptUtils::sameType( vec, v1.first );
+	concepts::ConceptUtils::sameType( d, v1.second );
+	concepts::ConceptUtils::sameType( v1, x.operator()( dss, 0, 10 ) );
+	concepts::ConceptUtils::sameType( v1, v1 += v2 );
+      }
+      void checkConstConstraints() const
+      {
+	ConceptUtils::sameType( v1, x.operator()( dss, 0, 10 ) );
+      }
+      // ------------------------- Private Datas --------------------------------
+    private:
+      RealVector vec;
+      Value v1, v2;
+      T x;
+      double d;
+      TDSS dss;
+    }; // end of concept CLMSTTangentFromDSS
+    
+  } // namespace concepts
 } // namespace DGtal
 
 //                                                                           //
