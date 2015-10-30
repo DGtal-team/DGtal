7d50680529d07ba41b182aa3108a215d3e90cf0a
David Coeurjolly <david.coeurjolly@liris.cnrs.fr>
2015-10-30 13:08:17 +0100

    Windows build (removing OldAlias and similar deprecated classes)

--- tests/base/testClone2.cpp
+++ tests/base/testClone2.cpp
@@ -39,9 +39,6 @@
 #include "DGtal/base/Clone.h"
 #include "DGtal/base/Alias.h"
 #include "DGtal/base/ConstAlias.h"
-#include "DGtal/base/OldClone.h"
-#include "DGtal/base/OldAlias.h"
-#include "DGtal/base/OldConstAlias.h"
 #include "DGtal/helpers/StdDefs.h"
 
 using namespace DGtal;
@@ -400,16 +397,6 @@ struct TriangleByConstReference {
 
 struct TriangleByValue {
   TriangleByValue( Point a, Point b, Point c )
-    : _a( a ), _b( b ), _c( c ) {}
-  double perimeter() const
-  {
-    return (_a - _b).norm() + (_b - _c).norm() + (_c - _a).norm();
-  }
-  Point _a, _b, _c;
-};
-
-struct TriangleByOldClone {
-  TriangleByOldClone( deprecated::Clone<Point> a, deprecated::Clone<Point> b, deprecated::Clone<Point> c )
     : _a( a ), _b( b ), _c( c ) {}
   double perimeter() const
   {
@@ -918,16 +905,6 @@ bool testCloneTimings()
   trace.beginBlock ( "Total perimeter of triangles with by-const reference parameter passing." );
   double t2 = computeTriangles<TriangleByConstReference>( size );
   trace.info() << "Perimeter is " << t2 << std::endl;
-  ++nb, nbok += Point::nbCreated == Point::nbDeleted ? 1 : 0;
-  ++nb, nbok += Point::nbCreated < nbC ? 1 : 0;
-  trace.info() << "(" << nbok << "/" << nb << ")"
-               << " Point nbCreated=" << Point::nbCreated 
-               << " nbDeleted=" << Point::nbDeleted << std::endl; 
-  Point::reset();
-  trace.endBlock();
-  trace.beginBlock ( "Total perimeter of triangles with by deprecated::Clone parameter passing." );
-  double t3 = computeTriangles<TriangleByOldClone>( size );
-  trace.info() << "Perimeter is " << t3 << std::endl;
   ++nb, nbok += Point::nbCreated == Point::nbDeleted ? 1 : 0;
   ++nb, nbok += Point::nbCreated < nbC ? 1 : 0;
   trace.info() << "(" << nbok << "/" << nb << ")"
