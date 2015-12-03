7ea291884d66faa0c81706bb36c249cc294a51e4
David Coeurjolly <david.coeurjolly@liris.cnrs.fr>
2015-12-03 09:35:23 +0100

    testing c++11 bug

--- src/DGtal/base/Common.h
+++ src/DGtal/base/Common.h
@@ -90,6 +90,11 @@
 #endif // defined( WIN32 )
 
 
+/* This feature is available in gcc versions 2.5 and later.  */
+# if __GNUC__ < 2 || (__GNUC__ == 2 && __GNUC_MINOR__ < 5) || __STRICT_ANSI__
+#  define __attribute__(Spec) /* empty */
+# endif
+
 #include "DGtal/base/Config.h"
 #include "DGtal/base/Trace.h"
 #include "DGtal/base/TraceWriterTerm.h"
