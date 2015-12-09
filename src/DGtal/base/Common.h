21c8b334f630975f77f4585e86ca3861ef640bee
David Coeurjolly <david.coeurjolly@liris.cnrs.fr>
2015-12-09 13:08:07 +0100

    Defining boost::hash on BigIntegers

--- src/DGtal/base/Common.h
+++ src/DGtal/base/Common.h
@@ -89,12 +89,6 @@
 #define secured_sprintf snprintf
 #endif // defined( WIN32 )
 
-
-/* This feature is available in gcc versions 2.5 and later.  */
-#if __GNUC__ < 2 || (__GNUC__ == 2 && __GNUC_MINOR__ < 5) || __STRICT_ANSI__
-#define __attribute__(Spec) /* empty */
-#endif
-
 #include "DGtal/base/Config.h"
 #include "DGtal/base/Trace.h"
 #include "DGtal/base/TraceWriterTerm.h"
