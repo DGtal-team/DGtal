0fb13985e17dfff4107b296fc1f52c0e1ab4a0b4
David Coeurjolly <david.coeurjolly@liris.cnrs.fr>
2016-02-26 13:20:23 +0100

    fixing scripts

--- .travis/getAndCheckDGtalTools.sh
+++ .travis/getAndCheckDGtalTools.sh
@@ -1,7 +1,7 @@
 ## Get and test if DGtalTools compiles in default mode
-
+pwd
 git clone --depth 1 git://github.com/DGtal-team/DGtalTools.git
 cd DGtalTools
 mkdir build ; cd build
-cmake .. -DDGtal_DIR=../../
+cmake .. -DDGtal_DIR=/home/travis/build/dcoeurjo/DGtal
 make -j 2
