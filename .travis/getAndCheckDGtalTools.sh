07400446b1988954136ce09d080ffca972f51e6b
David Coeurjolly <david.coeurjolly@liris.cnrs.fr>
2016-02-26 15:19:30 +0100

    path to dgtal

--- .travis/getAndCheckDGtalTools.sh
+++ .travis/getAndCheckDGtalTools.sh
@@ -3,5 +3,5 @@ pwd
 git clone --depth 1 git://github.com/DGtal-team/DGtalTools.git
 cd DGtalTools
 mkdir build ; cd build
-cmake .. -DDGtal_DIR=/home/travis/build/dcoeurjo/DGtal
+cmake .. -DDGtal_DIR=~/
 make -j 2
