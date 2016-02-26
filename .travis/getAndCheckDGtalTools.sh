ce39649781881a15cefcea1bc214af90c4a3d1db
David Coeurjolly <david.coeurjolly@liris.cnrs.fr>
2016-02-26 16:09:39 +0100

    fixing path

--- .travis/getAndCheckDGtalTools.sh
+++ .travis/getAndCheckDGtalTools.sh
@@ -1,8 +1,8 @@
-## Get and test if DGtalTools compiles in default mode
+## Get and test if DGtalTools compiles
 DGTALPATH=`pwd`
 echo "DGtal path = $DGTALPATH"
 git clone --depth 1 git://github.com/DGtal-team/DGtalTools.git
 cd DGtalTools
 mkdir build ; cd build
-echo cmake .. -DDGtal_DIR=DGTALPATH
+echo cmake .. -DDGtal_DIR=$DGTALPATH
 make -j 2
