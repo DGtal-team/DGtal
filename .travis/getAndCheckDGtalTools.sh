fb42fc0b0161f2bd0f585e4c74161edf8c7c585a
David Coeurjolly <david.coeurjolly@liris.cnrs.fr>
2016-02-26 11:57:08 +0100

    editing travis

--- .travis/getAndCheckDGtalTools.sh
+++ .travis/getAndCheckDGtalTools.sh
@@ -1,6 +1,6 @@
 ## Get and test if DGtalTools compiles in default mode
 
-cd .. && git clone --depth 1 git://github.com/DGtal-team/DGtalTools.git
+git clone --depth 1 git://github.com/DGtal-team/DGtalTools.git
 cd DGtalTools
 mkdir build ; cd build
 cmake .. -DDGtal_DIR=../../
