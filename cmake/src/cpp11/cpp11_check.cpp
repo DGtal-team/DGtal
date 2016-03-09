9b156c49e44a285f9c808cd592b90574efd5e3ef
Roland Denis <roland.denis@univ-savoie.fr>
2016-03-09 15:30:19 +0100

    Checking features instead of __cplusplus that is not well defined in MVS.

--- cmake/src/cpp11/cpp11_check.cpp
+++ cmake/src/cpp11/cpp11_check.cpp
@@ -14,11 +14,60 @@
  *
  **/
 
-#if __cplusplus < 201103L
-  #error Your compiler does not support C++11. Please specify another C++ compiler.
-#endif
+#include <cstddef>
+#include <array>
+#include <vector>
+#include <algorithm>
+#include <utility>
+#include <unordered_map>
+#include <unordered_set>
+#include <forward_list>
+#include <tuple>
+#include <type_traits>
+
+// Templated aliases & tuple
+template < typename T >
+using Record = std::tuple< double, char, T >;
+
+// Delayed return type & decltype.
+template < typename U, typename V >
+constexpr auto AddIt( U u, V v ) -> decltype( u + v ) { return u + v; }
 
 int main()
 {
+  constexpr auto N = 5u;      // constexpr & auto
+  std::array<int, N> myArray; // array
+  std::vector<int> myVector = {1, 2, 3, 4, 5}; // initializer list
+
+  // Range-based loop
+  for ( auto & v : myVector )
+    v += N;
+
+  // New algorithms and lambdas
+  bool valid = std::all_of( myVector.begin(), myVector.end(), [&N] ( int v ) { return v > N; } );
+
+  // decltype, declval & rvalue ref
+  decltype( std::declval< std::vector<int> >()[0] ) && b = myVector[3];
+
+  // Unordered set
+  std::unordered_set<int> mySet;
+  std::unordered_multiset<int> myMultiSet;
+
+  // Unordered map
+  std::unordered_map<int, int> myMap;
+
+  // Forward list
+  std::forward_list< Record<int> > myList;
+
+  // variadic template & std::forward
+  myList.emplace_front( 3.14, 'a', 3 );
+
+  // type_traits & move
+  constexpr bool isMovable = std::is_move_assignable< decltype( AddIt(3, 3.14) ) >::value;
+
+  // nullptr
+  int* ptr = nullptr;
+
+
   return 0;
 }
