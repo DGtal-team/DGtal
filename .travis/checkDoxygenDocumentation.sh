7d6c80716e5dd02cb6e06c301ed3f0412326fafc
David Coeurjolly <david.coeurjolly@liris.cnrs.fr>
2016-06-02 17:46:00 +0200

    checking of doxygen is empty or not

--- .travis/checkDoxygenDocumentation.sh
+++ .travis/checkDoxygenDocumentation.sh
@@ -4,10 +4,20 @@ return_code=0
 return_code2=0
 return_code3=0
 HOMEPATH=$PWD
+
+
 ## We first check that the doxygen.log is empty
-
+##we need to check is 
+if [ -s doxygen.log ]
+then
+    return_code=0
+else
+    return_code=1
+    echo "Doxygen log file not empty !"
+    cat doxygen.log
+fi
+   
 ## We check src code consitency
-
 cd src/
 if ! $( $HOMEPATH/.travis/check_src_file_tag.sh  )
 then
@@ -16,7 +26,6 @@ fi
 cd ..
 
 ## We check examples consistency
-
 cd examples/
 if ! $( $HOMEPATH/.travis/check_examples_file_tag.sh  )
 then
