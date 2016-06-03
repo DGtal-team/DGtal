6c3eff90a4d985a64ca5f1bd60fba06c4bc56c66
David Coeurjolly <david.coeurjolly@liris.cnrs.fr>
2016-06-03 13:31:45 +0200

    fixing log

--- .travis/check_examples_file_tag.sh
+++ .travis/check_examples_file_tag.sh
@@ -6,7 +6,7 @@ return_code=0
 for file in `find * -type f \( -name \*.c -o -name \*.cpp -o -name \*.cxx -o -name \*.h -o -name \*.hpp -o -name \*.hxx \)`
 do
   expected_name=$file
-  if ! $(grep -aqP "^\s*(\**|//[/!]|/\*[\*!])\s*?[@\\\\]example(\s+${expected_name})?\s*$" $file)
+  if ! $(grep -aqE "^\s*(\**|//[/!]|/\*[\*!])\s*?[@\\\\]example(\s+${expected_name})?\s*$" $file)
   then
     echo -E "Error in file $file:"
     echo -E "  expecting \" * @example ${expected_name}\""
