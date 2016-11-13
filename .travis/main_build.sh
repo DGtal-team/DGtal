3b0774dc775b5fb2d6954630769633c633cef0a3
David Coeurjolly <david.coeurjolly@liris.cnrs.fr>
2016-11-13 18:26:20 +0100

    cleanup dgtal build

--- .travis/main_build.sh
+++ .travis/main_build.sh
@@ -10,20 +10,20 @@ echo "Using C++ = $CXXCOMPILER"
 cmake ..  $BTYPE -DCMAKE_CXX_COMPILER=$CXXCOMPILER -DCMAKE_C_COMPILER=$CCOMPILER 
  
 ### DGtal Core build
-if [ $NEEDCORE == "true" ]
+if [ $NEEDCORE == "true" ];
 then
     make -j 3 DGtal && make -j 3 DGtalIO
 fi
 
 
 ### DGtal Examples and Examples
-if [ $NEEDEXAMPLESANDTESTS == "true" ]
+if [ $NEEDEXAMPLESANDTESTS == "true" ];
 then
     cd examples && make  -j 3 && cd ../tests &&  make -j 3 && make test  ARGS=--output-on-failure
 fi
 
 ### DGtal doc
-if [ $BUILD_DOC == "true" ]
+if [ $BUILD_DOC == "true" ];
 then
     make doc
 fi
