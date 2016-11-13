e5a6d53765053afd13daae21196616998a152ce4
David Coeurjolly <david.coeurjolly@liris.cnrs.fr>
2016-11-13 16:23:43 +0100

    travis

--- .travis/before_global.sh
+++ .travis/before_global.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
 
+set -v
+
 BUILD_DOC="false"
 UPLOAD_DOC="false"
 NEED_CORE="true"
 NEEDEXAMPLESANDTESTS="true";
 SRC_DIR="`pwd`"
+
+CCOMPILER="cc"
+CXXCOMPILER="c++"
 
 
 ##Preparing folders
