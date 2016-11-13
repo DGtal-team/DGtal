5068ee0b7a129d0784fb4e605672b1723d8a97a5
David Coeurjolly <david.coeurjolly@liris.cnrs.fr>
2016-11-13 15:44:44 +0100

    Refactoring travis

---
+++ .travis/before_global.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+BUILD_DOC="false"
+UPLOAD_DOC="false"
+NEED_CORE="true"
+NEEDEXAMPLESANDTESTS="true";
+SRC_DIR="`pwd`"
+
+
+##Preparing folders
+mkdir deps/
+mkdir deps/local
---
+++ .travis/install_deps_macos.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+
+BTYPE="$BTYPE -DWITH_HDF5=false" && echo "Disabling HDF5 on MacOS"; 
+
+brew update && brew install boost doxygen homebrew/science/hdf5 graphviz graphicsmagick  gmp libqglviewer fftw
+
+
