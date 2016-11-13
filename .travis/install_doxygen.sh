b3864506a695fb9baf9a40b07cffe03a7b406e0d
David Coeurjolly <david.coeurjolly@liris.cnrs.fr>
2016-11-13 15:44:44 +0100

    Refactoring travis

--- .travis/install_doxygen.sh
+++ .travis/install_doxygen.sh
@@ -1,4 +1,8 @@
+#!/bin/bash
+
+#
+##Download and install 1.8.1
+#
 
 mkdir ~/doxygen && cd ~/doxygen
-wget http://ftp.stack.nl/pub/users/dimitri/doxygen-1.8.10.linux.bin.tar.gz
-tar xzf doxygen-1.8.10.linux.bin.tar.gz
+wget http://ftp.stack.nl/pub/users/dimitri/doxygen-1.8.10.linux.bin.tar.gz && tar xzf doxygen-1.8.10.linux.bin.tar.gz
