e5ef3f0e1477b61e8445cd35d25042cda64a9666
David Coeurjolly <david.coeurjolly@liris.cnrs.fr>
2016-11-14 13:15:21 +0100

    clang

--- .travis/before_global.sh
+++ .travis/before_global.sh
@@ -6,8 +6,16 @@ export NEEDCORE="true"
 export NEEDEXAMPLESANDTESTS="true";
 export SRC_DIR="`pwd`"
 
-export CCOMPILER="gcc-4.8"
-export CXXCOMPILER="g++-4.8"
+export CCOMPILER=$CC
+export CXXCOMPILER=$CXX
+
+##Forcing gcc-4.8
+if [ $CC = "gcc" ];
+then
+    export CCOMPILER="gcc-4.8"
+    export CXXCOMPILER="g++-4.8"
+fi
+
 
 ##Preparing folders
 mkdir deps/
