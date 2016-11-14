7c1a90ffa42ef0ace03674d233923871db913f4f
David Coeurjolly <david.coeurjolly@liris.cnrs.fr>
2016-11-14 15:16:20 +0100

    Update install_deps_macos.sh

--- .travis/install_deps_macos.sh
+++ .travis/install_deps_macos.sh
@@ -4,6 +4,6 @@ set -e
 
 BTYPE="$BTYPE -DWITH_HDF5=false" && echo "Disabling HDF5 on MacOS"; 
 
-brew update && brew install boost doxygen homebrew/science/hdf5 graphviz graphicsmagick  gmp libqglviewer fftw
+brew update && brew install boost doxygen homebrew/science/hdf5 graphviz graphicsmagick  gmp  fftw
 
 brew install homebrew/boneyard/libqglviewer
