a5a24377117db7af6c65f659f9c7088426cef370
Pablo Hernandez-Cerdan <pablo.hernandez.cerdan@outlook.com>
2016-12-08 19:45:14 +1300

    Avoid enforcing std=c++11 when not necessary.
    For example when the default flag of the compiler is
    std=gnu++11,c++14, etc...

--- cmake/CheckCPP11.cmake
+++ cmake/CheckCPP11.cmake
@@ -2,10 +2,21 @@
 # Test some c++11 functionalities
 # -----------------------------------------------------------------------------
 
-try_compile( CPP11_COMPATIBLE 
+try_compile( CPP11_COMPATIBLE_BY_DEFAULT
   ${CMAKE_BINARY_DIR}/CMakeTmp
   ${CMAKE_SOURCE_DIR}/cmake/src/cpp11/cpp11_check.cpp
-  COMPILE_DEFINITIONS "-std=c++11"
   OUTPUT_VARIABLE OUTPUT
   )
+
+IF (NOT CPP11_COMPATIBLE_BY_DEFAULT)
+  try_compile( CPP11_COMPATIBLE_AFTER_FLAG
+    ${CMAKE_BINARY_DIR}/CMakeTmp
+    ${CMAKE_SOURCE_DIR}/cmake/src/cpp11/cpp11_check.cpp
+    COMPILE_DEFINITIONS "-std=c++11"
+    OUTPUT_VARIABLE OUTPUT
+    )
+  SET(CPP11_COMPATIBLE_AFTER_FLAG CPP11_COMPATIBLE_AFTER_FLAG PARENT_SCOPE)
+ENDIF()
+
+SET(CPP11_COMPATIBLE CPP11_COMPATIBLE_BY_DEFAULT OR CPP11_COMPATIBLE_AFTER_FLAG)
 
