c7b15f660100c1615c6fa31df42be31625138f9c
David Coeurjolly <david.coeurjolly@liris.cnrs.fr>
2017-01-07 22:46:15 +0100

    Update install_deps_macos.sh

--- .travis/install_deps_macos.sh
+++ .travis/install_deps_macos.sh
@@ -3,7 +3,7 @@
 
 ## Temporary HDF5 build issue
 export BTYPE="$BTYPE -DWITH_HDF5=false" && echo "Disabling HDF5 on MacOS";
-export BTYPE="$BTYPE -DWITH_QT5=true -DQt5_DIR=/usr/local/Cellar/qt5/5.7.1_1/lib/cmake/Qt5/" && echo "Forcing Qt5 on MacOS"; 
+export BTYPE="$BTYPE -DWITH_QT5=true -DQt5_DIR=$(brew --prefix qt5)" && echo "Forcing Qt5 on MacOS"; 
 
 #
 # Note: gmp and boost already installed
