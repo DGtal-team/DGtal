2bae46cd5d3dbb80e9cae381a705d86aa7d64147
David Coeurjolly <david.coeurjolly@liris.cnrs.fr>
2017-01-08 13:56:13 +0100

    WIP

--- .travis/install_deps_macos.sh
+++ .travis/install_deps_macos.sh
@@ -8,4 +8,4 @@ brew install qt5 doxygen homebrew/science/hdf5 graphviz graphicsmagick fftw eige
 
 ## Temporary HDF5 build issue
 export BTYPE="$BTYPE -DWITH_HDF5=false" && echo "Disabling HDF5 on MacOS";
-export BTYPE="$BTYPE -DWITH_QT5=true -DQt5_DIR=$(brew --prefix qt5)" && echo "Forcing Qt5 on MacOS";
+export BTYPE="$BTYPE -DWITH_QT5=true -DCMAKE_PREFIX_PATH=$(brew --prefix qt5)" && echo "Forcing Qt5 on MacOS";
