61cb1d516723a6277b9de4931d725a411abd29c3
David Coeurjolly <david.coeurjolly@liris.cnrs.fr>
2017-01-08 15:48:21 +0100

    reactivating linux bots

--- .travis/install_deps_macos.sh
+++ .travis/install_deps_macos.sh
@@ -4,7 +4,7 @@
 # Note: gmp and boost already installed
 #
 brew update
-brew install qt5 doxygen homebrew/science/hdf5 graphviz graphicsmagick fftw eigen homebrew/boneyard/libqglviewer
+brew install qt5 doxygen  graphviz graphicsmagick fftw eigen homebrew/boneyard/libqglviewer
 
 ## Temporary HDF5 build issue
 export BTYPE="$BTYPE -DWITH_HDF5=false" && echo "Disabling HDF5 on MacOS";
