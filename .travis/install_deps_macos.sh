87cc679da4881217e52e5d31cbf3553d4351389b
David Coeurjolly <david.coeurjolly@liris.cnrs.fr>
2017-01-08 10:25:23 +0100

    WIP

--- .travis/install_deps_macos.sh
+++ .travis/install_deps_macos.sh
@@ -1,13 +1,11 @@
 #!/bin/bash
 
+#
+# Note: gmp and boost already installed
+#
+brew update
+brew install qt5 doxygen homebrew/science/hdf5 graphviz graphicsmagick fftw eigen homebrew/boneyard/libqglviewer
 
 ## Temporary HDF5 build issue
 export BTYPE="$BTYPE -DWITH_HDF5=false" && echo "Disabling HDF5 on MacOS";
 export BTYPE="$BTYPE -DWITH_QT5=true -DQt5_DIR=$(brew --prefix qt5)" && echo "Forcing Qt5 on MacOS";
-
-#
-# Note: gmp and boost already installed
-#
-
-brew update
-brew install qt5 doxygen homebrew/science/hdf5 graphviz graphicsmagick fftw eigen homebrew/boneyard/libqglviewer
