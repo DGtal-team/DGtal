bff9be3528a17c150f62a4a4e99d5fa1a60047b7
Pablo Hernandez-Cerdan <pablo.hernandez.cerdan@outlook.com>
2017-01-20 02:12:41 +1300

    Fix configure_file in tables.

--- cmake/NeighborhoodTablesConfig.cmake
+++ cmake/NeighborhoodTablesConfig.cmake
@@ -6,23 +6,23 @@
 
 # ------ Build Tree ------ #
 #--- Configuration of the src/topology/tables/NeighborhoodTables.h.in
-set(TABLE_DIR ${PROJECT_BINARY_DIR}/src/DGtal/topology/tables)
+set(TABLE_DIR ${PROJECT_SOURCE_DIR}/src/DGtal/topology/tables)
 configure_file(
   ${PROJECT_SOURCE_DIR}/src/DGtal/topology/tables/NeighborhoodTables.h.in
   ${PROJECT_BINARY_DIR}/src/DGtal/topology/tables/NeighborhoodTables.h)
 
 # ------ Install Tree ------ #
 #--- Configuration of the src/topology/tables/NeighborhoodTables.h.in for the install tree. Save to tmp file.
-set(TABLE_DIR ${INSTALL_INCLUDE_DIR}/DGtal/topology/tables)
+# dev note: The variables are expanded before they are passed to install(CODE). We have to scape TABLE_DIR inside configure_file if we want it to be updated with the value set inside the install script.
+install(CODE "
+set(TABLE_DIR $ENV{DESTDIR}${INSTALL_INCLUDE_DIR}/DGtal/topology/tables)
 configure_file(
   ${PROJECT_SOURCE_DIR}/src/DGtal/topology/tables/NeighborhoodTables.h.in
-  ${PROJECT_BINARY_DIR}/InstallFiles/NeighborhoodTables.h)
+  \${TABLE_DIR}/NeighborhoodTables.h @ONLY)")
 
 #--- Install compressed tables and the header pointing to them ---#
 set(table_folder_install ${INSTALL_INCLUDE_DIR}/DGtal/topology/tables)
-install(FILES ${PROJECT_BINARY_DIR}/InstallFiles/NeighborhoodTables.h
-        DESTINATION ${table_folder_install})
 #--- Select all the tables and install ---#
-install(DIRECTORY ${PROJECT_SOURCE_DIR}/src/DGtal/topology/tables/
-        DESTINATION ${table_folder_install}
+install(DIRECTORY "${PROJECT_SOURCE_DIR}/src/DGtal/topology/tables/"
+        DESTINATION "${table_folder_install}"
         FILES_MATCHING PATTERN "*.zlib")
