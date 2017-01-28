3126c9fd82ef43fa38868eba519a1ed5c4b10410
Pablo Hernandez-Cerdan <pablo.hernandez.cerdan@outlook.com>
2017-01-28 17:02:00 +1300

    [FIX] Let CMake handle DESTDIR instead of us.
    Install a temporary file in InstallFiles at build time, and then use regular
    install(FILES), which takes into account any DESTDIR logic.

--- cmake/NeighborhoodTablesConfig.cmake
+++ cmake/NeighborhoodTablesConfig.cmake
@@ -13,17 +13,17 @@ configure_file(
 
 # ------ Install Tree ------ #
 #--- Configuration of the src/topology/tables/NeighborhoodTables.h.in for the install tree. Save to tmp file.
-# dev note: we escape \$ENV{DESTDIR} because we want DESTDIR to be read at
-#  install time, not at configure time. The same with TABLE_DIR inside configure_file.
-#  Read more: https://cmake.org/pipermail/cmake-developers/2013-January/017810.html
-install(CODE "
-set(TABLE_DIR \$ENV{DESTDIR}${INSTALL_INCLUDE_DIR}/DGtal/topology/tables)
+set(TABLE_DIR ${INSTALL_INCLUDE_DIR}/DGtal/topology/tables)
 configure_file(
   ${PROJECT_SOURCE_DIR}/src/DGtal/topology/tables/NeighborhoodTables.h.in
-  \${TABLE_DIR}/NeighborhoodTables.h @ONLY)")
+  ${PROJECT_BINARY_DIR}/InstallFiles/NeighborhoodTables.h @ONLY)
 
 #--- Install compressed tables and the header pointing to them ---#
 set(table_folder_install ${INSTALL_INCLUDE_DIR}/DGtal/topology/tables)
+
+#--- Install header pointing to tables. ---#
+install(FILES ${PROJECT_BINARY_DIR}/InstallFiles/NeighborhoodTables.h
+        DESTINATION "${table_folder_install}")
 #--- Select all the tables and install ---#
 install(DIRECTORY "${PROJECT_SOURCE_DIR}/src/DGtal/topology/tables/"
         DESTINATION "${table_folder_install}"
