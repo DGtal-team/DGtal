d3994852a7b034284880b8a117eae8999c5423e0
Thomas Caissard <cgurps@gmail.com>
2017-06-22 08:31:33 +0200

    removing unused variables

--- src/DGtal/shapes/parametric/StarShaped2D.h
+++ src/DGtal/shapes/parametric/StarShaped2D.h
@@ -212,10 +212,9 @@ namespace DGtal
      * @param left a point that is supposed to be projected left of p (regarding the angle)
      * @param right a point that is supposed to be projected right of p (regarding the angle)
      * @param step precision of the approximation
-     * @param h the grid step
      * @return a point that lies between the projection of left and right and that is the closest regarding the \f$L_2\f$ norm.
      * */
-    RealPoint closestPointWithWitnesses( const RealPoint& p, const RealPoint& left, const RealPoint& right, const int step, const double h ) const;
+    RealPoint closestPointWithWitnesses( const RealPoint& p, const RealPoint& left, const RealPoint& right, const int step) const;
 
     // ----------------------- Interface --------------------------------------
   public:
