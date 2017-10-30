7d5e33d22726985132b45509de941bf45c5e82ce
Roland Denis <roland.denis@math.univ-lyon1.fr>
2017-10-30 12:09:12 +0100

    Removing deprecated throw exception specifications.
    And replacing throw() by noexcept.

--- src/DGtal/io/readers/VolReader.h
+++ src/DGtal/io/readers/VolReader.h
@@ -112,7 +112,7 @@ namespace DGtal
      * @return an instance of the ImageContainer.
      */
     static ImageContainer importVol(const std::string & filename, 
-                                    const Functor & aFunctor =  Functor()) throw(DGtal::IOException);
+                                    const Functor & aFunctor =  Functor());
     
   private:
 
