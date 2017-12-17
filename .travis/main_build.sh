b2711182d04546f51208da2f3e8f787802a5fd3b
David Coeurjolly <david.coeurjolly@liris.cnrs.fr>
2017-12-17 10:36:22 +0100

    Switching to ninja build

--- .travis/main_build.sh
+++ .travis/main_build.sh
@@ -7,27 +7,22 @@ cd build
 
 ### Cmake
 echo "Using C++ = $CXXCOMPILER"
-cmake ..  $BTYPE -DCMAKE_CXX_COMPILER=$CXXCOMPILER -DCMAKE_C_COMPILER=$CCOMPILER
+cmake ..  $BTYPE -DCMAKE_CXX_COMPILER=$CXXCOMPILER -DCMAKE_C_COMPILER=$CCOMPILER -G Ninja
 
 
-### DGtal Core build
-if [ $NEEDCORE = "true" ];
-then
-    make -j 3 DGtal
-fi
+### DGtal  build
+ninja
 
 echo "NeedExample $NEEDEXAMPLESANDTESTS"
 ### DGtal Examples and Examples
 if [ $NEEDEXAMPLESANDTESTS = "true" ];
 then
-    cd examples ; make  -j 3
-    cd ../tests ;  make -j 3
-    ctest -j 3--output-on-failure
+    ctest -j 3 --output-on-failure
 fi
 
 ### DGtal doc
 if [ $BUILD_DOC = "true" ];
 then
     echo "Building the doc"
-    make doc > buildDoc.log
+    ninja doc
 fi
