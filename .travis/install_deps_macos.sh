e94a8764e2ed31bfa6af40eae753de001b852c6f
David Coeurjolly <david.coeurjolly@liris.cnrs.fr>
2018-01-05 20:57:35 +0100

    debuging

--- .travis/install_deps_macos.sh
+++ .travis/install_deps_macos.sh
@@ -5,7 +5,7 @@
 #
 brew update
 #brew install qt5 doxygen homebrew/science/hdf5 graphviz graphicsmagick fftw eigen
-brew install qt5 graphicsmagick imagemagick  fftw eigen
+brew install qt5 graphicsmagick fftw eigen
 # Explicit install of libqglviewer
 brew install http://liris.cnrs.fr/david.coeurjolly/misc/libqglviewer.rb
 
