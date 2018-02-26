b34578b7f01bada8abcfec35edb90dba5a902c19
gurps <cgurps@gmail.com>
2018-02-26 10:14:29 +0100

    Fix Indent & Unused Parameter & Shadow Parameter

--- examples/dec/exampleHeatLaplace.cpp
+++ examples/dec/exampleHeatLaplace.cpp
@@ -99,149 +99,149 @@ std::function<double(const RealPoint&)> xx_derivative =
 //! [heat_functions]
 
 template <typename Shape>
-void convergence(const Options& options, Shape& shape, int argc, char** argv,
+void convergence(const Options& options, Shape& shape,
         const std::function< double(const RealPoint&) >& input_function,
-        const std::function< double(const RealPoint&) >& result_function,
-        const std::function< double(const RealPoint&, const RealPoint&) >& distance_function)
+        const std::function< double(const RealPoint&) >& result_function)
 {
-    trace.beginBlock("Laplacian 3D");
+  trace.beginBlock("Laplacian 3D");
 
-    trace.beginBlock("Extracting Digital Surface");
+  trace.beginBlock("Extracting Digital Surface");
 
-    Board3D<Space, KSpace> board;
+  Board3D<Space, KSpace> board;
 
 //! [surface_extract]
-    typedef GaussDigitizer<Z3i::Space, Shape> Digitizer;
+  typedef GaussDigitizer<Z3i::Space, Shape> Digitizer;
 
-    Digitizer digitizer;
-    digitizer.attach(shape);
-    digitizer.init(shape.getLowerBound() + Z3i::Vector(-1,-1,-1), shape.getUpperBound() + Z3i::Vector(1,1,1), options.h);
+  Digitizer digitizer;
+  digitizer.attach(shape);
+  digitizer.init(shape.getLowerBound() + Z3i::Vector(-1,-1,-1), shape.getUpperBound() + Z3i::Vector(1,1,1), options.h);
 
-    Z3i::Domain domain = digitizer.getDomain();
+  Z3i::Domain domain = digitizer.getDomain();
 
-    Z3i::KSpace kspace;
-    bool ok = kspace.init(domain.lowerBound(), domain.upperBound(), true);
-    if( !ok ) std::cerr << "KSpace init failed" << std::endl;
+  Z3i::KSpace kspace;
+  bool ok = kspace.init(domain.lowerBound(), domain.upperBound(), true);
+  if( !ok ) std::cerr << "KSpace init failed" << std::endl;
 
-    typedef KSpace::SurfelSet SurfelSet;
-    typedef SetOfSurfels< KSpace, SurfelSet > MySetOfSurfels;
-    typedef DigitalSurface< MySetOfSurfels > MyDigitalSurface;
-    typedef SurfelAdjacency<KSpace::dimension> MySurfelAdjacency;
+  typedef KSpace::SurfelSet SurfelSet;
+  typedef SetOfSurfels< KSpace, SurfelSet > MySetOfSurfels;
+  typedef DigitalSurface< MySetOfSurfels > MyDigitalSurface;
+  typedef SurfelAdjacency<KSpace::dimension> MySurfelAdjacency;
 
-    MySurfelAdjacency surfAdj( true ); // interior in all directions.
-    MySetOfSurfels theSetOfSurfels( kspace, surfAdj );
-    Surfaces<KSpace>::sMakeBoundary( theSetOfSurfels.surfelSet(),
-                     kspace, digitizer,
-                     domain.lowerBound(),
-                     domain.upperBound() );
-    MyDigitalSurface digSurf( theSetOfSurfels );
+  MySurfelAdjacency surfAdj( true ); // interior in all directions.
+  MySetOfSurfels theSetOfSurfels( kspace, surfAdj );
+  Surfaces<KSpace>::sMakeBoundary( theSetOfSurfels.surfelSet(),
+                   kspace, digitizer,
+                   domain.lowerBound(),
+                   domain.upperBound() );
+  MyDigitalSurface digSurf( theSetOfSurfels );
 //! [surface_extract]
-    trace.info() << "Digital Surface has " << digSurf.size() << " surfels." << std::endl;
+  trace.info() << "Digital Surface has " << digSurf.size() << " surfels." << std::endl;
 
-    trace.endBlock();
+  trace.endBlock();
 
-    trace.beginBlock("Initializing Normal Functor");
+  trace.beginBlock("Initializing Normal Functor");
 //! [normal_estimation]
-    typedef CanonicSCellEmbedder<KSpace> CanonicSCellEmbedder;
-    CanonicSCellEmbedder canonicSCellEmbedder(kspace);
+  typedef CanonicSCellEmbedder<KSpace> CanonicSCellEmbedder;
+  CanonicSCellEmbedder canonicSCellEmbedder(kspace);
 
-    typedef functors::IINormalDirectionFunctor<Space> MyIINormalFunctor;
-    typedef IntegralInvariantCovarianceEstimator<KSpace, Digitizer, MyIINormalFunctor> MyIINormalEstimator;
+  typedef functors::IINormalDirectionFunctor<Space> MyIINormalFunctor;
+  typedef IntegralInvariantCovarianceEstimator<KSpace, Digitizer, MyIINormalFunctor> MyIINormalEstimator;
 
-    const double radius = options.normal_radius * pow(options.h, 1. / 3.);
+  const double radius = options.normal_radius * pow(options.h, 1. / 3.);
 
-    MyIINormalFunctor normalFunctor;
-    normalFunctor.init(options.h, radius);
+  MyIINormalFunctor normalFunctor;
+  normalFunctor.init(options.h, radius);
 
-    MyIINormalEstimator normalEstimator(normalFunctor);
-    normalEstimator.attach(kspace, digitizer);
-    normalEstimator.setParams(radius / options.h);
+  MyIINormalEstimator normalEstimator(normalFunctor);
+  normalEstimator.attach(kspace, digitizer);
+  normalEstimator.setParams(radius / options.h);
 
-    normalEstimator.init(options.h, digSurf.begin(), digSurf.end());
+  normalEstimator.init(options.h, digSurf.begin(), digSurf.end());
 //! [normal_estimation]
-    trace.endBlock();
+  trace.endBlock();
 
-    trace.beginBlock("Initializing DEC");
+  trace.beginBlock("Initializing DEC");
 //! [init_dec]
-    typedef DiscreteExteriorCalculus<2, 3, EigenLinearAlgebraBackend> Calculus;
-    typedef DiscreteExteriorCalculusFactory<EigenLinearAlgebraBackend> CalculusFactory;
+  typedef DiscreteExteriorCalculus<2, 3, EigenLinearAlgebraBackend> Calculus;
+  typedef DiscreteExteriorCalculusFactory<EigenLinearAlgebraBackend> CalculusFactory;
 
-    const Calculus calculus = CalculusFactory::createFromNSCells<2>(digSurf.begin(), digSurf.end(), normalEstimator, options.h);
+  const Calculus calculus = CalculusFactory::createFromNSCells<2>(digSurf.begin(), digSurf.end(), normalEstimator, options.h);
 //! [init_dec]
-    trace.info() << calculus << std::endl;
+  trace.info() << calculus << std::endl;
 
-    trace.endBlock();
-    trace.beginBlock("Computing the input function");
+  trace.endBlock();
+  trace.beginBlock("Computing the input function");
 //! [input_function]
-    Calculus::DualForm0 input_func(calculus);
+  Calculus::DualForm0 input_func(calculus);
 
-    for(auto itb = digSurf.begin(), ite = digSurf.end(); itb != ite; itb++)
-    {
-        const Calculus::Index i_calc = calculus.getCellIndex( kspace.unsigns( *itb ) );
-        input_func.myContainer( i_calc ) = input_function( options.h * canonicSCellEmbedder( *itb ) );
-    }
+  for(auto itb = digSurf.begin(), ite = digSurf.end(); itb != ite; itb++)
+  {
+    const Calculus::Index i_calc = calculus.getCellIndex( kspace.unsigns( *itb ) );
+    input_func.myContainer( i_calc ) = input_function( options.h * canonicSCellEmbedder( *itb ) );
+  }
 //! [input_function]
-    trace.endBlock();
+  trace.endBlock();
 
-    trace.beginBlock("Computing the Laplace operator");
+  trace.beginBlock("Computing the Laplace operator");
 //! [laplace_operator]
-    const double t = options.convolution_radius * pow(options.h, 2. / 3.);
-    const double K = log( - log1p( t ) ) + 2.;
-    const Calculus::DualIdentity0 laplace = calculus.heatLaplace<DUAL>(options.h, t, K);
+  const double t = options.convolution_radius * pow(options.h, 2. / 3.);
+  const double K = log( - log1p( t ) ) + 2.;
+  const Calculus::DualIdentity0 laplace = calculus.heatLaplace<DUAL>(options.h, t, K);
 //! [laplace_operator]
-    trace.info() << "Matrix has " << ((double)laplace.myContainer.nonZeros() / (double)laplace.myContainer.size() * 100.) << "% of non-zeros elements." << std::endl;
-    trace.endBlock();
+  trace.info() << "Matrix has " << ((double)laplace.myContainer.nonZeros() / (double)laplace.myContainer.size() * 100.) << "% of non-zeros elements." << std::endl;
+  trace.endBlock();
 
 //! [operator_result]
-    const Eigen::VectorXd laplace_result = (laplace * input_func).myContainer;
+  const Eigen::VectorXd laplace_result = (laplace * input_func).myContainer;
 //! [operator_result]
-    Eigen::VectorXd error( digSurf.size() );
-    Eigen::VectorXd real_laplacian_values( digSurf.size() );
-    Eigen::VectorXd estimated_laplacian_values( digSurf.size() );
+  Eigen::VectorXd error( digSurf.size() );
+  Eigen::VectorXd real_laplacian_values( digSurf.size() );
+  Eigen::VectorXd estimated_laplacian_values( digSurf.size() );
 
-    int i = 0;
-    for(auto itb = digSurf.begin(), ite = digSurf.end(); itb != ite; itb++)
-    {
-        const Calculus::Index i_calc = calculus.getCellIndex( kspace.unsigns( *itb ) );
+  int i = 0;
+  for(auto itb = digSurf.begin(), ite = digSurf.end(); itb != ite; itb++)
+  {
+    const Calculus::Index i_calc = calculus.getCellIndex( kspace.unsigns( *itb ) );
 
-        const RealPoint p = options.h * canonicSCellEmbedder( *itb );
-        const RealPoint p_normalized = p / p.norm();
-        const RealPoint p_s = cartesian_to_spherical( p );
+    const RealPoint p = options.h * canonicSCellEmbedder( *itb );
+    const RealPoint p_normalized = p / p.norm();
+    const RealPoint p_s = cartesian_to_spherical( p );
 
-        const double real_laplacian_value = result_function( p_normalized );
-        const double estimated_laplacian_value = laplace_result( i_calc );
+    const double real_laplacian_value = result_function( p_normalized );
+    const double estimated_laplacian_value = laplace_result( i_calc );
 
-        estimated_laplacian_values(i) = estimated_laplacian_value;
-        real_laplacian_values(i) = real_laplacian_value;
+    estimated_laplacian_values(i) = estimated_laplacian_value;
+    real_laplacian_values(i) = real_laplacian_value;
 
-        error(i) = estimated_laplacian_value - real_laplacian_value;
+    error(i) = estimated_laplacian_value - real_laplacian_value;
 
-        ++i;
-    }
+    ++i;
+  }
 
-    trace.info() << "Estimated Laplacian Range : " << estimated_laplacian_values.minCoeff() << " / " << estimated_laplacian_values.maxCoeff() << std::endl;
-    trace.info() << "Real Laplacian Range : " << real_laplacian_values.minCoeff() << " / " << real_laplacian_values.maxCoeff() << std::endl;
+  trace.info() << "Estimated Laplacian Range : " << estimated_laplacian_values.minCoeff() << " / " << estimated_laplacian_values.maxCoeff() << std::endl;
+  trace.info() << "Real Laplacian Range : " << real_laplacian_values.minCoeff() << " / " << real_laplacian_values.maxCoeff() << std::endl;
 
-    trace.info() << "h = " << options.h << " t = " << t << " K = " << K << std::endl;
-    trace.info() << "Mean error = " << error.array().abs().mean() << " max error = " << error.array().abs().maxCoeff() << std::endl;
+  trace.info() << "h = " << options.h << " t = " << t << " K = " << K << std::endl;
+  trace.info() << "Mean error = " << error.array().abs().mean() << " max error = " << error.array().abs().maxCoeff() << std::endl;
 
-    trace.endBlock();
+  trace.endBlock();
 }
 
-int main(int argc, char **argv) {
-    Options options;
+int main(int argc, char **argv)
+{
+  Options options;
 
-    options.h = 0.1;
-    options.normal_radius = 2.0;
-    options.convolution_radius = 0.1;
+  options.h = 0.1;
+  options.normal_radius = 2.0;
+  options.convolution_radius = 0.1;
 
-    typedef Ball3D<Z3i::Space> Ball;
-    Ball ball(Point(0.0,0.0,0.0), 1.0);
+  typedef Ball3D<Z3i::Space> Ball;
+  Ball ball(Point(0.0,0.0,0.0), 1.0);
 
-    std::function<double(const RealPoint&, const RealPoint&)> l2_distance =
-        [](const RealPoint& a, const RealPoint& b) { return (a - b).norm(); };
+  std::function<double(const RealPoint&, const RealPoint&)> l2_distance =
+      [](const RealPoint& a, const RealPoint& b) { return (a - b).norm(); };
 
-    convergence<Ball>(options, ball, argc, argv, xx_function, xx_derivative, l2_distance);
+  convergence<Ball>(options, ball, xx_function, xx_derivative);
 
-    return 0;
+  return 0;
 }
