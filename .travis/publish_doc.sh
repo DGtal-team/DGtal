dc9d12f00d4c8d81cce1bd47bd2d7c9545a8d836
Roland Denis <roland.denis@math.univ-lyon1.fr>
2018-07-12 15:14:45 +0200

    Fixing online documentation publication

--- .travis/publish_doc.sh
+++ .travis/publish_doc.sh
@@ -1,26 +1,22 @@
 #!/bin/bash
-
 $SCRIPT_BEGIN
 
-cd "$BUILD_DIR"
-echo $HOME
-rsync -azv --delete --delete-after -e 'ssh -oStrictHostKeyChecking=no -i  /home/travis/build/DGtal-team/DGtal/.travis/dgtal_rsa' html/ dgtal@connect.liris.cnrs.fr:/home-projets/dgtal/public_html/doc/nightly/
-
+rsync -azv --delete --delete-after -e 'ssh -oStrictHostKeyChecking=no -i "$SRC_DIR/.travis/dgtal_rsa"' "$BUILD_DIR/html/" dgtal@connect.liris.cnrs.fr:/home-projets/dgtal/public_html/doc/nightly/
 
 ##DOCSET build
-cd html
-make
+cd "$BUILD_DIR/html"
+
+set +e
+make  # Build is expected to fail when launching docsetutil. However, the result is still usable in Dash.
+set -e
+
 wget --no-check-certificate http://dgtal.org/doc/docset/template.tgz
 tar zxvf template.tgz ; mv template/* org.dgtal.docset
 tar zcvf DGtal-devel.tgz org.dgtal.docset
 
-cd ..
-
-rsync  -azv --delete --delete-after -e 'ssh -oStrictHostKeyChecking=no -i  .travis/dgtal_rsa' html/DGtal-devel.tgz dgtal@connect.liris.cnrs.fr:/home-projets/dgtal/public_html/doc/docset
-
+rsync  -azv --delete --delete-after -e 'ssh -oStrictHostKeyChecking=no -i "$SRC_DIR/.travis/dgtal_rsa"' "$BUILD_DIR/html/DGtal-devel.tgz" dgtal@connect.liris.cnrs.fr:/home-projets/dgtal/public_html/doc/docset
 
 ###TAGS for DGtalTools
-scp -i  /home/travis/build/DGtal-team/DGtal/.travis/dgtal_rsa  DGtal-tagfile dgtal@connect.liris.cnrs.fr:/home-projets/dgtal/public_html/doc/tags/
-scp -i  /home/travis/build/DGtal-team/DGtal/.travis/dgtal_rsa  Board-tagfile dgtal@connect.liris.cnrs.fr:/home-projets/dgtal/public_html/doc/tags/
+scp -i "$SRC_DIR/.travis/dgtal_rsa"  "$BUILD_DIR/DGtal-tagfile" dgtal@connect.liris.cnrs.fr:/home-projets/dgtal/public_html/doc/tags/
 
 $SCRIPT_END
