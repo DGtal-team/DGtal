9e17147f657cbc8052f76cd5d665740424c4b6b8
Roland Denis <roland.denis@math.univ-lyon1.fr>
2018-07-16 09:53:13 +0200

    Fixing single/double quotes in public_doc.sh
    Thanks to codacy...

--- .travis/publish_doc.sh
+++ .travis/publish_doc.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 $SCRIPT_BEGIN
 
-rsync -azv --delete --delete-after -e 'ssh -oStrictHostKeyChecking=no -i "$SRC_DIR/.travis/dgtal_rsa"' "$BUILD_DIR/html/" dgtal@connect.liris.cnrs.fr:/home-projets/dgtal/public_html/doc/nightly/
+rsync -azv --delete --delete-after -e "ssh -oStrictHostKeyChecking=no -i '$SRC_DIR/.travis/dgtal_rsa'" "$BUILD_DIR/html/" dgtal@connect.liris.cnrs.fr:/home-projets/dgtal/public_html/doc/nightly/
 
 ##DOCSET build
 cd "$BUILD_DIR/html"
@@ -14,7 +14,7 @@ wget --no-check-certificate http://dgtal.org/doc/docset/template.tgz
 tar zxvf template.tgz ; mv template/* org.dgtal.docset
 tar zcvf DGtal-devel.tgz org.dgtal.docset
 
-rsync  -azv --delete --delete-after -e 'ssh -oStrictHostKeyChecking=no -i "$SRC_DIR/.travis/dgtal_rsa"' "$BUILD_DIR/html/DGtal-devel.tgz" dgtal@connect.liris.cnrs.fr:/home-projets/dgtal/public_html/doc/docset
+rsync  -azv --delete --delete-after -e "ssh -oStrictHostKeyChecking=no -i '$SRC_DIR/.travis/dgtal_rsa'" "$BUILD_DIR/html/DGtal-devel.tgz" dgtal@connect.liris.cnrs.fr:/home-projets/dgtal/public_html/doc/docset
 
 ###TAGS for DGtalTools
 scp -i "$SRC_DIR/.travis/dgtal_rsa"  "$BUILD_DIR/DGtal-tagfile" dgtal@connect.liris.cnrs.fr:/home-projets/dgtal/public_html/doc/tags/
