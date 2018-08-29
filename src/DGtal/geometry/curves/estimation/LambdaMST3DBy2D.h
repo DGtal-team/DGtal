7dd3d7a9e0b84d11baa351f0bb6548d1071a8366
Kacper Pluta <kacper.pluta@esiee.fr>
2018-08-29 12:41:44 +0200

    remove commas to avoid doc. errors.

--- src/DGtal/geometry/curves/estimation/LambdaMST3DBy2D.h
+++ src/DGtal/geometry/curves/estimation/LambdaMST3DBy2D.h
@@ -77,8 +77,8 @@ namespace DGtal {
     
     /**
      * Initialisation.
-     * @param itB, begin iterator
-     * @param itE, end iterator
+     * @param itB begin iterator
+     * @param itE end iterator
      */
     void init ( Iterator3D itB, Iterator3D itE );
     
