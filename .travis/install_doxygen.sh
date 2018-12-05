b53d02192ed550fcde0dd10de7b4ed714a0d2329
David Coeurjolly <david.coeurjolly@liris.cnrs.fr>
2018-12-05 11:27:10 +0100

    Fixing doxygen scripts

--- .travis/install_doxygen.sh
+++ .travis/install_doxygen.sh
@@ -2,10 +2,10 @@
 $SCRIPT_BEGIN
 
 #
-##Download and install 1.8.1
+##Download and install 1.8.14
 #
 
 mkdir ~/doxygen && cd ~/doxygen
-wget http://ftp.stack.nl/pub/users/dimitri/doxygen-1.8.10.linux.bin.tar.gz && tar xzf doxygen-1.8.10.linux.bin.tar.gz
+wget http://doxygen.nl/files/doxygen-1.8.14.linux.bin.tar.gz && tar xzf doxygen-1.8.14.linux.bin.tar.gz
 
 $SCRIPT_END
