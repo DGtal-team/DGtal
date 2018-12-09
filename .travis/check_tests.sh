80df242c51de32bea36a7113da9e3c6663893089
David Coeurjolly <david.coeurjolly@liris.cnrs.fr>
2018-12-09 15:52:58 +0100

    Source debug

--- .travis/check_tests.sh
+++ .travis/check_tests.sh
@@ -4,7 +4,12 @@ $SCRIPT_BEGIN
 echo "Running the unit tests."
 ### DGtal Tests
 echo cd "$BUILD_DIR/tests"
-cd "$BUILD_DIR/tests"
+cd "$BUILD_DIR/tests" && echo ctest -j $BJOBS --output-on-failure
+ctest -j $BJOBS --output-on-failure
+
+$SCRIPT_END
+
+
 #if [ -f io/writers/testMagickWriter ]; then
 #    io/writers/testMagickWriter -s
 #fi
@@ -12,8 +17,3 @@ cd "$BUILD_DIR/tests"
 #if [ -f io/readers/testMagickReader ]; then
 #    io/readers/testMagickReader
 #fi
-
-echo ctest -j $BJOBS --output-on-failure
-ctest -j $BJOBS --output-on-failure
-
-$SCRIPT_END
