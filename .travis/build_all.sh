e67786a222d0c66e9ad47129a4657cb9545a0efe
David Coeurjolly <david.coeurjolly@liris.cnrs.fr>
2018-12-10 20:41:04 +0100

    Cleaning up (testing true + shell options)

--- .travis/build_all.sh
+++ .travis/build_all.sh
@@ -20,7 +20,7 @@ if [ $TRAVIS_OS_NAME == osx ]; then source "$SRC_DIR/.travis/install_deps_macos.
 #     cmake
 #############################
 #Build directory
-cd "$BUILD_DIR"
+cd $BUILD_DIR
 
 # Common build options
 export BTYPE="$BTYPE -DCMAKE_CXX_COMPILER=$CXXCOMPILER -DCMAKE_C_COMPILER=$CCOMPILER"
