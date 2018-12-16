b5b66291f595d83ec212ae1c3d6e609f3baa6d14
David Coeurjolly <david.coeurjolly@liris.cnrs.fr>
2018-12-16 20:12:33 +0100

    Paths

--- .travis/build_all.sh
+++ .travis/build_all.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 $SCRIPT_BEGIN
 
-#cd "$BUILD_DIR"
 
 export CONFIG="Debug,Magick,GMP,ITK,FFTW3,Debug,Cairo,QGLviewer,HDF5,EIGEN"
 
@@ -21,7 +20,7 @@ if [ $TRAVIS_OS_NAME == osx ]; then source "$SRC_DIR/.travis/install_deps_macos.
 #     cmake
 #############################
 #Build directory
-#cd $BUILD_DIR
+cd build
 # Common build options
 export BTYPE="$BTYPE -DCMAKE_CXX_COMPILER=$CXXCOMPILER -DCMAKE_C_COMPILER=$CCOMPILER"
 
@@ -29,7 +28,7 @@ export BTYPE="$BTYPE -DCMAKE_CXX_COMPILER=$CXXCOMPILER -DCMAKE_C_COMPILER=$CCOMP
 echo "Using C++ = $CXXCOMPILER"
 echo "CMake options = $BTYPE"
 echo "Pwd = $PWD"
-
+echo "SRC_dir=$SRC_DIR"
 cmake "$SRC_DIR" $BTYPE
 
 #############################
