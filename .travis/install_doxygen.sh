0fa2462a80e62cd2cac4b8806d24e07d815cf4ce
David Coeurjolly <david.coeurjolly@liris.cnrs.fr>
2018-12-17 08:56:46 +0100

    using default doxygen

--- .travis/install_doxygen.sh
+++ .travis/install_doxygen.sh
@@ -5,7 +5,7 @@ $SCRIPT_BEGIN
 ##Download and install 1.8.14
 #
 
-mkdir ~/doxygen && cd ~/doxygen
-wget http://doxygen.nl/files/doxygen-1.8.14.linux.bin.tar.gz && tar xzf doxygen-1.8.14.linux.bin.tar.gz
+#mkdir ~/doxygen && cd ~/doxygen
+#wget http://doxygen.nl/files/doxygen-1.8.14.linux.bin.tar.gz && tar xzf doxygen-1.8.14.linux.bin.tar.gz
 
 $SCRIPT_END
