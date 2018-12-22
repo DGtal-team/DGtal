01f5c6f013c75233f421b7acfb9e86075de883e2
David Coeurjolly <david.coeurjolly@liris.cnrs.fr>
2018-12-22 16:32:35 +0100

    disabling doc check

--- .travis/build_doc.sh
+++ .travis/build_doc.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 $SCRIPT_BEGIN
 
+### Downloading tag file for DGtalTools
+wget --no-check-certificate -O "$BUILD_DIR/DGtalTools-tagfile" http://dgtal.org/doc/tags/DGtalTools-tagfile;
+
 ### DGtal Doc
 make doc > buildDoc.log
 $SCRIPT_END
