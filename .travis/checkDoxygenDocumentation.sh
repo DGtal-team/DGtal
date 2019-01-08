d4131d07261ade782f7374cffe7a481a0fe1d927
Roland Denis <roland.denis@math.univ-lyon1.fr>
2019-01-08 10:10:33 +0100

    Adding a doxygen warning filtering rule for a missing argument
    Also adding execution right on some travis scripts.

--- .travis/checkDoxygenDocumentation.sh
+++ .travis/checkDoxygenDocumentation.sh
@@ -9,13 +9,20 @@ DOXYGENLOG=doxygen.log
 if [[ -f "$DOXYGENLOG" ]]
 then
 
-    # Filtering doxygen log (e.g. to ignore some bugs)
-    # See https://github.com/doxygen/doxygen/issues/6352
+    # Filtering doxygen log (e.g. to ignore some bugs):
+    # 1) "warning: unexpected token TK_EOF as the argument of ref"
+    #    at src/DGtal/topology/doc/moduleDigitalTopology.dox:300-316
+    #    See https://github.com/doxygen/doxygen/issues/6352
+    # 2) "warning: argument 'surface' of command @param is not found in the argument list of DGtal::ShortcutsGeometry< TKSpace >::getKSpace(typename TDigitalSurfaceContainer)"
+    #    at src/DGtal/helpers/Shortcuts.h 386 & 397
+    #    Probably a bug (e.g. removing the template parameter solves the warning)
     rm -f /tmp/doxygen.*.log
-    awk '/unexpected token TK_EOF as the argument of ref/ \
-        {print $0 > "/tmp/doxygen.ignored.log"; next} \
-        {print $0 > "/tmp/doxygen.kept.log"}' \
-        "$DOXYGENLOG"
+    awk '\
+            /unexpected token TK_EOF as the argument of ref/ \
+        ||  /argument .surface. of command @param is not found in the argument list/ \
+                {print $0 > "/tmp/doxygen.ignored.log"; next} \
+                {print $0 > "/tmp/doxygen.kept.log"} \
+        ' "$DOXYGENLOG"
 
     if [[ -s "/tmp/doxygen.kept.log" ]]
     then
