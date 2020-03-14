7ead4fb8f47dffaff85bc309d36aea7ad2a84bda
David Coeurjolly <david.coeurjolly@liris.cnrs.fr>
2020-03-14 10:28:16 +0100

    Update src/DGtal/geometry/surfaces/estimation/IIGeometricFunctors.h
    Co-Authored-By: rolanddenis <rolanddenis@users.noreply.github.com>

--- src/DGtal/geometry/surfaces/estimation/IIGeometricFunctors.h
+++ src/DGtal/geometry/surfaces/estimation/IIGeometricFunctors.h
@@ -492,10 +492,10 @@ namespace DGtal {
           ASSERT ( (std::abs(eigenValues[0]) <= std::abs(eigenValues[1]))
                   && (std::abs(eigenValues[1]) <= std::abs(eigenValues[2])) );
 
-          Quantity res =  std::make_tuple (d6_PIr6 * ( eigenValues[2] - ( 3.0 * eigenValues[1] )) + d8_5r,
-                                           d6_PIr6 * ( eigenValues[1] - ( 3.0 * eigenValues[2] )) + d8_5r,
-                                           eigenVectors.column( Space::dimension - 1 ),
-                                           eigenVectors.column( Space::dimension - 2 ));
+          Quantity res(d6_PIr6 * ( eigenValues[2] - ( 3.0 * eigenValues[1] )) + d8_5r,
+                       d6_PIr6 * ( eigenValues[1] - ( 3.0 * eigenValues[2] )) + d8_5r,
+                       eigenVectors.column( Space::dimension - 1 ),
+                       eigenVectors.column( Space::dimension - 2 ));
           return res;
         }
 
