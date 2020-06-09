b82ae29405279f5f2fe79b71e871581855d55a02
Jacques-Olivier Lachaud <jacques-olivier.lachaud@univ-savoie.fr>
2020-06-09 18:01:41 +0200

    Start shrouds

---
+++ src/DGtal/geometry/surfaces/ShroudsRegularization.h
@@ -0,0 +1,472 @@
+/**
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU Lesser General Public License as
+ *  published by the Free Software Foundation, either version 3 of the
+ *  License, or  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ **/
+
+#pragma once
+
+/**
+ * @file ShroudsRegularization.h
+ * @author Jacques-Olivier Lachaud (\c jacques-olivier.lachaud@univ-savoie.fr )
+ * Laboratory of Mathematics (CNRS, UMR 5807), University of Savoie, France
+ *
+ * @date 2020/06/09
+ *
+ * Header file for module ShroudsRegularization.cpp
+ *
+ * This file is part of the DGtal library.
+ */
+
+#if defined(ShroudsRegularization_RECURSES)
+#error Recursive header files inclusion detected in ShroudsRegularization.h
+#else // defined(ShroudsRegularization_RECURSES)
+/** Prevents recursive inclusion of headers. */
+#define ShroudsRegularization_RECURSES
+
+#if !defined ShroudsRegularization_h
+/** Prevents repeated inclusion of headers. */
+#define ShroudsRegularization_h
+
+//////////////////////////////////////////////////////////////////////////////
+// Inclusions
+#include <iostream>
+#include "DGtal/base/Common.h"
+#include "DGtal/base/ConstAlias.h"
+#include "DGtal/topology/IndexedDigitalSurface.h"
+#include "DGtal/topology/DigitalSurface2DSlice.h"
+//////////////////////////////////////////////////////////////////////////////
+
+namespace DGtal {
+  /// Description of template class 'ShroudsRegularization' <p>
+  ///
+  /// \brief Aim: Implements the Shrouds Regularization as described
+  /// in @cite coeurjolly17regDGCI.
+  ///
+  /// Represents a decomposition of a closed digital surface into
+  /// three stacks of slices, one per dimension. Gives methods to
+  /// optimize the positions of vertices and smooth the resulting
+  /// surface, generally according to curvature-related
+  /// regularizers.
+  ///
+  /// @tparam TKSpace any celular grid space (any model of
+  /// concepts::CCellularGridSpaceND), generally some
+  /// KhalimskySpaceND.
+  ///
+  ///
+  /// @see testShroudsRegularization.cpp
+  template <typename TDigitalSurfaceContainer>
+  class ShroudsRegularization
+  {
+  public:
+    typedef TDigitalSurfaceContainer           Container
+    typedef ShroudsRegularization< Container > Self;
+    typedef typename Container::KSpace         KSpace;
+    typedef typename KSpace::Space             Space;
+    typedef typename Space::RealVector         RealVector;
+    typedef typename Space::RealPoint          RealPoint;
+    typedef typename RealVector::Component     Scalar;
+    typedef IndexedDigitalSurface< Container > IdxDigitalSurface;
+    typedef typename IdxDigitalSurface::Vertex IdxVertex;
+    typedef typename IdxDigitalSurface::Surfel IdxSurfel;
+    typedef IdxVertex                          Vertex;
+    typedef std::vector< IdxSurfel >           IdxSurfelRange;
+    typedef std::vector< Scalar >              Scalars;
+    typedef std::vector< RealVector >          RealVectors;
+    typedef std::vector< RealPoint >           RealPoints;
+
+    // ----------------------- Standard services ------------------------------
+  public:
+    /// @name Standard services (construction, initialization)
+    /// @{
+    
+    /// Default constructor. The object is not valid.
+    Shroud()
+      : myPtrIdxSurface( nullptr ), myPtrK( nullptr )
+    {}
+    
+    /// Constructor from (closed) \a surface.
+    /// Also calls methods \ref precomputeTopology and \ref init.
+    /// @param surface a counted pointer on an indexed digital surface.
+    /// @param eps the bounds for varying the positions of vertices in ]0,1[
+    ///
+    /// @note Complexity is linear in the number of surfels of \a surface.
+    Shroud( CountedPtr< IdxDigitalSurface > surface, Scalar eps = 0.00001 )
+      : myPtrIdxSurface( surface ),
+	myPtrK( &surface->container().space() ),
+	myEpsilon( eps )
+    {
+      precomputeTopology();
+      init();
+    }
+
+    /// Prepares the shroud for optimization. Must be called before any
+    /// call to oneStep, oneStepELSnake, oneStepELCurv2.
+    ///
+    /// @note Complexity is linear in the number of surfels of \a surface.
+    void init()
+    {
+      const auto embedder = SH3::getSCellEmbedder( myK );
+      const auto nbV      = myPtrIdxSurface->nbVertices();
+      myT.resize( nbV );
+      myInsV.resize( nbV );
+      myOutV.resize( nbV );
+      for ( Dimension i = 0; i < 3; ++i )
+	{
+	  myPrevD[ i ].resize( nbV );
+	  myNextD[ i ].resize( nbV );
+	}
+      for ( Vertex v = 0; v < myT.size(); ++v )
+	{
+	  const auto s = myPtrIdxSurface->surfel( v );
+	  const auto k = myK.sOrthDir( s );
+	  myInsV[ v ]  = embedder( myK.sDirectIncident( s, k ) );
+	  myOutV[ v ]  = embedder( myK.sIndirectIncident( s, k ) );
+	}
+    }
+    /// @}
+    
+    // ----------------------- Accessor services ------------------------------
+  public:
+    /// @name Accessor services
+    /// @{
+    
+    /// @param v any valid vertex.
+    /// @param t some adjustement parameter
+    /// @return the position of vertex v for this parameter t.
+    RealPoint position( Vertex v, double t ) const
+    {
+      return (1-t) * myInsV[ v ] + t * myOutV[ v ];
+    }
+    
+    /// @param v any valid vertex.
+    /// @return its position.
+    RealPoint position( Vertex v ) const
+    {
+      const auto t = myT[ v ];
+      return (1-t) * myInsV[ v ] + t * myOutV[ v ];
+    }
+    
+    /// @return the vector of vertex positions
+    RealPoints positions() const
+    {
+      RealPoints result( myT.size() );
+      for ( Vertex v = 0; v < myT.size(); ++v )
+	result[ v ] = position( v );
+      return result;
+    }
+
+    /// Useful to find the two tangent directions to a given vertex
+    /// (i.e. `(orthDir(v)+1)%3` and `(orthDir(v)+2)%3`).
+    ///
+    /// @return the orthogonal direction to vertex v.
+    Dimension orthDir( Vertex v ) const
+    {
+      return myOrthDir[ v ];
+    }
+    
+    /// Useful to navigate tangentially along a slice.
+    /// @param v_i a pair (vertex,tangent direction)
+    /// @return the next vertex and associated tangent direction along the slice. 
+    std::pair<Vertex,Dimension> next( std::pair<Vertex,Dimension> v_i ) const
+    {
+      const Vertex    vn = myNext[ v_i.second ][ v_i.first ];
+      const Dimension in = myOrthDir[ vn ] == v_i.second
+	? myOrthDir[ v_i.first ] : v_i.second;
+      return std::make_pair( vn, in );
+    }
+
+    /// Useful to navigate tangentially along a slice.
+    /// @param v_i a pair (vertex,tangent direction)
+    /// @return the previous vertex and associated tangent direction along the slice. 
+    std::pair<Vertex,Dimension> prev( std::pair<Vertex,Dimension> v_i ) const
+    {
+      const Vertex    vp = myPrev[ v_i.second ][ v_i.first ];
+      const Dimension ip = myOrthDir[ vp ] == v_i.second
+	? myOrthDir[ v_i.first ] : v_i.second;
+      return std::make_pair( vp, ip );
+    }
+    
+    /// @}
+
+    // ----------------------- Geometric services ------------------------------
+  public:
+    /// @name Geometric services
+    /// @{
+
+    /// Computes the distances between the vertices along slices.
+    void parameterize()
+    {
+      Scalar d = 0.0;
+      // Vertex n = 0;
+      for ( Dimension i = 0; i < 3; ++i )
+	for ( Vertex v = 0; v < myT.size(); ++v )
+	  {
+	    if ( myNext[ i ][ v ] == myInvalid )  continue; // not a valid slice
+	    myNextD[ i ][ v ] = ( position( myNext[ i ][ v ] ) - position( v ) ).norm();
+	    myPrevD[ i ][ v ] = ( position( myPrev[ i ][ v ] ) - position( v ) ).norm();
+	    d += myNextD[ i ][ v ];
+	    //	    n += 1;
+	  }
+    }
+
+    /// @param v_i a pair (vertex,tangent direction)
+    /// @return the coefficient for centered first-order finite difference.
+    /// @note We have y'_i ~= c1 * (y_{i+1} - y_{i-1} )
+    Scalar c1( std::pair<Vertex,Dimension> v_i ) const
+    {
+      const Scalar din = myNextD[ v_i.second ][ v_i.first ];
+      const Scalar dip = myPrevD[ v_i.second ][ v_i.first ];
+      return 1.0 / (din + dip);
+    }
+    
+    /// @param v_i a pair (vertex,tangent direction)
+    /// @return the coefficients for centered second-order finite difference.
+    /// @note We have y''_i ~= c2<0> * y_{i+1} - c2<1> * y_{i} + c2<2> * y_{i-1}
+    std::tuple<Scalar,Scalar,Scalar> c2_all( std::pair<Vertex,Dimension> v_i ) const
+    {
+      const Scalar din = myNextD[ v_i.second ][ v_i.first ];
+      const Scalar dip = myPrevD[ v_i.second ][ v_i.first ];
+      return std::make_tuple( 2.0 / ( din * ( din + dip ) ),
+			      2.0 / ( din * dip ),
+			      2.0 / ( dip * ( din + dip ) ) );
+    }
+    
+    /// @}
+
+    // -------------------------- regularization services ----------------------------
+  public:
+    /// @name Regularization services
+    /// @{
+
+    /// Smooths the shape according to the minimization of area.
+    ///
+    /// @param randomization if greater than 0.0 add some perturbation to
+    /// the solution. May be used for quitting a local minima.
+    ///
+    /// @return the pair of \f$ l_\infty \f$ and \f$ l_2 \f$ norms of
+    /// vertex displacements.
+    ///
+    /// @note Not very nice.
+    std::pair<double,double> oneStepAreaMinimization( double randomization = 0.0 );
+
+    /// Smooths the shape according to the minimization of elastic + thin plate
+    /// energy (like snakes).
+    ///
+    /// \f[ E(C) = \int_C alpha * (x'(s)^2 + y'(s)^2) + beta *
+    /// (x''(s)^2 + y''(s)^2) ds, \f]
+    ///
+    /// for \f$ C=(x(s),y(s)) \f$ and boundary constraints.
+    ///
+    /// @param alpha parameter for first order regularization (~ area)
+    /// @param beta  parameter for second order regularization (~ curvature)
+    /// @param randomization if greater than 0.0 add some perturbation to
+    /// the solution. May be used for quitting a local minima.
+    ///
+    /// @return the pair of \f$ l_\infty \f$ and \f$ l_2 \f$ norms of
+    /// vertex displacements.
+    ///
+    /// Euler-Lagrange equations leads to necessary conditions:
+    /// for all s, \f$ x''''(s) = 0 \f$ and \f$ y''''(s) = 0 \f$.
+    ///
+    /// A vertex \f$ v(s) \f$ has a position \f$ X \f$. Next and previous vertices are
+    /// denoted by \f$ Xn, Xnn, Xp, Xpp \f$. If i the tangent direction and k the
+    /// orthogonal direction at \f$ X \f$, then \f$ x(s)=X[i], y(s)=X[k] \f$.
+    ///
+    /// At a vertex \f$ v(s) \f$, the position is parameterized by \a t. Varying
+    /// \a t only modify the vertical position \f$ X[k] \f$, i.e. \a y.
+    /// So \f$ y(t) = (1-t)*I[v][k] + t*O[v][k] \f$
+    /// (for \f$ I[v] \f$ and \f$ O[v] \f$ inside and outside voxel positions).
+    ///
+    /// \f$ x' ~= ( Xn[i] - Xp[i] ) / (d(X,Xn)+d(X,Xp) \f$ does not depend on t.
+    /// \f$ y' ~= ( Xn[k] - Xp[k] ) / (d(X,Xn)+d(X,Xp) \f$ does not depend on t.
+    /// \f$ x'' ~= ( c_0 * Xn[i] - c_1 * X[i] + c_2 * Xp[i] ) \f$ does not depend on t.
+    ///
+    /// for \f$ c_0, c_1, c_2 \f$ constants depending on \f$ d(X,Xn)
+    /// \f$ and \f$ d(X,Xp) \f$.  \f$ y'' ~= ( c_0 * Xn[k] - c_1 *
+    /// X[k] + c_2 * Xp[k] ) \f$ solely \b depend on t.
+    std::pair<double,double> oneStepSnakeMinimization
+    ( double alpha = 1.0, double beta = 1.0, double randomization = 0.0 );
+
+    /// Smooths the shape according to the minimization of squared curvature.
+    ///
+    /// \f[
+    /// E(C) = int_C (x'(s)*y''(s) + x''(s)*y'(s))^2 / (x'(s)^2 + y'(s)^2)^3 ds,
+    /// \f]
+    ///
+    /// for \f$ C=(x(s),y(s)) \f$ and boundary constraints.
+    ///
+    /// @param randomization if greater than 0.0 add some perturbation to
+    /// the solution. May be used for quitting a local minima.
+    ///
+    /// @return the pair of \f$ l_\infty \f$ and \f$ l_2 \f$ norms of
+    /// vertex displacements.
+    ///
+    /// Euler-Lagrange equations leads to rather complex necessary
+    /// conditions.  At each position (we omit parameter \a s for making
+    /// things more readable):
+    ///
+    /// \f[ 24*x'^3*x''^3*y' 
+    /// + x'''*( - 13*x'^4*x''*y' - 14*x'^2*x''*y'^3 - x''*y'^5 )
+    /// + y'''*( 8*x'^5*x'' + 4*x'^3*x''*y'^2 - 4*x'*x''*y'^4 )
+    /// + x''''*( x'^5*y' + 2*x'^3*y'^3 + x'*y'^5 )
+    /// + y''*( 12*x'^4*y'*y''' + 12*x'^2*y'^3*y''' - 24*x'^4*x''^2 + 5*x'^5*x''' + 51*x'^2*x''^2*y'^2 - 2*x'^3*x'''*y'^2 + 3*x''^2*y'^4 - 7*x'*x'''*y'^4 )
+    /// + y''^2*( -54*x'^3*x''*y' + 18*x'*x''*y'^3 )
+    /// + y''^3*( 3*x'^4 - 21*x'^2*y'^2 )
+    /// + y''''*( - x'^6 - 2*x'^4*y'^2 - x'^2*y'^4 )
+    /// == 0 \f]
+    ///
+    /// A vertex \f$ v(s) \f$ has a position \f$ X \f$. Next and previous vertices are
+    /// denoted by \f$ Xn, Xnn, Xp, Xpp \f$. If \a i the tangent direction and \a k the
+    /// orthogonal direction at \f$ X \f$, then \f$ x(s)=X[i], y(s)=X[k] \f$.
+    ///
+    /// At a vertex \f$ v(s) \f$, the position is parameterized by \a t. Varying
+    /// \a t only modify the vertical position \f$ X[k] \f$, i.e. \a y.
+    /// So \f$ y(t) = (1-t)*I[v][k] + t*O[v][k] \f$
+    /// (for \f$ I[v] \f$ and \f$ O[v] \f$ inside and outside voxel positions).
+    ///
+    /// The Euler-Lagrange are factorized as above in order to isolate
+    /// at best the parameter \a t of vertex \f$ v(s) \f$.
+    ///
+    /// - The first four lines do not depend on \a t (except \f$ y''' \f$ but it is
+    ///  neglected and the old value is used).
+    /// - \f$ y''^2 \f$ and \f$ y''^3 \f$ are linarized as \f$ y'' * y''_{old} \f$ and \f$ y'' * y''_{old}^2 \f$
+    /// - \f$ y'''' ~= ( c_0 * Yn - c_1 * y'' + c_2 * Yp ) \f$
+    /// - all \f$ y'' \f$ are sumed up, while the rest gives a constant.
+    ///
+    /// \f$ x' ~= ( Xn[i] - Xp[i] ) / (d(X,Xn)+d(X,Xp) \f$ does not depend on \a t.
+    /// \f$ y' ~= ( Xn[k] - Xp[k] ) / (d(X,Xn)+d(X,Xp) \f$ does not depend on \a t.
+    /// \f$ x'' ~= ( c_0 * Xn[i] - c_1 * X[i] + c_2 * Xp[i] ) \f$ does not depend on \a t.
+    ///
+    /// for \f$ c_0, c_1, c_2 \f$ constants depending on \f$ d(X,Xn) \f$ and \f$ d(X,Xp) \f$.
+    /// \f$ y'' ~= ( c_0 * Xn[k] - c_1 * X[k] + c_2 * Xp[k] ) \f$ solely \b depend on t.
+    /// where \f$ X[k] = y(t) = (1-t)*I[v][k] + t*O[v][k]. \f$
+    /// And so on.
+    std::pair<double,double> oneStepSquaredCurvatureMinimization
+    ( double randomization = 0.0 );
+
+    /// Forces t to stay in ]0,1[
+    void enforceBounds();
+
+    /// @}
+    
+    // -------------------------- internal methods ------------------------------
+  protected:
+    /// @name Internal methods
+    /// @{
+    
+    /// This method precomputes the neighbors of each vertex along each
+    /// crossing curves. Must be called at shroud initialization.
+    void precomputeTopology()
+    {
+      typedef typename Container::Tracker      Tracker;
+      typedef DigitalSurface2DSlice< Tracker > Slice;
+      myT       = Scalars( myPtrIdxSurface->nbVertices(), 0.5 );
+      myInvalid = myT.size();
+      myOrthDir.resize( myT.size() );
+      for ( Dimension i = 0; i < 3; ++i )
+      {
+	myNext[ i ] = std::vector<Vertex>( myT.size(), myInvalid );
+	myPrev[ i ] = std::vector<Vertex>( myT.size(), myInvalid );
+      }
+      // for each vertex, extracts its two slices.
+      for ( Vertex v = 0; v < myT.size(); ++v )
+	{
+	  auto surf      = myPtrIdxSurface->surfel( v );
+	  Dimension k    = myPtrK->sOrthDir( surf );
+	  myOrthDir[ v ] = k;
+	  for ( Dimension i = 0; i < 3; ++i )
+	    {
+	      if ( k == i )   continue; // not a valid slice
+	      if ( myNext[ i ][ v ] != myInvalid ) continue; // already computed
+	      Tracker* tracker = myPtrIdxSurface->container().newTracker( surf );
+	      Slice    slice( tracker, i );
+	      if ( ! slice.isClosed() ) {
+		trace.error() << "Shrouds works solely on closed surfaces." << endl;
+		return;
+	      }
+	      auto start = slice.cstart();
+	      auto  next = start;
+	      auto  prev = next++;
+	      Vertex  vp = v;
+	      do
+		{
+		  auto sp = *prev;
+		  auto sn = *next;
+		  Dimension in = myPtrK->sOrthDir( sn ) == k ? i : k;
+		  Dimension ip = myPtrK->sOrthDir( sp ) == k ? i : k;
+		  Vertex vn = myPtrIdxSurface->getVertex( sn );
+		  myNext[ ip ][ vp ] = vn;
+		  myPrev[ in ][ vn ] = vp;
+		  prev = next++;
+		  vp   = vn;
+		}
+	      while ( prev != start );	    
+	      delete tracker;
+	    }
+	}
+    }
+
+    /// @}
+    
+    // -------------------------- data ---------------------------------
+  private:
+
+    /// the indexed digital surface (internal surface representation).
+    CountedPtr<IdxDigitalSurface>      myPtrIdxSurface;
+    /// A const pointer to the cellular space in which lives the digital surface.
+    const KSpace*                      myPtrK;
+    /// The limiting bounds for the displacement of vertices along
+    /// their unit dual edge: \f$ \lbrack \epsilon, 1-\epsilon \rbrack \f$
+    const Scalar                       myEpsilon;
+    /// the index of the invalid vertex.
+    Vertex                             myInvalid;
+    /// the vector of vertex displacements along their dual edge (each
+    /// value lies in \f$ \lbrack \epsilon, 1-\epsilon \rbrack \f$),
+    /// which is optimized by the shrouds algorithm.
+    Scalars                            myT;
+    /// the vector of dual points lying inside (inside extremity of
+    /// each dual edge).
+    RealPoints                         myInsV;
+    /// the vector of dual points lying outside (outside extremity of
+    /// each dual edge).
+    RealPoints                         myOutV;
+    /// the direction axis of each dual edge.
+    std::vector<Dimension>             myOrthDir;
+    /// for each vertex, its successor on the slice of given axis direction.
+    std::vector<Vertex>                myNext[ 3 ];
+    /// for each vertex, its predessor on the slice of given axis direction.
+    std::vector<Vertex>                myPrev[ 3 ];
+    /// for each vertex, the estimated distance to its successor on
+    /// the slice of given axis direction.
+    Scalars                            myNextD[ 3 ];
+    /// for each vertex, the estimated distance to its predessor on
+    /// the slice of given axis direction.
+    Scalars                            myPrevD[ 3 ];
+    
+  }; // end of class ShroudsRegularization
+  
+  
+} // namespace surfaces
+
+
+///////////////////////////////////////////////////////////////////////////////
+// Includes inline functions/methods if necessary.
+#include "DGtal/geometry/surfaces/ShroudsRegularization.ih"
+//                                                                           //
+///////////////////////////////////////////////////////////////////////////////
+
+#endif // !defined ShroudsRegularization_h
+
+#undef ShroudsRegularization_RECURSES
+#endif // else defined(ShroudsRegularization_RECURSES)
