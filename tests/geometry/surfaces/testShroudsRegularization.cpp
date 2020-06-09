3882213c907b2ae7277636868b226f8ccde67fcb
Jacques-Olivier Lachaud <jacques-olivier.lachaud@univ-savoie.fr>
2020-06-09 22:58:03 +0200

    Working version of shrouds

--- tests/geometry/surfaces/testShroudsRegularization.cpp
+++ tests/geometry/surfaces/testShroudsRegularization.cpp
@@ -50,7 +50,7 @@ TEST_CASE( "Testing ShroudsRegularization" )
   typedef SH3::ExplicitSurfaceContainer  Container;
   auto params = SH3::defaultParameters();
   
-  params( "polynomial", "goursat" )( "gridstep", 1.0)("verbose", 0);
+  params( "polynomial", "goursat" )( "gridstep", 0.3)("verbose", 0);
   auto implicit_shape  = SH3::makeImplicitShape3D  ( params );
   auto digitized_shape = SH3::makeDigitizedImplicitShape3D( implicit_shape, params );
   auto K               = SH3::getKSpace( params );
@@ -60,37 +60,35 @@ TEST_CASE( "Testing ShroudsRegularization" )
   auto surface         = SH3::makeDigitalSurface( digitized_shape, K, params );
   auto idxsurface      = SH3::makeIdxDigitalSurface( surface, params );
   auto surfels         = SH3::getIdxSurfelRange( idxsurface, params );
-  ShroudsRegularization< Container > shrouds_reg( surface );
+  ShroudsRegularization< Container > shrouds_reg( idxsurface );
   auto originalPos     = shrouds_reg.positions();
   //! [ShroudsRegUsage]
 
-  const int maxNb = 100;
   double loo      = 0.0;
   double  l2      = 0.0;
-  int     nb      = 0;
-  double   r      = 0.5;
-  do {
-    std::tie( loo, l2 ) = shroud.oneStepELCurv2( r );
-    trace.info() << "[Iteration " << nb << "] dx <= " << loo << " l2=" << l2 << endl;
-    r  *= 0.9;
-    n  += 1;
-  } while ( loo > 0.0001 && nb < maxNb );
+  typedef ShroudsRegularization< Container >::Regularization RegType;
+  std::tie( loo, l2 ) = shrouds_reg.regularize( RegType::SQUARED_CURVATURE,
+						0.5, 0.0001, 1000 );
+  // const int maxNb = 100;
+  // int     nb      = 0;
+  // double   r      = 0.5;
+  // do {
+  //   std::tie( loo, l2 ) = shrouds_reg.oneStepSquaredCurvatureMinimization( r );
+  //   trace.info() << "[Iteration " << nb << "] dx <= " << loo << " l2=" << l2 << endl;
+  //   r  *= 0.9;
+  //   nb += 1;
+  // } while ( loo > 0.0001 && nb < maxNb );
 
   //! [ShroudsRegOutput]
-  auto regularizedPos = shrouds_regul.positions();
+  auto regularizedPos = shrouds_reg.positions();
   //! [ShroudsRegOutput]
 
   //! [ShroudsRegSaveObj]
-  SH3::Surfel2Index s2i;
-  auto polySurf = SH3::makeDualPolygonalSurface( s2i, surface );
-  auto X        = polySurf->positions();
-  for ( int v = 0; v < shroud.myT.size(); v++ )
-    {
-      auto surfel = shroud.myPtrIdxSurface->surfel( v );
-      auto v2     = s2i[ surfel ];
-      X[ v2 ] = shroud.position( v );
-    }
-  SH3::saveOBJ( polySurf, output );
+  auto polySurf = SH3::makeDualPolygonalSurface( idxsurface );
+  auto polySurfPos = polySurf->positions();
+  for ( auto i = 0; i < regularizedPos.size(); i++ )
+    polySurfPos[ i ] = regularizedPos[ i ];
+  SH3::saveOBJ( polySurf, "goursat-shrouds-reg-k2.obj" );
   //! [ShroudsRegSaveObj]
   REQUIRE( loo < 0.1 );
 }
