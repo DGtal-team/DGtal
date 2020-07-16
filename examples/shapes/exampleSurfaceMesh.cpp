4ab80fa989105ad2e2806d4ca85f0ea245a168c3
Jacques-Olivier Lachaud <jacques-olivier.lachaud@univ-savoie.fr>
2020-07-16 17:37:44 +0200

    Finishing user doc

--- examples/shapes/exampleSurfaceMesh.cpp
+++ examples/shapes/exampleSurfaceMesh.cpp
@@ -53,9 +53,12 @@
 #include "DGtal/shapes/SurfaceMesh.h"
 #include "DGtal/shapes/SurfaceMeshHelper.h"
 #include "DGtal/shapes/MeshHelpers.h"
+#include "DGtal/graph/BreadthFirstVisitor.h"
 #include "DGtal/io/readers/SurfaceMeshReader.h"
+#include "DGtal/io/writers/SurfaceMeshWriter.h"
 #include "DGtal/io/viewers/Viewer3D.h"
 #include "DGtal/io/Color.h"
+#include "DGtal/io/colormaps/GradientColorMap.h"
 
 ///////////////////////////////////////////////////////////////////////////////
 
@@ -69,7 +72,8 @@ int main( int argc, char** argv )
 {
   trace.beginBlock ( "Reading a mesh OBJ file" );
   //! [exampleSurfaceMesh-read-mesh]
-  SurfaceMesh< RealPoint, RealVector > smesh;
+  typedef SurfaceMesh< RealPoint, RealVector > SurfMesh;
+  SurfMesh    smesh;
   std::string S = examplesPath + "samples/spot.obj";
   std::ifstream input( S.c_str() );
   bool ok_read  =  SurfaceMeshReader< RealPoint, RealVector >::readOBJ( input, smesh );
@@ -89,28 +93,62 @@ int main( int argc, char** argv )
 
   trace.beginBlock ( "Building a pyramid" );
   //! [exampleSurfaceMesh-make-pyramid]
-  typedef SurfaceMesh< RealPoint, RealVector > MySurfaceMesh;
-  typedef MySurfaceMesh::Vertices              Vertices;
+  typedef SurfMesh::Vertices Vertices;
   std::vector< RealPoint > positions =
     { { 0, 0, 5 }, { 1, 1, 3 }, { -1, 1, 3 }, { -1, -1, 3 }, { 1, -1, 3 } };
   std::vector< Vertices  > faces =
     { { 0, 1, 2 }, { 0, 2, 3 }, { 0, 3, 4 }, { 0, 4, 1 }, { 4, 3, 2, 1 } };
-  auto pyramid_mesh = MySurfaceMesh( positions.cbegin(), positions.cend(),
-                                     faces.cbegin(), faces.cend() );
+  auto pyramid_mesh = SurfMesh( positions.cbegin(), positions.cend(),
+                                faces.cbegin(), faces.cend() );
   //! [exampleSurfaceMesh-make-pyramid]
   trace.endBlock();
-  
+
+  trace.beginBlock ( "Traversing a mesh by BF" );
+  //! [exampleSurfaceMesh-graph-bft]
+  BreadthFirstVisitor< SurfMesh > visitor( smesh, 0 );
+  std::vector<double>        distances( smesh.nbVertices() );
+  while ( ! visitor.finished() )
+    {
+      auto v = visitor.current().first;  // current vertex
+      auto d = visitor.current().second; // current distance
+      distances[ v ] = (double) d;
+      visitor.expand();
+    }
+  //! [exampleSurfaceMesh-graph-bft]
+  trace.endBlock();
+
+  trace.beginBlock ( "Colored output of BF traversal" );
+  //! [exampleSurfaceMesh-write-obj]
+  // Displaying faces colored by their distance to vertex 0.
+  auto cmap = GradientColorMap< double >( 0.0, distances.back(), CMAP_JET );
+  std::vector<Color> face_colors( smesh.nbFaces() );
+  for ( SurfMesh::Face j = 0; j < smesh.nbFaces(); ++j )
+    {
+      auto ivtcs = smesh.incidentVertices( j );
+      SurfMesh::Vertex best = ivtcs[ 0 ];
+      for ( auto&& v : ivtcs )
+        if ( distances[ v ] < distances[ best ] ) best = v;
+      face_colors[ j ] = cmap( distances[ best ] );      
+    }
+  typedef SurfaceMeshWriter< RealPoint, RealVector > Writer;
+  Writer::writeOBJ( "spot-bft.obj", smesh, face_colors );
+
+  // Displaying three isolines.
+  auto face_distances = smesh.computeFaceValuesFromVertexValues( distances );
+  Writer::writeIsoLinesOBJ( "spot-iso-0_25.obj", smesh,
+                            face_distances, distances, distances.back() * 0.25, 0.2 );
+  Writer::writeIsoLinesOBJ( "spot-iso-0_5.obj",  smesh,
+                            face_distances, distances, distances.back() * 0.5,  0.2 );
+  Writer::writeIsoLinesOBJ( "spot-iso-0_75.obj", smesh,
+                            face_distances, distances, distances.back() * 0.75, 0.2 );
+  //! [exampleSurfaceMesh-write-obj]
+  trace.endBlock();
+
+  // Conversion to Mesh for easy display.
   Mesh< RealPoint > viewmesh, viewmesh2, viewmesh3;
   MeshHelpers::surfaceMesh2Mesh( smesh,      viewmesh  );
   MeshHelpers::surfaceMesh2Mesh( torus_mesh, viewmesh2 );
   MeshHelpers::surfaceMesh2Mesh( pyramid_mesh, viewmesh3 );
-  // for ( auto&& v : smesh.positions() )
-  //   viewmesh.addVertex( v );
-  // for ( auto&& f : smesh.allIncidentVertices() )
-  //   {
-  //     Mesh< RealPoint >::MeshFace face( f.cbegin(), f.cend() );
-  //     viewmesh.addFace( face, Color( 200, 200, 255, 255 ) );
-  //   }
 
   QApplication application(argc,argv);
   Viewer3D<> viewer;
