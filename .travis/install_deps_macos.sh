328faaddc287dce9ef2e7dadd748eeef5f29df47
David Coeurjolly <david.coeurjolly@liris.cnrs.fr>
2020-09-30 09:44:37 +0200

    Disabling Viewer in mac os travis bot

--- .travis/install_deps_macos.sh
+++ .travis/install_deps_macos.sh
@@ -8,14 +8,14 @@ set +e # The brew install has errors
 #
 brew update > brew.log
 #brew install qt5 doxygen homebrew/science/hdf5 graphviz graphicsmagick fftw eigen
-brew install qt5 graphicsmagick fftw eigen ninja
+brew install graphicsmagick fftw eigen ninja
 # Explicit install of libqglviewer
-brew tap DGtal-team/homebrew-dgtal
-brew install libqglviewer
+#brew tap DGtal-team/homebrew-dgtal
+#brew install libqglviewer
 
 ## Temporary HDF5 build issue
 BTYPE="$BTYPE -DWITH_HDF5=false" && echo "Disabling HDF5 on MacOS";
-BTYPE="$BTYPE -DWITH_QT5=true -DCMAKE_PREFIX_PATH=$(brew --prefix qt5)" && echo "Forcing Qt5 on MacOS";
+#BTYPE="$BTYPE -DWITH_QT5=true -DCMAKE_PREFIX_PATH=$(brew --prefix qt5)" && echo "Forcing Qt5 on MacOS";
 
 # Restoring set options
 set -$set_options
