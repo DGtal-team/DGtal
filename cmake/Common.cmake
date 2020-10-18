dc620a8d47e19db917e6bab7fdb10b35bd273bbb
Pablo Hernandez-Cerdan <pablo.hernandez.cerdan@outlook.com>
2020-10-18 16:14:45 +0200

    Move debug and verbose options from Common.cmake to DevelopmentOptions.cmake
    To avoid using add_definitions, we need DGtal target to exists.
    So these options are moved to a CMake file included after DGtal target
    is created.

--- cmake/Common.cmake
+++ cmake/Common.cmake
@@ -103,36 +103,6 @@ option(DGTAL_NO_ESCAPED_CHAR_IN_TRACE "Avoid printing special color and font wei
 option(DGTAL_CONFIG_HINTS "Provide HINTS to find_dependency in DGtalConfig.cmake. Projects consuming DGtal does not have to provide FOO_DIR to their project, where FOO is a DGtal dependency. Recommended to turn it off when deploying." ON)
 mark_as_advanced(DGTAL_CONFIG_HINTS)
 
-# Enable floating point exception when DGtal library is loaded. Only works for gcc.
-if (UNIX AND NOT APPLE)
-  # Only used in Common.cpp
-  option(DGTAL_ENABLE_FLOATING_POINT_EXCEPTIONS "Enable feenableexcept when DGtal library is loaded." OFF)
-  mark_as_advanced(DGTAL_ENABLE_FLOATING_POINT_EXCEPTIONS)
-  if(DGTAL_ENABLE_FLOATING_POINT_EXCEPTIONS)
-    target_compile_definitions(DGtal PRIVATE -DDGTAL_ENABLE_FLOATING_POINT_EXCEPTIONS)
-  endif()
-endif()
-
-set(VERBOSE_DGTAL 0)
-set(DEBUG_VERBOSE_DGTAL 0)
-set(COLOR_WITH_ALPHA_ARITH_DGTAL 0)
-
-if (DEBUG_VERBOSE)
-  set(DEBUG_VERBOSE_DGTAL 1)
-  target_compile_definitions(DGtal PUBLIC -DDEBUG_VERBOSE)
-  message(STATUS "Debug verbose mode activated")
-endif()
-if (VERBOSE)
-  set(VERBOSE_DGTAL 1)
-  target_compile_definitions(DGtal PUBLIC -DVERBOSE)
-  message(STATUS "Verbose mode activated")
-endif()
-
-if(COLOR_WITH_ALPHA_ARITH)
-  set(COLOR_WITH_ALPHA_ARITH_DGTAL 1)
-  target_compile_definitions(DGtal PUBLIC -DCOLOR_WITH_ALPHA_ARITH)
-endif()
-
 # -----------------------------------------------------------------------------
 # Benchmark target
 # -----------------------------------------------------------------------------
@@ -161,12 +131,3 @@ if(PROJECT_BINARY_DIR STREQUAL ${PROJECT_SOURCE_DIR})
 d build outside the sources (for example 'mkdir build ; cmake <DGTAL_DIR>'.")
 endif()
 
-
-# -----------------------------------------------------------------------------
-# Debug specific options
-# -----------------------------------------------------------------------------
-option(WARNING_AS_ERROR "Transform compiler warnings as errors (in Debug build type)." OFF)
-if (WARNING_AS_ERROR)
-  set(CMAKE_CXX_FLAGS_DEBUG "${CMAKE_CXX_FLAGS_DEBUG} -Werror")
-  message(STATUS "Warnings as Errors ENABLED.")
-endif()
