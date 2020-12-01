3836f5dbc51102d59112209708e010907f8bb8f1
David Coeurjolly <david.coeurjolly@liris.cnrs.fr>
2020-12-01 20:05:10 +0100

    lower case in cmake fetch_content

--- cmake/FetchExternalDeps.cmake
+++ cmake/FetchExternalDeps.cmake
@@ -1,17 +1,16 @@
 Include(FetchContent)
 
-# https://cliutils.gitlab.io/modern-cmake/chapters/projects/fetch.html
-# special macro for cmake >= 3.11 but < 3.14+
+## https://cliutils.gitlab.io/modern-cmake/chapters/projects/fetch.html
 if(${CMAKE_VERSION} VERSION_LESS 3.14)
-    macro(FetchContent_MakeAvailable NAME)
+  macro(FetchContent_MakeAvailable NAME)
+        string(TOLOWER ${NAME} lcNAME)
         FetchContent_GetProperties(${NAME})
-        if(NOT ${NAME}_POPULATED)
+        if(NOT ${lcNAME}_POPULATED)
             FetchContent_Populate(${NAME})
-            add_subdirectory(${${NAME}_SOURCE_DIR} ${${NAME}_BINARY_DIR})
+            add_subdirectory(${${lcNAME}_SOURCE_DIR} ${${lcNAME}_BINARY_DIR})
         endif()
     endmacro()
 endif()
-
 # -----------------------------------------------------------------------------
 # Fetching Catch2 (only if the BUILD_TESTING variable has been set to true)
 # -----------------------------------------------------------------------------
@@ -19,11 +18,12 @@ if (BUILD_TESTING)
   FetchContent_Declare(
     Catch2
     GIT_REPOSITORY https://github.com/catchorg/Catch2.git
-    GIT_TAG        v2.13.3)
+    GIT_TAG        v2.13.3
+    )
 
   message(STATUS "    Catch2 (v2.13.3)")
   FetchContent_MakeAvailable(Catch2)
-
+  
   list(APPEND CMAKE_MODULE_PATH ${catch2_SOURCE_DIR}/contrib)
   include(CTest)
   include(Catch)
