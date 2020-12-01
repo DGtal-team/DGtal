da59a048adae991cb24bcacc6594d4260171611b
David Coeurjolly <david.coeurjolly@liris.cnrs.fr>
2020-12-01 13:54:26 +0100

    Adding Catch2 as external deps using Fetch_Content (missing file)

--- cmake/FetchExternalDeps.cmake
+++ cmake/FetchExternalDeps.cmake
@@ -5,7 +5,7 @@ FetchContent_Declare(
   GIT_REPOSITORY https://github.com/catchorg/Catch2.git
   GIT_TAG        v2.13.3)
 
-message(STATUS "    Catch2...")
+message(STATUS "    Catch2 (v2.13.3)")
 FetchContent_MakeAvailable(Catch2)
 
 list(APPEND CMAKE_MODULE_PATH ${catch2_SOURCE_DIR}/contrib)
