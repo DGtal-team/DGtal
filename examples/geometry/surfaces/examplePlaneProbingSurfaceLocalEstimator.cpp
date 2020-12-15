02cc1dbd939f3ec08873bfa5262db69568053ddc
Jocelyn MEYRON <Jocelyn.Meyron@insa-lyon.fr>
2020-12-15 15:12:53 +0100

    Add PlaneProbingDigitalSurfaceLocalEstimator class

---
+++ examples/geometry/surfaces/examplePlaneProbingSurfaceLocalEstimator.cpp
@@ -0,0 +1,149 @@
+/**
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU Lesser General Public License as
+ *  published by the Free Software Foundation, either version 3 of the
+ *  License, or  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ **/
+
+/**
+ * @file
+ * @ingroup Examples
+ * @author Jocelyn Meyron (\c jocelyn.meyron@liris.cnrs.fr )
+ * Laboratoire d'InfoRmatique en Image et Systemes d'information - LIRIS (CNRS, UMR 5205), CNRS, France
+ *
+ * @date 2020/12/07
+ *
+ * An example file named examplePlaneProbingSurfaceLocalEstimator.
+ *
+ * This file is part of the DGtal library.
+ */
+
+///////////////////////////////////////////////////////////////////////////////
+#include <iostream>
+#include "ConfigExamples.h"
+#include "DGtal/helpers/StdDefs.h"
+#include "DGtal/base/Common.h"
+#include "DGtal/geometry/surfaces/estimation/PlaneProbingTetrahedronEstimator.h"
+#include "DGtal/geometry/surfaces/estimation/PlaneProbingParallelepipedEstimator.h"
+#include "DGtal/geometry/surfaces/DigitalSurfacePredicate.h"
+#include "DGtal/geometry/surfaces/estimation/PlaneProbingDigitalSurfaceLocalEstimator.h"
+#include "DGtal/helpers/Shortcuts.h"
+#include "DGtal/helpers/ShortcutsGeometry.h"
+#include "DGtal/io/viewers/Viewer3D.h"
+///////////////////////////////////////////////////////////////////////////////
+
+using namespace std;
+using namespace DGtal;
+
+using KSpace           = Z3i::KSpace;
+using SH3              = Shortcuts<KSpace>;
+using SHG3             = ShortcutsGeometry<KSpace>;
+using Surface          = SH3::DigitalSurface;
+using Surfel           = SH3::Surfel;
+using SurfacePredicate = DigitalSurfacePredicate<Surface>;
+using ProbingAlgorithm = PlaneProbingTetrahedronEstimatorParallelepiped<SurfacePredicate, ProbingMode::R1>;
+// using ProbingAlgorithm = PlaneProbingParallelepipedEstimator<SurfacePredicate, ProbingMode::H>;
+using Estimator        = PlaneProbingDigitalSurfaceLocalEstimator<Surface, ProbingAlgorithm>;
+using ProbingFactory   = Estimator::ProbingFactory;
+using Quantity         = Estimator::Quantity;
+using Point            = SH3::Point;
+using RealPoint        = SH3::RealPoint;
+using Integer          = Point::Coordinate;
+
+RealPoint centerSurfel (KSpace const& K, SH3::SCell const& s)
+{
+    auto pointels = SH3::getPrimalVertices(K, s, true);
+
+    Point p = K.uCoords(pointels[0]),
+          u = K.uCoords(pointels[1]) - p,
+          v = K.uCoords(pointels[3]) - p;
+
+    static const RealPoint shift(-0.5, -0.5, -0.5);
+
+    return p + 0.5 * u + 0.5 * v + shift;
+}
+
+///////////////////////////////////////////////////////////////////////////////
+
+int main( int argc, char** argv )
+{
+    QApplication application(argc, argv);
+
+    std::string volfile = (argc > 1) ? argv[1] : (examplesPath + "samples/Al.100.vol");
+
+    auto params    = SH3::defaultParameters() | SHG3::defaultParameters();
+    auto bimage    = SH3::makeBinaryImage(volfile, params);
+    auto K         = SH3::getKSpace(bimage, params);
+    auto surface   = SH3::makeDigitalSurface(bimage, K, params);
+    auto surfels   = SH3::getSurfelRange(surface);
+
+    Integer bound = params["maxAABB"].as<Integer>();
+    double gridstep = params["gridstep"].as<double>();
+
+    Viewer3D<> viewer(K);
+    viewer << SetMode3D(Surfel().className(), "Basic");
+    viewer.show();
+
+    // Parameters
+    ProbingFactory probingFactory = [&bound](const auto& frame, const auto& surfacePredicate) {
+        // Tetrahedron-based estimator
+        return new ProbingAlgorithm(frame.p, { frame.b1, frame.b2, frame.normal }, surfacePredicate);
+
+        // Parallelepiped-based estimator
+        // return new ProbingAlgorithm(frame.p, { frame.b1, frame.b2, frame.normal }, surfacePredicate, bound);
+    };
+
+    bool verbose = true;
+
+    std::map<Surfel, RealPoint> preEstimations;
+    auto preEstimationsVector = SHG3::getCTrivialNormalVectors(surface, surfels, params);
+    for (std::size_t i = 0; i < surfels.size(); ++i)
+    {
+        preEstimations[surfels[i]] = preEstimationsVector[i];
+    }
+
+    Estimator estimator;
+    estimator.init(gridstep, surfels.begin(), surfels.end());
+    estimator.attach(surface);
+    estimator.setParams(probingFactory, preEstimations, verbose);
+
+    std::vector<Quantity> quantities;
+    estimator.eval(surfels.begin(), surfels.end(), std::back_inserter(quantities));
+
+    Color fillColor = viewer.getFillColor();
+
+    for (std::size_t i = 0; i < surfels.size(); ++i)
+    {
+        const Surfel& s = surfels[i];
+        const Quantity& n = quantities[i];
+
+        RealPoint origin = centerSurfel(K, s);
+
+        viewer.setFillColor(fillColor);
+        viewer << s;
+
+        // Pre-estimation in red
+        viewer.setLineColor(Color::Red);
+        viewer.addLine(origin, origin + 1.5 * preEstimationsVector[i].getNormalized(), 0.3);
+
+        // Estimated normal in green;
+        viewer.setLineColor(Color::Green);
+        viewer.addLine(origin, origin + 1.5 * n.getNormalized(), 0.3);
+    }
+
+    viewer << Viewer3D<>::updateDisplay;
+    application.exec();
+
+    return 0;
+}
+//                                                                           //
+///////////////////////////////////////////////////////////////////////////////
---
+++ src/DGtal/geometry/surfaces/estimation/PlaneProbingDigitalSurfaceLocalEstimator.h
@@ -0,0 +1,243 @@
+/**
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU Lesser General Public License as
+ *  published by the Free Software Foundation, either version 3 of the
+ *  License, or  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ **/
+
+#pragma once
+
+/**
+ * @file
+ * @author Jocelyn Meyron (\c jocelyn.meyron@liris.cnrs.fr )
+ * Laboratoire d'InfoRmatique en Image et Systemes d'information - LIRIS (CNRS, UMR 5205), CNRS, France
+ *
+ * @date 2020/12/07
+ *
+ * Header file for module PlaneProbingDigitalSurfaceLocalEstimator.cpp
+ *
+ * This file is part of the DGtal library.
+ */
+
+#if defined(PlaneProbingDigitalSurfaceLocalEstimator_RECURSES)
+#error Recursive header files inclusion detected in PlaneProbingDigitalSurfaceLocalEstimator.h
+#else // defined(PlaneProbingDigitalSurfaceLocalEstimator_RECURSES)
+/** Prevents recursive inclusion of headers. */
+#define PlaneProbingDigitalSurfaceLocalEstimator_RECURSES
+
+#if !defined PlaneProbingDigitalSurfaceLocalEstimator_h
+/** Prevents repeated inclusion of headers. */
+#define PlaneProbingDigitalSurfaceLocalEstimator_h
+
+//////////////////////////////////////////////////////////////////////////////
+// Inclusions
+#include <iostream>
+#include "DGtal/base/Common.h"
+#include "DGtal/geometry/surfaces/DigitalSurfacePredicate.h"
+//////////////////////////////////////////////////////////////////////////////
+
+namespace DGtal
+{
+
+  /////////////////////////////////////////////////////////////////////////////
+  // template class PlaneProbingDigitalSurfaceLocalEstimator
+  /**
+   * Description of template class 'PlaneProbingDigitalSurfaceLocalEstimator' <p>
+   * \brief Aim:
+   *
+   * @tparam TSurface the surface type.
+   * @tparam TProbingAlgorithm the probing algorithm.
+   */
+  template <typename TSurface, typename TProbingAlgorithm>
+  class PlaneProbingDigitalSurfaceLocalEstimator
+  {
+    // ----------------------- Private types ------------------------------
+  private:
+      struct ProbingFrame;
+
+    // ----------------------- Public types ------------------------------
+  public:
+      using Surface          = TSurface;
+      using ProbingAlgorithm = TProbingAlgorithm;
+      using Point            = typename ProbingAlgorithm::Point;
+      using Scalar           = double;
+      using Predicate        = DigitalSurfacePredicate<Surface>;
+      using ProbingFactory   = std::function<ProbingAlgorithm*(const ProbingFrame&, Predicate const&)>;
+
+      // ----------------------- model of CDigitalSurfaceLocalEstimator ----------------
+      using Surfel   = typename Surface::Surfel;
+      using Quantity = typename ProbingAlgorithm::Quantity;
+
+      // -------------------------------------- other types ----------------------------
+      using KSpace    = typename Surface::KSpace;
+      using SCell     = typename KSpace::SCell;
+      using Cell      = typename KSpace::Cell;
+      using Space     = typename KSpace::Space;
+      using RealPoint = typename Space::RealPoint;
+
+    // ----------------------- Standard services ------------------------------
+  public:
+    /**
+     * Default constructor.
+     */
+    PlaneProbingDigitalSurfaceLocalEstimator();
+
+    /*
+     * Constructor.
+     *
+     * @param aSurface a digital surface.
+     */
+    PlaneProbingDigitalSurfaceLocalEstimator(ConstAlias<Surface> aSurface);
+
+    /**
+     * Destructor.
+     */
+    ~PlaneProbingDigitalSurfaceLocalEstimator();
+
+    /**
+     * Copy constructor.
+     * @param other the object to clone.
+     */
+    PlaneProbingDigitalSurfaceLocalEstimator ( const PlaneProbingDigitalSurfaceLocalEstimator & other );
+
+    /**
+     * Move constructor.
+     * @param other the object to move.
+     */
+    PlaneProbingDigitalSurfaceLocalEstimator ( PlaneProbingDigitalSurfaceLocalEstimator && other ) = delete;
+
+    /**
+     * Copy assignment operator.
+     * @param other the object to copy.
+     * @return a reference on 'this'.
+     */
+    PlaneProbingDigitalSurfaceLocalEstimator & operator= ( const PlaneProbingDigitalSurfaceLocalEstimator & other );
+
+    /**
+     * Move assignment operator.
+     * @param other the object to move.
+     * @return a reference on 'this'.
+     */
+    PlaneProbingDigitalSurfaceLocalEstimator & operator= ( PlaneProbingDigitalSurfaceLocalEstimator && other ) = delete;
+
+    // ----------------- model of CSurfelLocalEstimator -----------------------
+  public:
+    template < typename SurfelConstIterator >
+    void init (Scalar const& h, SurfelConstIterator itb, SurfelConstIterator ite);
+
+    template < typename SurfelConstIterator >
+    Quantity eval (SurfelConstIterator it);
+
+    template < typename SurfelConstIterator, typename OutputIterator >
+    OutputIterator eval (SurfelConstIterator itb, SurfelConstIterator ite, OutputIterator out);
+
+    Scalar h () const;
+
+    // --------------- model of CDigitalSurfaceLocalEstimator ------------------
+  public:
+    void attach (ConstAlias<Surface> aSurface);
+
+    void setParams (ProbingFactory const& aProbingFactory,
+                    std::map<Surfel, RealPoint> const& aPreEstimations = {},
+                    bool aVerbose = false);
+
+    // ----------------------- Interface --------------------------------------
+  public:
+
+    /**
+     * Writes/Displays the object on an output stream.
+     * @param out the output stream where the object is written.
+     */
+    void selfDisplay ( std::ostream & out ) const;
+
+    /**
+     * Checks the validity/consistency of the object.
+     * @return 'true' if the object is valid, 'false' otherwise.
+     */
+    bool isValid() const;
+
+    // ------------------------- Protected Datas ------------------------------
+  protected:
+
+    // ------------------------- Private Datas --------------------------------
+  private:
+    ProbingAlgorithm* myProbingAlgorithm = nullptr;
+    CountedConstPtrOrConstPtr<Surface> mySurface;
+    Predicate myPredicate;
+    bool myH;
+    ProbingFactory myProbingFactory;
+    std::map<Surfel, RealPoint> myPreEstimations;
+    bool myVerbose;
+
+    // ------------------------- Hidden services ------------------------------
+  protected:
+
+    // ------------------------- Internals ------------------------------------
+  private:
+    struct ProbingFrame
+    {
+        Point p;
+        Point b1;
+        Point b2;
+        Point normal;
+
+        ProbingFrame rotatedCopy () const
+        {
+            Point newP  = p + b1,
+                  newB1 = b2,
+                  newB2 = -b1,
+                  newNormal = newB1.crossProduct(newB2);
+
+            return  { newP, newB1, newB2, newNormal };
+        }
+
+        Point shift () const
+        {
+            return b1 + b2 + normal;
+        }
+    };
+
+    ProbingFrame probingFrameFromSurfel (Surfel const& aSurfel) const;
+    ProbingFrame probingFrameWithPreEstimation (ProbingFrame const& aInitialFrame, RealPoint const& aPreEstimation) const;
+
+    static int signComponent (double x)
+    {
+        return (x >= 0) ? 1 : -1;
+    }
+
+  }; // end of class PlaneProbingDigitalSurfaceLocalEstimator
+
+
+  /**
+   * Overloads 'operator<<' for displaying objects of class 'PlaneProbingDigitalSurfaceLocalEstimator'.
+   * @param out the output stream where the object is written.
+   * @param object the object of class 'PlaneProbingDigitalSurfaceLocalEstimator' to write.
+   * @return the output stream after the writing.
+   */
+  template < typename TSurface, typename TProbingAlgorithm >
+  std::ostream&
+  operator<< ( std::ostream & out, const PlaneProbingDigitalSurfaceLocalEstimator<TSurface, TProbingAlgorithm> & object );
+
+} // namespace DGtal
+
+
+///////////////////////////////////////////////////////////////////////////////
+// Includes inline functions.
+#include "DGtal/geometry/surfaces/estimation/PlaneProbingDigitalSurfaceLocalEstimator.ih"
+
+//                                                                           //
+///////////////////////////////////////////////////////////////////////////////
+
+#endif // !defined PlaneProbingDigitalSurfaceLocalEstimator_h
+
+#undef PlaneProbingDigitalSurfaceLocalEstimator_RECURSES
+#endif // else defined(PlaneProbingDigitalSurfaceLocalEstimator_RECURSES)
