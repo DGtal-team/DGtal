fb03b28023584c73c140d6c3d4d38919766c1178
David Coeurjolly <david.coeurjolly@liris.cnrs.fr>
2020-12-26 17:11:33 +0100

    Doxygen warning and removing collab graphs

--- .travis/install_deps_macos.sh
+++ .travis/install_deps_macos.sh
@@ -8,6 +8,7 @@ set +e # The brew install has errors
 BTYPE="$BTYPE -DWITH_HDF5=false" && echo "Disabling HDF5 on MacOS";
 BTYPE="$BTYPE -DWITH_QGLVIEWER=false" && echo "Disabling QGLViewer on MacOS";
 BTYPE="$BTYPE -DWITH_MAGICK=false" && echo "Disabling imagemagick on MacOS";
+BTYPE="$BTYPE -DWITH_FFTW=false" && echo "Disabling fftw on MacOS";
 
 # Restoring set options
 set -$set_options
