455d9fa9a279b87a8cba8227436998801a0161a8
David Coeurjolly <david.coeurjolly@liris.cnrs.fr>
2021-11-09 09:43:55 +0100

    Eigen3 in tests.

--- tests/TestFunctions.cmake
+++ tests/TestFunctions.cmake
@@ -4,7 +4,7 @@ function(DGtal_add_test test_file) #optional_avoid_add_test
   string(RANDOM LENGTH 2 ALPHABET "0123456789" _random)
    if (${_random} LESS ${DGTAL_RANDOMIZED_TESTING_THRESHOLD}  OR ${test_file} IN_LIST DGTAL_RANDOMIZED_TESTING_WHITELIST)
    add_executable(${test_file} ${test_file}.cpp)
-    target_link_libraries (${test_file} PRIVATE DGtal Catch2::Catch2 DGtalCatch)
+    target_link_libraries (${test_file} PRIVATE DGtal ${DGtalLibDependencies} Catch2::Catch2 DGtalCatch)
     target_include_directories(${test_file} PRIVATE ${PROJECT_SOURCE_DIR}/tests/)
     target_include_directories(${test_file} PRIVATE ${PROJECT_BINARY_DIR}/tests/)
     if(NOT ${ARGC} GREATER 1)
