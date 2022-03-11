6952c1b7a63d2dc644ca2263c7a524a6235b2eed
Kerautret <bertrand.kerautret@univ-lyon2.fr>
2022-03-11 17:54:35 +0100

    typo

--- tests/io/writers/testGenericWriter.cpp
+++ tests/io/writers/testGenericWriter.cpp
@@ -154,7 +154,7 @@ bool testGenericWriter()
   trace.info() << "Testing writing color image writer ... ";  
   bool ok8 = DGtal::GenericWriter<Image2DColor>::exportFile( "testGenericWriterColorImage.ppm", anColorImage2D);
 #ifdef WITH_MAGICK
-  ok8 = ok8  DGtal::GenericWriter<Image2DColor>::exportFile( "testGenericWriterColorImage.png", anColorImage2D);
+  ok8 = ok8 &&  DGtal::GenericWriter<Image2DColor>::exportFile( "testGenericWriterColorImage.png", anColorImage2D);
 #endif  
   trace.info() <<"[done]"  << std::endl;
   nbok += ((ok1 && okh5 && ok2 & ok2bis && ok3 && ok3bis && ok4 && ok5 && ok6 && ok7 && ok8 && okITK) ? 1 : 0); 
