bd708f2986638d60f3916bfa1bd33890e057cd90
David Coeurjolly <david.coeurjolly@liris.cnrs.fr>
2022-03-17 16:40:35 +0100

    Testing edits in the azure scripts

--- wrap/deploy/setup.py
+++ wrap/deploy/setup.py
@@ -3,14 +3,14 @@
 from __future__ import print_function
 from os import sys, path
 
-try:
-    from skbuild import setup
-except ImportError:
-    print('scikit-build is required to build from source.', file=sys.stderr)
-    print('Please run:', file=sys.stderr)
-    print('', file=sys.stderr)
-    print('  python -m pip install scikit-build')
-    sys.exit(1)
+#try:
+#    from skbuild import setup
+#except ImportError:
+#    print('scikit-build is required to build from source.', file=sys.stderr)
+#    print('Please run:', file=sys.stderr)
+#    print('', file=sys.stderr)
+#    print('  python -m pip install scikit-build')
+#    sys.exit(1)
 
 sys.path.append(path.dirname(path.dirname(path.abspath(__file__))))
 from dgtalVersion import get_versions
