69e16cc0ef2169db41ae9afe533b82ed37961b95
Jacques-Olivier Lachaud <jacques-olivier.lachaud@univ-savoie.fr>
2022-07-07 15:31:13 +0200

    Add digital polyhedron example

--- examples/geometry/volumes/standardDigitalPolyhedronBuilder3D.cpp
+++ examples/geometry/volumes/standardDigitalPolyhedronBuilder3D.cpp
@@ -15,14 +15,14 @@
  **/
 
 /**
- * @file geometry/volumes/digitalPolyhedronBuilder3D.cpp
+ * @file geometry/volumes/standardDigitalPolyhedronBuilder3D.cpp
  * @ingroup Examples
  * @author Jacques-Olivier Lachaud (\c jacques-olivier.lachaud@univ-savoie.fr )
  * Laboratory of Mathematics (CNRS, UMR 5127), University of Savoie, France
  *
  * @date 2021/06/20
  *
- * An example file named digitalPolyhedronBuilder3D
+ * An example file named standardDigitalPolyhedronBuilder3D
  *
  * This file is part of the DGtal library.
  */
@@ -38,7 +38,7 @@
    For instance, you may call it on object "spot.obj" as
 
 \verbatim
-digitalPolyhedronBuilder3D ../examples/samples/spot.obj 0.005 7
+standardDigitalPolyhedronBuilder3D ../examples/samples/spot.obj 0.005 7
 \endverbatim
 
    The last parameter specifies whether you want to see vertices (1),
@@ -52,7 +52,7 @@ digitalPolyhedronBuilder3D ../examples/samples/spot.obj 0.005 7
 </td></tr>
 </table>
 
- \example geometry/volumes/digitalPolyhedronBuilder3D.cpp
+ \example geometry/volumes/standardDigitalPolyhedronBuilder3D.cpp
  */
 
 
@@ -83,17 +83,45 @@ typedef Space::RealVector   RealVector;
 typedef Space::Vector       Vector;
 typedef std::vector<Point>  PointRange;
 
+// Convenient class to represent different types of arithmetic planes as a predicate.
+template < bool Naive, bool Symmetric >
+struct MedianPlane {
+  Vector  N;
+  Integer mu;
+  Integer omega;
+  MedianPlane() = default;
+  MedianPlane( const MedianPlane& other ) = default;
+  MedianPlane( MedianPlane&& other ) = default;
+  MedianPlane& operator=( const MedianPlane& other ) = default;
+  MedianPlane& operator=( MedianPlane&& other ) = default;
+  MedianPlane( Point p, Point q, Point r )
+  {
+    N  = ( q - p ).crossProduct( r - p );
+    mu = N.dot( p );
+    omega = Naive ? N.norm( N.L_infty ) : N.norm( N.L_1 );
+    if ( Symmetric && ( ( omega & 1 ) == 0 ) ) omega += 1;
+    mu -= omega / 2;
+  }
+  bool operator()( const Point& p ) const
+  {
+    auto r = N.dot( p );
+    return ( mu <= r ) && ( r < mu+omega );
+  }
+};
+
+typedef MedianPlane< false, true > Plane;
+
 int main( int argc, char** argv )
 {
   trace.info() << "Usage: " << argv[ 0 ] << " <input.obj> <h> <view>" << std::endl;
   trace.info() << "\tComputes a digital polyhedron from an OBJ file" << std::endl;
   trace.info() << "\t- input.obj: choose your favorite mesh" << std::endl;
   trace.info() << "\t- h [==1]: the digitization gridstep" << std::endl;
-  trace.info() << "\t- view [==7]: display vertices(1), edges(2), faces(4)" << std::endl;
+  trace.info() << "\t- view [==7]: display vertices(1), edges(2), arcs(4), faces(8)" << std::endl;
   string filename = examplesPath + "samples/lion.obj";
   std::string  fn = argc > 1 ? argv[ 1 ]         : filename; //< vol filename
   double        h = argc > 2 ? atof( argv[ 2 ] ) : 1.0;
-  int        view = argc > 3 ? atoi( argv[ 3 ] ) : 7;
+  int        view = argc > 3 ? atoi( argv[ 3 ] ) : 31;
   // Read OBJ file
   std::ifstream input( fn.c_str() );
   DGtal::SurfaceMesh< RealPoint, RealVector > surfmesh;
@@ -107,7 +135,7 @@ int main( int argc, char** argv )
   QApplication application(argc,argv);
   typedef Viewer3D<Space,KSpace> MViewer;
   MViewer viewer;
-  viewer.setWindowTitle("digitalPolyhedronBuilder3D");
+  viewer.setWindowTitle("standardDigitalPolyhedronBuilder3D");
   viewer.show();  
 
   Point lo(-500,-500,-500);
@@ -124,46 +152,92 @@ int main( int argc, char** argv )
                 (Integer) round( p[ 2 ] ) );
       vertices[ v ] = q;
     }
-  std::set< Point > faces_set, edges_set;
+  std::set< Point > faces_set, pos_edges_set, neg_edges_set;
   auto faceVertices = surfmesh.allIncidentVertices();
   auto edgeVertices = surfmesh.allEdgeVertices();
   auto edgeFaces    = surfmesh.allEdgeFaces();
 
+  trace.beginBlock( "Checking face planarity" );
+  std::vector< Plane > face_planes;
+  face_planes.resize( surfmesh.nbFaces() );
+  bool planarity = true;
+  for ( int f = 0; f < surfmesh.nbFaces() && planarity; ++f )
+    {
+      PointRange X;
+      for ( auto v : faceVertices[ f ] )
+        X.push_back( vertices[ v ] );
+      face_planes[ f ] = Plane( X[ 0 ], X[ 1 ], X[ 2 ] );
+      for ( int v = 3; v < X.size(); v++ )
+        if ( ! face_planes[ f ]( X[ v ] ) )
+          {
+            trace.error() << "Face " << f << " is not planar." << std::endl;
+            planarity = false; break;
+          }
+    }
+  trace.endBlock();
+  if ( ! planarity ) return 1;
   trace.beginBlock( "Computing polyhedron" );
   for ( int f = 0; f < surfmesh.nbFaces(); ++f )
     {
       PointRange X;
       for ( auto v : faceVertices[ f ] )
         X.push_back( vertices[ v ] );
-      auto F = dconv.envelope( X, Algorithm::DIRECT );
+      auto F = dconv.relativeEnvelope( X, face_planes[ f ], Algorithm::DIRECT );
       faces_set.insert( F.cbegin(), F.cend() );
-    }
-  for ( int e = 0; e < surfmesh.nbEdges(); ++e )
-    {
-      PointRange X =
-        { vertices[ edgeVertices[ e ].first  ],
-          vertices[ edgeVertices[ e ].second ] };
-      auto E = dconv.envelope( X, Algorithm::DIRECT );
-      edges_set.insert( E.cbegin(), E.cend() );
+      for ( int i = 0; i < X.size(); i++ )
+        {
+          PointRange Y { X[ i ], X[ (i+1)%X.size() ] };
+          if ( Y[ 1 ] < Y[ 0 ] ) std::swap( Y[ 0 ], Y[ 1 ] ); 
+          int idx1 = faceVertices[ f ][ i ];
+          int idx2 = faceVertices[ f ][ (i+1)%X.size() ];
+          // Edges of both sides have many points in common
+          auto A = dconv.relativeEnvelope( Y, face_planes[ f ], Algorithm::DIRECT );
+          // Edges of both sides have much less points in common
+          // auto A = dconv.relativeEnvelope( Y, F, Algorithm::DIRECT );
+          bool pos = idx1 < idx2;
+          (pos ? pos_edges_set : neg_edges_set).insert( A.cbegin(), A.cend() );
+        }
     }
   trace.endBlock();
-  std::vector< Point > face_points, edge_points;
+  std::vector< Point > face_points, common_edge_points, arc_points, final_arc_points ;
+  std::vector< Point > pos_edge_points, neg_edge_points, both_edge_points;
   std::vector< Point > vertex_points = vertices;
   std::sort( vertex_points.begin(), vertex_points.end() );
+  std::set_symmetric_difference( pos_edges_set.cbegin(), pos_edges_set.cend(),
+                                 neg_edges_set.cbegin(), neg_edges_set.cend(),
+                                 std::back_inserter( arc_points ) );
+  std::set_intersection( pos_edges_set.cbegin(), pos_edges_set.cend(),
+                         neg_edges_set.cbegin(), neg_edges_set.cend(),
+                         std::back_inserter( common_edge_points ) );
+  std::set_union( pos_edges_set.cbegin(), pos_edges_set.cend(),
+                  neg_edges_set.cbegin(), neg_edges_set.cend(),
+                  std::back_inserter( both_edge_points ) );
   std::set_difference( faces_set.cbegin(), faces_set.cend(),
-                       edges_set.cbegin(), edges_set.cend(),
+                       both_edge_points.cbegin(), both_edge_points.cend(),
                        std::back_inserter( face_points ) );
-  std::set_difference( edges_set.cbegin(), edges_set.cend(),
+  std::set_difference( pos_edges_set.cbegin(), pos_edges_set.cend(),
+                       common_edge_points.cbegin(), common_edge_points.cend(),
+                       std::back_inserter( pos_edge_points ) );
+  std::set_difference( neg_edges_set.cbegin(), neg_edges_set.cend(),
+                       common_edge_points.cbegin(), common_edge_points.cend(),
+                       std::back_inserter( neg_edge_points ) );
+  std::set_difference( common_edge_points.cbegin(), common_edge_points.cend(),
                        vertex_points.cbegin(), vertex_points.cend(),
-                       std::back_inserter( edge_points ) );
-  auto total = vertex_points.size() + edge_points.size() + face_points.size();
-  trace.info() << "#vertex points=" << vertex_points.size() << std::endl;
-  trace.info() << "#edge   points=" << edge_points.size() << std::endl;
-  trace.info() << "#face   points=" << face_points.size() << std::endl;
-  trace.info() << "#total  points=" << total << std::endl;
+                       std::back_inserter( final_arc_points ) );
+  auto total = vertex_points.size() + pos_edge_points.size()
+    + neg_edge_points.size()
+    + final_arc_points.size() + face_points.size();
+  trace.info() << "#vertex   points=" << vertex_points.size() << std::endl;
+  trace.info() << "#pos edge points=" << pos_edge_points.size() << std::endl;
+  trace.info() << "#neg edge points=" << neg_edge_points.size() << std::endl;
+  trace.info() << "#arc      points=" << final_arc_points.size() << std::endl;
+  trace.info() << "#face     points=" << face_points.size() << std::endl;
+  trace.info() << "#total    points=" << total << std::endl;
 
   // display everything
-  Color colors[] = { Color::Black, Color( 100, 100, 100 ), Color( 200, 200, 200 ) };
+  Color colors[] =
+    { Color::Black, Color::Blue, Color::Red,
+      Color::Magenta, Color( 200, 200, 200 ) };
   if ( view & 0x1 )
     {
       viewer.setLineColor( colors[ 0 ] );
@@ -174,12 +248,24 @@ int main( int argc, char** argv )
     {
       viewer.setLineColor( colors[ 1 ] );
       viewer.setFillColor( colors[ 1 ] );
-      for ( auto p : edge_points ) viewer << p;
+      for ( auto p : pos_edge_points ) viewer << p;
     }
   if ( view & 0x4 )
     {
       viewer.setLineColor( colors[ 2 ] );
       viewer.setFillColor( colors[ 2 ] );
+      for ( auto p : neg_edge_points ) viewer << p;
+    }
+  if ( view & 0x8 )
+    {
+      viewer.setLineColor( colors[ 3 ] );
+      viewer.setFillColor( colors[ 3 ] );
+      for ( auto p : final_arc_points ) viewer << p;
+    }
+  if ( view & 0x10 )
+    {
+      viewer.setLineColor( colors[ 4 ] );
+      viewer.setFillColor( colors[ 4 ] );
       for ( auto p : face_points ) viewer << p;
     }
   viewer << MViewer::updateDisplay;
