dbb2097394e0e3a2be36e406a7f9c284edd272b5
David Coeurjolly <david.coeurjolly@liris.cnrs.fr>
2023-02-22 11:15:48 +0100

    Testing conan for windows CI

--- cmake/CheckDGtalDependencies.cmake
+++ cmake/CheckDGtalDependencies.cmake
@@ -5,6 +5,32 @@
 message(STATUS "-------------------------------------------------------------------------------")
 message(STATUS "DGtal required dependencies: ")
 
+
+list(APPEND CMAKE_MODULE_PATH ${CMAKE_BINARY_DIR})
+list(APPEND CMAKE_PREFIX_PATH ${CMAKE_BINARY_DIR})
+
+if(NOT EXISTS "${CMAKE_BINARY_DIR}/conan.cmake")
+  message(STATUS "Downloading conan.cmake from https://github.com/conan-io/cmake-conan")
+  file(DOWNLOAD "https://raw.githubusercontent.com/conan-io/cmake-conan/0.18.1/conan.cmake"
+                "${CMAKE_BINARY_DIR}/conan.cmake"
+                TLS_VERIFY ON)
+endif()
+
+include(${CMAKE_BINARY_DIR}/conan.cmake)
+
+conan_cmake_configure(REQUIRES zlib/1.2.13
+                      GENERATORS cmake_find_package)
+
+conan_cmake_configure(REQUIRES boost/1.81.0
+                      OPTIONS boost:header_only=True
+                      GENERATORS cmake_find_package)
+
+conan_cmake_autodetect(settings)
+conan_cmake_install(PATH_OR_REFERENCE .
+                    BUILD missing
+                    REMOTE conancenter
+                    SETTINGS ${settings})
+
 # -----------------------------------------------------------------------------
 # Looking for boost
 # -----------------------------------------------------------------------------
@@ -12,7 +38,7 @@ set(Boost_USE_STATIC_LIBS   ON)
 set(Boost_USE_MULTITHREADED ON)
 set(Boost_USE_STATIC_RUNTIME OFF)
 set(Boost_FOUND FALSE)
-find_package(Boost 1.50.0 REQUIRED)
+find_package(Boost 1.81.0 REQUIRED)
 target_compile_definitions(DGtal PUBLIC ${BOOST_DEFINITIONS} -DBOOST_ALL_NO_LIB)
 # SYSTEM to avoid warnings from boost.
 target_include_directories(DGtal SYSTEM PUBLIC ${Boost_INCLUDE_DIRS} )
