1d487ca33aa46ae71cd8ea4e761d422e06600198
David Coeurjolly <david.coeurjolly@cnrs.fr>
2024-03-08 08:54:14 +0100

    Update tests/shapes/testWindingNumbersShape.cpp
    Co-authored-by: Bertrand Kerautret <bertrand.kerautret@univ-lyon2.fr>

--- tests/shapes/testWindingNumbersShape.cpp
+++ tests/shapes/testWindingNumbersShape.cpp
@@ -15,7 +15,7 @@
  **/
 
 /**
- * @file testlibigl.cpp
+ * @file shapes/testWindingNumbersShape.cpp
  * @ingroup Tests
  * @author David Coeurjolly (\c david.coeurjolly@liris.cnrs.fr )
  * Laboratoire d'InfoRmatique en Image et Systemes d'information - LIRIS (CNRS, UMR 5205), CNRS, France
