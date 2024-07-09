62dd75503a5c0b351d89261af529f464b48f758b
Jacques-Olivier Lachaud <jacques-olivier.lachaud@univ-savoie.fr>
2024-07-09 11:30:37 +0200

    Update graphs and module doc

--- examples/geometry/volumes/pConvexity-benchmark.cpp
+++ examples/geometry/volumes/pConvexity-benchmark.cpp
@@ -215,6 +215,132 @@ timingsFullConvexityFast( std::vector< std::tuple< std::size_t, double, bool > >
 }
 
 
+template <Dimension dim>
+void
+timingsPConvexityNonConvex
+( std::vector< std::tuple< std::size_t, double, bool > >& results,
+  std::size_t nb_tries, std::size_t range )
+{
+  typedef KhalimskySpaceND<dim,int64_t>    KSpace;
+  typedef typename KSpace::Point           Point;
+  typedef typename KSpace::Space           Space;
+  typedef HyperRectDomain< Space >         Domain;
+  typedef DigitalConvexity< KSpace >       DConvexity;
+  typedef PConvexity< Space >              PConvexity;
+  DConvexity dconv( Point::diagonal( -1 ), Point::diagonal( range ) );
+  PConvexity pconv;
+  Domain     domain( Point::diagonal( 0 ), Point::diagonal( range ) );
+  std::cout << "Computing " << nb_tries << " P-convexities in Z" << dim << std::endl;
+  for ( auto n = 0; n < nb_tries; ++n )
+    {
+      double filling_probability = 0.1 + 0.9 * double( n ) / double( nb_tries );
+      // Create vertices
+      std::set< Point > S;
+      std::size_t nb_vertices
+	= std::size_t( filling_probability * ceil( pow( range, dim ) ) );
+      for ( auto i = 0; i < nb_vertices; i++ ) {
+	Point p;
+	for ( auto j = 0; j < dim; j++ ) p[ j ] = rand() % range;
+	S.insert( p );
+      }
+      // create digital set.
+      std::vector< Point > X( S.cbegin(), S.cend() );
+      // Analyse P-convexity
+      std::chrono::high_resolution_clock::time_point
+	t1 = std::chrono::high_resolution_clock::now();
+      bool is_pconvex = pconv.isPConvex( X );
+      std::chrono::high_resolution_clock::time_point
+	t2 = std::chrono::high_resolution_clock::now();
+      double dt = std::chrono::duration_cast<std::chrono::nanoseconds>(t2 - t1).count();
+      results.push_back( std::make_tuple( X.size(), dt/1e6, is_pconvex ) );
+    }
+}
+
+template <Dimension dim>
+void
+timingsFullConvexityNonConvex
+( std::vector< std::tuple< std::size_t, double, bool > >& results,
+  std::size_t nb_tries, std::size_t range )
+{
+  typedef KhalimskySpaceND<dim,int64_t>    KSpace;
+  typedef typename KSpace::Point           Point;
+  typedef typename KSpace::Space           Space;
+  typedef HyperRectDomain< Space >         Domain;
+  typedef DigitalConvexity< KSpace >       DConvexity;
+  typedef PConvexity< Space >              PConvexity;
+  DConvexity dconv( Point::diagonal( -1 ), Point::diagonal( range ) );
+  PConvexity pconv;
+  Domain     domain( Point::diagonal( 0 ), Point::diagonal( range ) );
+  std::cout << "Computing " << nb_tries << " full convexities in Z" << dim << std::endl;
+  for ( auto n = 0; n < nb_tries; ++n )
+    {
+      double filling_probability = 0.1 + 0.9 * double( n ) / double( nb_tries );
+      // Create vertices
+      std::set< Point > S;
+      std::size_t nb_vertices
+	= std::size_t( filling_probability * ceil( pow( range, dim ) ) );
+      for ( auto i = 0; i < nb_vertices; i++ ) {
+	Point p;
+	for ( auto j = 0; j < dim; j++ ) p[ j ] = rand() % range;
+	S.insert( p );
+      }
+      // create digital set.
+      std::vector< Point > X( S.cbegin(), S.cend() );
+      // Analyse full convexity
+      std::chrono::high_resolution_clock::time_point
+	t1 = std::chrono::high_resolution_clock::now();
+      bool is_fconvex = dconv.isFullyConvex( X );
+      std::chrono::high_resolution_clock::time_point
+	t2 = std::chrono::high_resolution_clock::now();
+      double dt = std::chrono::duration_cast<std::chrono::nanoseconds>(t2 - t1).count();
+      results.push_back( std::make_tuple( X.size(), dt/1e6, is_fconvex ) );
+    }
+}
+
+
+template <Dimension dim>
+void
+timingsFullConvexityFastNonConvex
+( std::vector< std::tuple< std::size_t, double, bool > >& results,
+  std::size_t nb_tries, std::size_t range )
+{
+  typedef KhalimskySpaceND<dim,int64_t>    KSpace;
+  typedef typename KSpace::Point           Point;
+  typedef typename KSpace::Space           Space;
+  typedef HyperRectDomain< Space >         Domain;
+  typedef DigitalConvexity< KSpace >       DConvexity;
+  typedef PConvexity< Space >              PConvexity;
+  DConvexity dconv( Point::diagonal( -1 ), Point::diagonal( range ) );
+  PConvexity pconv;
+  Domain     domain( Point::diagonal( 0 ), Point::diagonal( range ) );
+  std::cout << "Computing " << nb_tries << " full convexities (fast) in Z" << dim << std::endl;
+  for ( auto n = 0; n < nb_tries; ++n )
+    {
+      double filling_probability = 0.1 + 0.9 * double( n ) / double( nb_tries );
+      // Create vertices
+      std::set< Point > S;
+      std::size_t nb_vertices
+	= std::size_t( filling_probability * ceil( pow( range, dim ) ) );
+      for ( auto i = 0; i < nb_vertices; i++ ) {
+	Point p;
+	for ( auto j = 0; j < dim; j++ ) p[ j ] = rand() % range;
+	S.insert( p );
+      }
+      // create digital set.
+      std::vector< Point > X( S.cbegin(), S.cend() );
+      // Analyse full convexity
+      std::chrono::high_resolution_clock::time_point
+	t1 = std::chrono::high_resolution_clock::now();
+      bool is_fconvex = dconv.isFullyConvexFast( X );
+      std::chrono::high_resolution_clock::time_point
+	t2 = std::chrono::high_resolution_clock::now();
+      double dt = std::chrono::duration_cast<std::chrono::nanoseconds>(t2 - t1).count();
+      results.push_back( std::make_tuple( X.size(), dt/1e6, is_fconvex ) );
+    }
+}
+
+
+
 void outputResults( Dimension dim,
 		    const std::vector< std::tuple< std::size_t, double, bool > >& results,
 		    const std::string& fname )
@@ -241,13 +367,25 @@ void outputResults( Dimension dim,
   plot "./timings-p-convexity-Z4.txt" using 1:($3 == 1 ? $2 : 1/0) title "P-convex in Z4" w p, "./timings-p-convexity-Z4.txt" using 1:($3 == 0 ? $2 : 1/0) title "non P-convex in Z4" w p,  0.4e-5*x*log(x) w l lw 2
 
   set terminal eps font "Helvetica,14"
-  sezt key left
+  set key bottom right
   
-  plot [1e2:1e7][1e-2:1e4] "./timings-p-convexity-Z2.txt" using 1:($3 == 1 ? $2 : 1/0) title "P-convex in Z2" w p, "./timings-p-convexity-Z2.txt" using 1:($3 == 0 ? $2 : 1/0) title "non P-convex in Z2" w p, "./timings-fc-convexity-Z2.txt" using 1:($3 == 1 ? $2 : 1/0) title "fully convex in Z2" w p, "./timings-fc-convexity-Z2.txt" using 1:($3 == 0 ? $2 : 1/0) title "non fully convex in Z2" w p, "./timings-fcf-convexity-Z2.txt" using 1:($3 == 1 ? $2 : 1/0) title "fully convex in Z2 (fast)" w p pt 6, "./timings-fcf-convexity-Z2.txt" using 1:($3 == 0 ? $2 : 1/0) title "non fully convex in Z2 (fast)" w p pt 7, 0.2e-5*x*log(x) w l lw 2
+  plot [1e2:1e7][1e-2:1e4] 1e-6*x*log(x) w l lw 3, "./timings-p-convexity-Z2.txt" using 1:($3 == 1 ? $2 : 1/0) title "FC: P-convex charac. (in Z2)" w p pt 5 lc rgb "blue", "./timings-p-convexity-Z2.txt" using 1:($3 == 0 ? $2 : 1/0) title "non FC: P-convex charac. (in Z2)" w p pt 4 lc rgb "blue",  "./timings-fcf-convexity-Z2.txt" using 1:($3 == 1 ? $2 : 1/0) title "FC: cellular charac. (in Z2)" w p pt 7 lc rgb "black", "./timings-fcf-convexity-Z2.txt" using 1:($3 == 0 ? $2 : 1/0) title "non FC: cellular charac. (in Z2)" w p pt 6 lc rgb "black", "./timings-fc-convexity-Z2.txt" using 1:($3 == 1 ? $2 : 1/0) title "FC: discrete morphological charac. (in Z2)" w p pt 13 lc rgb "magenta", "./timings-fc-convexity-Z2.txt" using 1:($3 == 0 ? $2 : 1/0) title "non FC: discrete morphological charac. (in Z2)" w p pt 12 lc rgb "magenta"
 
-  plot [1e2:1e7][1e-2:1e4] "./timings-p-convexity-Z3.txt" using 1:($3 == 1 ? $2 : 1/0) title "P-convex in Z3" w p, "./timings-p-convexity-Z3.txt" using 1:($3 == 0 ? $2 : 1/0) title "non P-convex in Z3" w p, "./timings-fc-convexity-Z3.txt" using 1:($3 == 1 ? $2 : 1/0) title "fully convex in Z3" w p, "./timings-fc-convexity-Z3.txt" using 1:($3 == 0 ? $2 : 1/0) title "non fully convex in Z3" w p, "./timings-fcf-convexity-Z3.txt" using 1:($3 == 1 ? $2 : 1/0) title "fully convex in Z3 (fast)" w p pt 6, "./timings-fcf-convexity-Z3.txt" using 1:($3 == 0 ? $2 : 1/0) title "non fully convex in Z3 (fast)" w p pt 7, 0.2e-5*x*log(x) w l lw 2
+  
+  plot [1e2:1e7][1e-2:1e4] 1e-6*x*log(x) w l lw 3, "./timings-p-convexity-Z3.txt" using 1:($3 == 1 ? $2 : 1/0) title "FC: P-convex charac. (in Z3)" w p pt 5 lc rgb "blue", "./timings-p-convexity-Z3.txt" using 1:($3 == 0 ? $2 : 1/0) title "non FC: P-convex charac. (in Z3)" w p pt 4 lc rgb "blue",  "./timings-fcf-convexity-Z3.txt" using 1:($3 == 1 ? $2 : 1/0) title "FC: cellular charac. (in Z3)" w p pt 7 lc rgb "black", "./timings-fcf-convexity-Z3.txt" using 1:($3 == 0 ? $2 : 1/0) title "non FC: cellular charac. (in Z3)" w p pt 6 lc rgb "black", "./timings-fc-convexity-Z3.txt" using 1:($3 == 1 ? $2 : 1/0) title "FC: discrete morphological charac. (in Z3)" w p pt 13 lc rgb "magenta", "./timings-fc-convexity-Z3.txt" using 1:($3 == 0 ? $2 : 1/0) title "non FC: discrete morphological charac. (in Z3)" w p pt 12 lc rgb "magenta"
 
-  plot [1e2:1e7][1e-2:1e4] "./timings-p-convexity-Z4.txt" using 1:($3 == 1 ? $2 : 1/0) title "P-convex in Z4" w p, "./timings-p-convexity-Z4.txt" using 1:($3 == 0 ? $2 : 1/0) title "non P-convex in Z4" w p, "./timings-fc-convexity-Z4.txt" using 1:($3 == 1 ? $2 : 1/0) title "fully convex in Z4" w p, "./timings-fc-convexity-Z4.txt" using 1:($3 == 0 ? $2 : 1/0) title "non fully convex in Z4" w p, "./timings-fcf-convexity-Z4.txt" using 1:($3 == 1 ? $2 : 1/0) title "fully convex in Z4 (fast)" w p pt 6, "./timings-fcf-convexity-Z4.txt" using 1:($3 == 0 ? $2 : 1/0) title "non fully convex in Z4 (fast)" w p pt 7, 0.2e-5*x*log(x) w l lw 2
+  
+  plot [1e2:1e7][1e-2:1e4] 1e-6*x*log(x) w l lw 3, "./timings-p-convexity-Z4.txt" using 1:($3 == 1 ? $2 : 1/0) title "FC: P-convex charac. (in Z4)" w p pt 5 lc rgb "blue", "./timings-p-convexity-Z4.txt" using 1:($3 == 0 ? $2 : 1/0) title "non FC: P-convex charac. (in Z4)" w p pt 4 lc rgb "blue",  "./timings-fcf-convexity-Z4.txt" using 1:($3 == 1 ? $2 : 1/0) title "FC: cellular charac. (in Z4)" w p pt 7 lc rgb "black", "./timings-fcf-convexity-Z4.txt" using 1:($3 == 0 ? $2 : 1/0) title "non FC: cellular charac. (in Z4)" w p pt 6 lc rgb "black", "./timings-fc-convexity-Z4.txt" using 1:($3 == 1 ? $2 : 1/0) title "FC: discrete morphological charac. (in Z4)" w p pt 13 lc rgb "magenta", "./timings-fc-convexity-Z4.txt" using 1:($3 == 0 ? $2 : 1/0) title "non FC: discrete morphological charac. (in Z4)" w p pt 12 lc rgb "magenta"
+
+  set terminal eps font "Helvetica,12"
+  set key bottom right
+  
+  plot [1e2:1e7][1e-2:1e4] 1e-6*x*log(x) w l lw 3, "./timings-p-convexity-ncvx-Z2.txt" using 1:2 title "P-convex charac. (in Z2)" w p pt 4 lc rgb "blue", "./timings-fc-convexity-ncvx-Z2.txt" using 1:2 title "discrete morphological charac. (in Z2)" w p pt 12 lc rgb "magenta", "./timings-fcf-convexity-ncvx-Z2.txt" using 1:2 title "cellular charac. (in Z2)" w p pt 6 lc rgb "black"
+
+  plot [1e2:1e7][1e-2:1e4] 1e-6*x*log(x) w l lw 3, "./timings-p-convexity-ncvx-Z3.txt" using 1:2 title "P-convex charac. (in Z3)" w p pt 4 lc rgb "blue", "./timings-fc-convexity-ncvx-Z3.txt" using 1:2 title "discrete morphological charac. (in Z3)" w p pt 12 lc rgb "magenta", "./timings-fcf-convexity-ncvx-Z3.txt" using 1:2 title "cellular charac. (in Z3)" w p pt 6 lc rgb "black"
+
+  plot [1e2:1e7][1e-2:1e4] 1e-6*x*log(x) w l lw 3, "./timings-p-convexity-ncvx-Z4.txt" using 1:2 title "P-convex charac. (in Z4)" w p pt 4 lc rgb "blue", "./timings-fc-convexity-ncvx-Z4.txt" using 1:2 title "discrete morphological charac. (in Z4)" w p pt 12 lc rgb "magenta", "./timings-fcf-convexity-ncvx-Z4.txt" using 1:2 title "cellular charac. (in Z4)" w p pt 6 lc rgb "black"
+  
   
 */
 
@@ -295,7 +433,7 @@ int main( int argc, char* argv[] )
 
   // Full convexity
   srand( 0 );
-  if ( true )
+  if ( false )
     {
       std::vector< std::tuple< std::size_t, double, bool > > R2;
       timingsFullConvexity<2>( R2, 50, 3, 100, 0.5 );
@@ -307,7 +445,7 @@ int main( int argc, char* argv[] )
       timingsFullConvexity<2>( R2, 25, 5,2000, 0.5 );
       outputResults( 2, R2, "timings-fc-convexity-Z2.txt" );
     }
-  if ( true )
+  if ( false )
     {
       std::vector< std::tuple< std::size_t, double, bool > > R3;
       timingsFullConvexity<3>( R3, 50, 3, 10, 0.5 );
@@ -318,7 +456,7 @@ int main( int argc, char* argv[] )
       timingsFullConvexity<3>( R3, 25, 5, 320, 0.5 );
       outputResults( 3, R3, "timings-fc-convexity-Z3.txt" );
     }
-  if ( true )
+  if ( false )
     {
       std::vector< std::tuple< std::size_t, double, bool > > R4;
       timingsFullConvexity<4>( R4, 50, 5, 10, 0.5 );
@@ -335,7 +473,7 @@ int main( int argc, char* argv[] )
 
   // Full convexity fast
   srand( 0 );
-  if ( true )
+  if ( false )
     {
       std::vector< std::tuple< std::size_t, double, bool > > R2;
       timingsFullConvexityFast<2>( R2, 50, 3, 100, 0.5 );
@@ -347,7 +485,7 @@ int main( int argc, char* argv[] )
       timingsFullConvexityFast<2>( R2, 25, 5,2000, 0.5 );
       outputResults( 2, R2, "timings-fcf-convexity-Z2.txt" );
     }
-  if ( true )
+  if ( false )
     {
       std::vector< std::tuple< std::size_t, double, bool > > R3;
       timingsFullConvexityFast<3>( R3, 50, 3, 10, 0.5 );
@@ -358,7 +496,7 @@ int main( int argc, char* argv[] )
       timingsFullConvexityFast<3>( R3, 25, 5, 320, 0.5 );
       outputResults( 3, R3, "timings-fcf-convexity-Z3.txt" );
     }
-  if ( true )
+  if ( false )
     {
       std::vector< std::tuple< std::size_t, double, bool > > R4;
       timingsFullConvexityFast<4>( R4, 50, 5, 10, 0.5 );
@@ -372,6 +510,106 @@ int main( int argc, char* argv[] )
       timingsFullConvexityFast<4>( R4, 5, 6, 120, 0.5 );
       outputResults( 4, R4, "timings-fcf-convexity-Z4.txt" );
     }
+
+  // P-convexity
+  srand( 0 );
+  if ( false )
+    {
+      std::vector< std::tuple< std::size_t, double, bool > > R2;
+      timingsPConvexityNonConvex<2>( R2, 50,  100 );
+      timingsPConvexityNonConvex<2>( R2, 50,  200 );
+      timingsPConvexityNonConvex<2>( R2, 50,  400 );
+      timingsPConvexityNonConvex<2>( R2, 50,  600 );
+      timingsPConvexityNonConvex<2>( R2, 50,  800 );
+      timingsPConvexityNonConvex<2>( R2, 50, 1200 );
+      timingsPConvexityNonConvex<2>( R2, 50, 2000 );
+      outputResults( 2, R2, "timings-p-convexity-ncvx-Z2.txt" );
+    }
+  if ( false )
+    {
+      std::vector< std::tuple< std::size_t, double, bool > > R3;
+      timingsPConvexityNonConvex<3>( R3, 50,  20 );
+      timingsPConvexityNonConvex<3>( R3, 50,  40 );
+      timingsPConvexityNonConvex<3>( R3, 50,  80 );
+      timingsPConvexityNonConvex<3>( R3, 50,  160 );
+      timingsPConvexityNonConvex<3>( R3, 50,  320 );
+      outputResults( 3, R3, "timings-p-convexity-ncvx-Z3.txt" );
+    }
+  if ( false )
+    {
+      std::vector< std::tuple< std::size_t, double, bool > > R4;
+      timingsPConvexityNonConvex<4>( R4, 50,  10 );
+      timingsPConvexityNonConvex<4>( R4, 50,  20 );
+      timingsPConvexityNonConvex<4>( R4, 50,  30 );
+      timingsPConvexityNonConvex<4>( R4, 40,  40 );
+      timingsPConvexityNonConvex<4>( R4, 20,  50 );
+      outputResults( 4, R4, "timings-p-convexity-ncvx-Z4.txt" );
+    }
+  if ( false )
+    {
+      std::vector< std::tuple< std::size_t, double, bool > > R2;
+      timingsFullConvexityNonConvex<2>( R2, 50,  100 );
+      timingsFullConvexityNonConvex<2>( R2, 50,  200 );
+      timingsFullConvexityNonConvex<2>( R2, 50,  400 );
+      timingsFullConvexityNonConvex<2>( R2, 50,  600 );
+      timingsFullConvexityNonConvex<2>( R2, 50,  800 );
+      timingsFullConvexityNonConvex<2>( R2, 50, 1200 );
+      timingsFullConvexityNonConvex<2>( R2, 50, 2000 );
+      outputResults( 2, R2, "timings-fc-convexity-ncvx-Z2.txt" );
+    }
+  if ( false )
+    {
+      std::vector< std::tuple< std::size_t, double, bool > > R3;
+      timingsFullConvexityNonConvex<3>( R3, 50,  20 );
+      timingsFullConvexityNonConvex<3>( R3, 50,  40 );
+      timingsFullConvexityNonConvex<3>( R3, 50,  80 );
+      timingsFullConvexityNonConvex<3>( R3, 40,  160 );
+      timingsFullConvexityNonConvex<3>( R3, 25,  320 );
+      outputResults( 3, R3, "timings-fc-convexity-ncvx-Z3.txt" );
+    }
+  if ( false )
+    {
+      std::vector< std::tuple< std::size_t, double, bool > > R4;
+      timingsFullConvexityNonConvex<4>( R4, 50,  10 );
+      timingsFullConvexityNonConvex<4>( R4, 50,  20 );
+      timingsFullConvexityNonConvex<4>( R4, 50,  30 );
+      timingsFullConvexityNonConvex<4>( R4, 40,  40 );
+      timingsFullConvexityNonConvex<4>( R4, 20,  50 );
+      outputResults( 4, R4, "timings-fc-convexity-ncvx-Z4.txt" );
+    }
+  if ( false )
+    {
+      std::vector< std::tuple< std::size_t, double, bool > > R2;
+      timingsFullConvexityFastNonConvex<2>( R2, 50,  100 );
+      timingsFullConvexityFastNonConvex<2>( R2, 50,  200 );
+      timingsFullConvexityFastNonConvex<2>( R2, 50,  400 );
+      timingsFullConvexityFastNonConvex<2>( R2, 50,  600 );
+      timingsFullConvexityFastNonConvex<2>( R2, 50,  800 );
+      timingsFullConvexityFastNonConvex<2>( R2, 50, 1200 );
+      timingsFullConvexityFastNonConvex<2>( R2, 50, 2000 );
+      outputResults( 2, R2, "timings-fcf-convexity-ncvx-Z2.txt" );
+    }
+  if ( false )
+    {
+      std::vector< std::tuple< std::size_t, double, bool > > R3;
+      timingsFullConvexityFastNonConvex<3>( R3, 50,  20 );
+      timingsFullConvexityFastNonConvex<3>( R3, 50,  40 );
+      timingsFullConvexityFastNonConvex<3>( R3, 50,  80 );
+      timingsFullConvexityFastNonConvex<3>( R3, 40,  160 );
+      timingsFullConvexityFastNonConvex<3>( R3, 25,  320 );
+      outputResults( 3, R3, "timings-fcf-convexity-ncvx-Z3.txt" );
+    }
+  if ( false )
+    {
+      std::vector< std::tuple< std::size_t, double, bool > > R4;
+      timingsFullConvexityFastNonConvex<4>( R4, 50,  10 );
+      timingsFullConvexityFastNonConvex<4>( R4, 50,  20 );
+      timingsFullConvexityFastNonConvex<4>( R4, 50,  30 );
+      timingsFullConvexityFastNonConvex<4>( R4, 40,  40 );
+      timingsFullConvexityFastNonConvex<4>( R4, 20,  50 );
+      outputResults( 4, R4, "timings-fcf-convexity-ncvx-Z4.txt" );
+    }
+  
   
   return 0;
 }
