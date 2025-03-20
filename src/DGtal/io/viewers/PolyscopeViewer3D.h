9e3a272298453749a30cc15600dd088ecb17422c
Bastien DOIGNIES <bastien.doignies@liris.cnrs.fr>
2025-03-20 11:10:42 +0100

    Polyscope Viewer Lines + Cubes
    Matching: viewer3D-3-objets
    
    Bug Fix:
     - Line list not being created with DrawAdjacencies

---
+++ src/DGtal/io/viewers/PolyscopeViewer3D.h
@@ -0,0 +1,69 @@
+/**
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU Lesser General Public License as
+ *  published by the Free Software Foundation, either version 3 of the
+ *  License, or  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ **/
+#pragma once
+
+#include <iostream>
+#include "polyscope/polyscope.h"
+#include "polyscope/curve_network.h"
+#include "polyscope/volume_mesh.h"
+
+#include "DGtal/io/Display3D.h"
+#include "DGtal/io/Display3DFactory.h"
+/**
+ * @file PolyscopeViewer3D.h
+ * @author Bastien Doignies
+ *
+ * @date 2025/03/20
+ *
+ * Header file for concept Polyscopeviewer.cpp
+ *
+ * This file is part of the DGtal library.
+ */
+
+namespace DGtal
+{
+    template <typename TSpace  = SpaceND<3>,
+              typename TKSpace = KhalimskySpaceND<3>>
+    class PolyscopeViewer3D : public Display3D<TSpace, TKSpace>
+    {
+    public:
+
+
+        PolyscopeViewer3D();
+
+        void show();
+
+
+        PolyscopeViewer3D<TSpace, TKSpace> & operator<<(
+            const typename PolyscopeViewer3D<TSpace, TKSpace>::StreamKey & key
+        );
+
+        template <typename TDrawableWithViewer3D>
+        PolyscopeViewer3D<TSpace, TKSpace> & operator<<(const TDrawableWithViewer3D & object);
+
+    public:
+        void selfDisplay( std::ostream & out ) const;
+    private:
+        void createPolyscopeObjects() const;
+        void registerCubeMaps() const;
+        void registerLines() const;
+    };  
+
+    template <typename TSpace, typename TKSpace>
+    std::ostream & operator<<(std::ostream & out, const PolyscopeViewer3D<TSpace, TKSpace> & object);
+}
+
+#include "PolyscopeViewer3D.ih"
--- tests/io/viewers/testPolyscopeViewer.cpp
+++ tests/io/viewers/testPolyscopeViewer.cpp
@@ -1,9 +1,38 @@
 #include <iostream>
-#include "polyscope/polyscope.h"
 
-int main()
+#include "DGtal/base/Common.h"
+#include "DGtal/shapes/Shapes.h"
+#include "DGtal/helpers/StdDefs.h"
+#include "DGtal/io/viewers/PolyscopeViewer3D.h"
+
+using namespace std;
+using namespace DGtal;
+using namespace Z3i;
+
+int main( int argc, char** argv )
 {
-	polyscope::init();
-  	polyscope::show();
-	return 0;
+    typedef PolyscopeViewer3D<> MyViewer;
+    MyViewer viewer;
+    {
+        Point p1( 0, 0, 0 );
+        Point p2( 10, 10 , 10 );
+        Domain domain( p1, p2 );
+
+        DigitalSet shape_set( domain );
+        Shapes<Domain>::addNorm1Ball( shape_set, Point( 5, 5, 5 ), 2 );
+        Shapes<Domain>::addNorm2Ball( shape_set, Point( 3, 3, 3 ), 2 );
+        viewer <<  CustomColors3D(Color(250, 200,0, 100),Color(250, 200,0, 25));
+        viewer << shape_set;
+
+        Object6_18 shape( dt6_18, shape_set );
+        viewer << SetMode3D( shape.className(), "DrawAdjacencies" );
+        viewer << shape;
+
+        Object18_6 shape2( dt18_6, shape_set );
+        viewer << SetMode3D( shape2.className(), "DrawAdjacencies" );
+        viewer << shape2;
+    }
+    viewer << MyViewer::updateDisplay;
+    viewer.show();
+    return 0;
 }
