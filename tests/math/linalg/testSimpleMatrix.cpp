773d63987984b10b4003976ba4596c793ec6db0d
Jacques-Olivier Lachaud <jacques-olivier.lachaud@univ-savoie.fr>
2025-08-25 15:25:50 +0200

    Try to fix stupid warning

--- tests/math/linalg/testSimpleMatrix.cpp
+++ tests/math/linalg/testSimpleMatrix.cpp
@@ -442,7 +442,9 @@ bool testBareissDeterminant()
     DGtal::functions::determinantBareiss( M, big_db );
     int64_t   cdb = NumberTraits<BigInteger>::castToInt64_t( big_db );
     trace.info() << "d=" << d << " (i64)db=" << db << " (big)db=" << cdb << "\n";
-    nbok += ( int64_t(d) != -171492636038LL ) ? 1 : 0; // int overflow
+    // The line below raises a warning in the macos compiler.
+    // nbok += ( int64_t(d) != -171492636038LL ) ? 1 : 0; // int overflow
+    nbok += ( int(d) != (-171492636038LL % 2147483648LL ) ) ? 1 : 0; // int overflow
     nb++;
     nbok += ( db != -171492636038LL ) ? 1 : 0; // int64 overflow (intermediate computation)
     nb++;
