ac4ec34a2c9254e4ee9bb4fa3a445d675a17296b
David Coeurjolly <david.coeurjolly@liris.cnrs.fr>
2026-09-25 17:22:04 +0200

    ponca 1.4

--- src/DGtal/geometry/surfaces/estimation/estimationFunctors/SphereFittingEstimator.h
+++ src/DGtal/geometry/surfaces/estimation/estimationFunctors/SphereFittingEstimator.h
@@ -159,12 +159,9 @@ namespace DGtal
                              const double h,
                              const double radius,
                              ConstAlias<NormalVectorEstimatorCache> anEstimator):
-        myEmbedder(&anEmbedder), myH(h), myNormalEsitmatorCache(&anEstimator)
+        myEmbedder(&anEmbedder), myH(h),  myRadius(radius), myNormalEsitmatorCache(&anEstimator)
       {
-        //From Mellado's example
         myFit = new Fit();
-        myWeightFunction = new WeightFunc(radius);
-        myFit->setWeightFunc(*myWeightFunction);
       }
 
 
@@ -173,7 +170,6 @@ namespace DGtal
        */
       ~SphereFittingEstimator( )
       {
-        delete myWeightFunction;
         delete myFit ;
       }
 
@@ -203,7 +199,8 @@ namespace DGtal
         if (myFirstPoint)
           {
             myFirstPoint = false;
-            myFit->init(pp);
+            myFit->setWeightFunc({pp, myRadius});
+            myFit->init();
           }
         else
           myFit->addNeighbor(point);
@@ -268,7 +265,6 @@ namespace DGtal
         delete myFit;
         myFit = new Fit();
         myFirstPoint = true;
-        myFit->setWeightFunc(*myWeightFunction);
      }
 
 
@@ -283,14 +279,14 @@ namespace DGtal
       ///Grid step
       double myH;
 
+      ///Radius
+      double myRadius;
+
       ///Boolean for initial point
       bool myFirstPoint;
 
       ///NormalVectorCache
       const NormalVectorEstimatorCache *myNormalEsitmatorCache;
-
-      ///const WeightFunction
-      const  WeightFunc *myWeightFunction;
 
     }; // end of class SphereFittingEstimator
   }
